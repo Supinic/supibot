10d282fd716b5bc56c329ec3f277408e4c6fdf33
Supinic <supinic@protonmail.com>
2020-07-23 00:31:32 +0200

    table structure updated (Error)

--- init/definitions/chat_data/tables/Error.sql
+++ init/definitions/chat_data/tables/Error.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS `chat_data`.`Error` (
   `Message` mediumtext DEFAULT NULL,
   `Stack` text NOT NULL DEFAULT '',
   `Arguments` text DEFAULT NULL,
+  `Context` text DEFAULT NULL,
   `Timestamp` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(),
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
