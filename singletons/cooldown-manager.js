2c18418f9e53d28f9b83817bb572be738347b687
Supinic <supinic@protonmail.com>
2020-08-31 21:44:15 +0200

    major Pending cooldown fix - add current timestamp...

--- singletons/cooldown-manager.js
+++ singletons/cooldown-manager.js
@@ -151,7 +151,7 @@ module.exports = (function (Module) {
 			this.data.push(new Pending({
 				user,
 				description,
-				expires: this.pendingCooldownExpiration
+				expires: Date.now() + this.pendingCooldownExpiration
 			}));
 		}
 
