0e6920c91ddb37b6de4af97b337274edb06f6b08
Supinic <supinic@protonmail.com>
2020-09-12 19:25:28 +0200

    added dynamic description

--- commands/unmention/index.js
+++ commands/unmention/index.js
@@ -2,7 +2,7 @@ module.exports = {
 	Name: "unmention",
 	Aliases: ["remention"],
 	Author: "supinic",
-	Last_Edit: "2020-09-08T17:25:36.000Z",
+	Last_Edit: "2020-09-09T16:46:44.000Z",
 	Cooldown: 5000,
 	Description: "Makes a specific command (or, in advanced mode, a combination of command/channel/platform, or global) not mention you by removing the \"username,\" part at the beginning.",
 	Flags: ["mention"],
@@ -150,5 +150,47 @@ module.exports = {
 			};
 		}
 	}),
-	Dynamic_Description: null
+	Dynamic_Description: async (prefix) => {
+		return [
+			`Removes, or adds back, the "mention" of a specific command.`,
+			`A mention is basically the "user," part at the start of the command response.`,
+			"While unmentioned, the command(s) will not add this part.",
+			"",
+		
+			`<code><u>Simple mode</u></code>`,
+			`<code>${prefix}unmention (command)</code>`,
+			`Will remove the mention from a given command`,
+			"",
+	
+			`<code>${prefix}remention (command)</code>`,
+			`Will put the mention back in a given command`,
+			"",
+	
+			`<code><u>Total mode</u></code>`,
+			`<code>${prefix}unmention all</code>`,
+			`Will remove all mentions from all current and future commands that support unmentioning, everywhere.`,
+			"Currently, there is no way to combine a global unmention with command-specific ones.",
+			"E.g. you can't unmention all, and then decide to remention from one command. Support for this might come in the future, though.",
+			"",
+	
+			`<code><u>Advanced mode</u></code>`,
+			`<code>${prefix}unmention channel:(chn) command:(cmd) platform:(p)</code>`,
+			`Will remove the mention(s) from a specified combination of channel/command/platform.`,
+			"E.g.:",
+			`<ul>
+				<li> 
+					<code>${prefix}unmention command:rl channel:supibot</code>
+					Will remove the mention from command rl only in channel "supibot".
+				</li>
+				<li> 
+					<code>${prefix}unmention command:rl platform:twitch</code>
+					Will remove the mention from command rl only in Twitch.
+				</li>
+				<li> 
+					<code>${prefix}unmention channel:supibot</code>
+					Will remove the mention from opt-outable commands, only in channel "supibot".
+				</li>
+			</ul>`,
+		];
+	}
 };
