90f847dd8fbf1258bc9dfc88452b3e024de7ef76
Supinic <supinic@protonmail.com>
2020-09-22 19:08:17 +0200

    adjusted the reply, as A. Pollard's parole seems to have been denied

--- commands/freebobby/index.js
+++ commands/freebobby/index.js
@@ -2,15 +2,25 @@ module.exports = {
 	Name: "freebobby",
 	Aliases: null,
 	Author: "supinic",
-	Last_Edit: "2020-09-08T17:25:36.000Z",
+	Last_Edit: "2020-09-22T15:42:24.000Z",
 	Cooldown: 15000,
 	Description: "When is Bobby Shmurda going to be freed? TriHard",
 	Flags: ["mention","pipe"],
 	Whitelist_Response: null,
 	Static_Data: null,
-	Code: async () => {
-		const free = new sb.Date("2020-12-11");
-		return { reply: "Our boy might be free " + sb.Utils.timeDelta(free) + " if he gets his parole TriHard" };
-	},
+	Code: (async function freeBobby () {
+		const probably = new sb.Date("2020-12-11");
+		const free = new sb.Date("2021-12-11");
+	
+		return { 
+			reply: sb.Utils.tag.trim `
+				Our boy might be free ${sb.Utils.timeDelta(probably)},
+				but his parole seems to have been denied on 2020-09-21,
+				and he will most likely serve the rest of his sentence,
+				getting out ${sb.Utils.timeDelta(free)}
+				TriHard
+			`
+		};
+	}),
 	Dynamic_Description: null
 };
