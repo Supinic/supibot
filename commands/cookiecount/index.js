745a9e19795e2a4266a625bdd3aea44ec43a2758
Alex <etztrefishello@gmail.com>
2020-10-04 03:33:44 +0700

    Refactor the cc command into the stats cc (#10)
    * Refactor the cc command into the stats cc

--- commands/cookiecount/index.js
+++ commands/cookiecount/index.js
@@ -91,7 +91,7 @@ module.exports = {
 			}
 	
 			return {
-				reply: `${who} eaten ${total} cookies so far. Out of those, ${cookies.Received} were gifted to ${target}${voidString}. ${giftedString} ${reaction}`
+				reply: `${who} eaten ${total} cookies so far. Out of those, ${cookies.Received} were gifted to ${target}${voidString}. ${giftedString} ${reaction} ❗ This command will soon be archived. Use => "$stats ${context.invocation} (user)" based on the current invocation, with the user included.` 
 			};
 		}
 	}),
