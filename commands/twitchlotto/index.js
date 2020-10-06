68a461df1d53802804346cf0ed1e3b728eace904
Ruben Anders <ruben.anders@robotty.de>
2020-10-06 20:25:37 +0200

    Retry twitchlotto image up to 3 times (#11)
    * Retry twitchlotto image up to 3 times

--- commands/twitchlotto/index.js
+++ commands/twitchlotto/index.js
@@ -35,7 +35,8 @@ module.exports = {
 					string: "Buttocks - Exposed",
 					replacement: "ass"
 				}
-			]
+			],
+			maxRetries: 3
 		};
 	}),
 	Code: (async function lotto (context, channel) {
@@ -57,8 +58,8 @@ module.exports = {
 				};
 			}
 		}
-	
-		let image = null;
+
+		// Preparation work that does not need to run more than once, so it is placed before the loop below.
 		if (channel) {
 			if (!this.data.counts[channel]) {
 				this.data.counts[channel] = await sb.Query.getRecordset(rs => rs
@@ -69,16 +70,6 @@ module.exports = {
 					.flat("Amount")
 				);
 			}
-	
-			const roll = sb.Utils.random(1, this.data.counts[channel]) - 1;
-			image = await sb.Query.getRecordset(rs => rs
-				.select("*")
-				.from("data", "Twitch_Lotto")
-				.where("Channel = %s", channel)
-				.offset(roll)
-				.limit(1)
-				.single()
-			);
 		}
 		else {
 			if (!this.data.counts.total) {
@@ -89,53 +80,78 @@ module.exports = {
 					.flat("Amount")
 				);
 			}
-	
-			const roll = sb.Utils.random(1, this.data.counts.total);
-			const link = await sb.Query.getRecordset(rs => rs
-				.select("Link")
-				.from("data", "Twitch_Lotto")
-				.orderBy("Link ASC")
-				.limit(1)
-				.offset(roll)
-				.single()
-				.flat("Link")
-			);
-	
-			image = await sb.Query.getRecordset(rs => rs
-				.select("*")
-				.from("data", "Twitch_Lotto")
-				.where("Link = %s", link)
-				.single()
-			);
 		}
-	
-		if (image.Available === false) {
-			return {
-				success: false,
-				reply: `This image has been deleted from its host! Try again.`,
-				cooldown: 2500
-			};
-		}
-		else if (image.Available === null) {
-			const { statusCode } = await sb.Got({
-				method: "HEAD",
-				throwHttpErrors: false,
-				followRedirect: false,
-				url: `https://i.imgur.com/${image.Link}`
-			});
-	
-			if (statusCode !== 200) {
-				await sb.Query.getRecordUpdater(ru => ru
-					.update("data", "Twitch_Lotto")
-					.set("Available", false)
-					.where("Link = %s", image.Link)
+
+		// Now try to find an image that is available.
+		let image = null;
+		let failedTries = 0;
+		while (image === null) {
+			if (channel) {
+				const roll = sb.Utils.random(1, this.data.counts[channel]) - 1;
+				image = await sb.Query.getRecordset(rs => rs
+					.select("*")
+					.from("data", "Twitch_Lotto")
+					.where("Channel = %s", channel)
+					.offset(roll)
+					.limit(1)
+					.single()
 				);
-	
+			}
+			else {
+				const roll = sb.Utils.random(1, this.data.counts.total);
+				const link = await sb.Query.getRecordset(rs => rs
+					.select("Link")
+					.from("data", "Twitch_Lotto")
+					.orderBy("Link ASC")
+					.limit(1)
+					.offset(roll)
+					.single()
+					.flat("Link")
+				);
+
+				image = await sb.Query.getRecordset(rs => rs
+					.select("*")
+					.from("data", "Twitch_Lotto")
+					.where("Link = %s", link)
+					.single()
+				);
+			}
+
+			if (image.Available === false) {
+				// discard this image. Loop will continue.
+				failedTries++;
+				image = null;
+			}
+			else if (image.Available === null) {
+				const { statusCode } = await sb.Got({
+					method: "HEAD",
+					throwHttpErrors: false,
+					followRedirect: false,
+					url: `https://i.imgur.com/${image.Link}`
+				});
+
+				if (statusCode !== 200) {
+					await sb.Query.getRecordUpdater(ru => ru
+						.update("data", "Twitch_Lotto")
+						.set("Available", false)
+						.where("Link = %s", image.Link)
+					);
+
+					// discard this image. Loop will continue.
+					failedTries++;
+					image = null;
+				}
+			}
+
+			if (failedTries > this.staticData.maxRetries) {
+				// Was not able to find an image that existed.
 				return {
 					success: false,
-					reply: `Image is no longer available! https://i.imgur.com/${image.Link}`
+					reply: `Could not find an image that was still available (${this.staticData.maxRetries} images were checked and found to be deleted)!`,
+					cooldown: 2500
 				};
 			}
+			// Success: image is not null, and loop will terminate.
 		}
 	
 		if (image.Score === null) {
