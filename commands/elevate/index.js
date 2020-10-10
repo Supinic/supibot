fc1c4b44ccf49dce21fcdec295415ce73ec9f5bc
Supinic <supinic@protonmail.com>
2020-10-10 21:30:21 +0200

    initial command skeleton created

---
+++ commands/elevate/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+	Name: "elevate",
+	Aliases: null,
+	Author: "supinic",
+	Cooldown: 5000,
+	Description: "Transforms a suggestion that you have created into a Github issue, thus \"elevating\" its status. Only usable by people who have linked their Github account via the supinic.com website.",
+	Flags: ["developer","mention"],
+	Whitelist_Response: null,
+	Static_Data: null,
+	Code: (async function elevate (context, ID) {
+	
+	}),
+	Dynamic_Description: null
+};
