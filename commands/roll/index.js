19b0c4e5ce2c2cb70e45089ac73754c93bcf3388
Jan Procházka <honza.spacir@gmail.com>
2020-10-13 19:54:11 +0200

    Enhance roll command (#24)
    * Implemented recursive descent parser for standard dice notation expressions

--- commands/roll/index.js
+++ commands/roll/index.js
@@ -3,12 +3,21 @@ module.exports = {
 	Aliases: null,
 	Author: "supinic",
 	Cooldown: 5000,
-	Description: "Rolls a random number. If nothing is specified, rolls 1-100. You can also specify <number X>d<number Y> for X amount of dice rolls with Y sides each.",
+	Description: "Rolls a random number. If nothing is specified, rolls 1-100. You can specify min and max values, or some expression using standard dice notation.",
 	Flags: ["mention","pipe","skip-banphrase"],
 	Whitelist_Response: null,
 	Static_Data: null,
-	Code: (async function roll (context, first, second) {
-		let format = [1, 100];
+	Code: (async function roll (context, ...args) {
+		if (args.length === 0) {
+			const result = sb.Utils.random(1, 100);
+			if (context.append.pipe) {
+				return { reply: String(result) };
+			} 
+			else {
+				return { reply: `Your roll is ${result}.` };
+			}
+		}
+
 		if (Number(first) && Number(second)) {
 			first = Number(first);
 			second = Number(second);
@@ -24,53 +33,29 @@ module.exports = {
 				return { reply: String(number ) };
 			}
 			else {
-				return { reply: "Your specific roll (no dice) is " + number };
+				return { reply: `Your specific roll (no dice) is ${number}.` };
 			}
 		}
-		else if (first) {
-			format = first.split("d").map(Number);
-		}
-	
-		if (format[0] < 0) {
-			const prefix = (Math.abs(format[0]) === 1) ? "die" : "dice";
-			return { reply: "Don't take my " + prefix + " away FeelsBadMan" };
-		}
-		else if (format[0] === 0) {
-			return { reply: "I can't roll with no dice FeelsBadMan" };
-		}
-		else if (format[1] <= 1) {
-			const prefix = (format[0] === 1) ? "Die" : "Dice";
-			const sides = (format[1] === 0)
-				? "no sides"
-				: (format[1] === 1) ? "one side" : "negative amount of sides";
-	
-			return { reply: `${prefix} with ${sides}? That's an interesting topological exercise...` };
-		}
-		else if (format.length !== 2 || format.some(i => i <= 0 || Math.trunc(i) !== i || !Number.isFinite(i))) {
-			return { reply: "Incorrect dice format!" };
-		}
-		else if (format[1] > Number.MAX_SAFE_INTEGER) {
-			const prefix = (format[0] === 1)? "That die has" : "Those dice have";
-			return { reply: prefix + " way too many sides!" };
-		}
-		else if (format[0] > 1.0e6) {
-			return { reply: "I don't have that many dice!" };
-		}
-	
-		let sum = 0;
-		let [times, sides] = format;
-		while (times--) {
-			sum += sb.Utils.random(1, sides);
-		}
-	
-		if (sum === Infinity) {
-			return { reply: "WAYTOODANK" };
-		}
-		else if (context.append.pipe) {
-			return { reply: String(sum) };
-		}
-		else {
-			return { reply: "Your roll is " + sum + "." };
+
+		try {
+			const result = sb.Utils.evalDiceRoll(args.join(""), 1.0e6);
+
+			if (result === Infinity) {
+				return { reply: "WAYTOODANK" };
+			} 
+			else if (context.append.pipe) {
+				return { reply: String(result) };
+			} 
+			else {
+				return { reply: `Your roll is ${result}` };
+			}
+		} catch (error) {
+			if (context.append.pipe) {
+				return { reply: String(error.message), success: false };
+			} 
+			else {
+				return { reply: `WAYTOODANK ${error.message}` };
+			}
 		}
 	}),
 	Dynamic_Description: null
