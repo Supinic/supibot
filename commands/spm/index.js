497f53a97aa48f0276d37215e7cb164bfa054547
Supinic <supinic@protonmail.com>
2020-10-17 20:07:53 +0200

    major refactor - uses static data, added chat-module support

--- commands/spm/index.js
+++ commands/spm/index.js
@@ -7,32 +7,15 @@ module.exports = {
 	Flags: ["developer","mention","whitelist"],
 	Whitelist_Response: "Only Supi can use this command, but you can check the repository here: https://github.com/supinic/supibot-package-manager peepoHackies",
 	Static_Data: (() => ({
-		exists: require("util").promisify(require("fs").exists)
-	})),
-	Code: (async function spm (context, ...args) {
-		const operation = args.shift()?.toLowerCase();
-		if (!operation) {
-			return {
-				success: false,
-				reply: "No spm operation provided"
-			};
-		}
-	
-		const type = args.shift()?.toLowerCase();
-		if (!type) {
-			return {
-				success: false,
-				reply: "No operation type provided"
-			};
-		}
-	
-		const fs = require("fs").promises;
-		const shell = require("util").promisify(require("child_process").exec);
-	
-		if (operation === "dump") {
-			switch (type) {
-				case "command":
-				case "commands": {
+		exists: require("util").promisify(require("fs").exists),
+		operations: ["dump", "load"],
+		helpers: {},
+		commands: [
+			{
+				name: "command",
+				aliases: ["commands"],
+				description: "",
+				dump: async function (fs, shell, ...args) {
 					const now = new sb.Date();
 					const updated = [];
 					const commands = (args.length > 0)
@@ -51,10 +34,10 @@ module.exports = {
 						if (!await this.staticData.exists(dir)) {
 							await fs.mkdir(dir);
 						}
-	
+
 						let row = await sb.Query.getRow("chat_data", "Command");
 						let save = false;
-	
+
 						try {
 							// Only allow the overwrite of an existing command when
 							// the database definition changed more recently than the file
@@ -62,7 +45,7 @@ module.exports = {
 								fs.stat(`${dir}/index.js`),
 								row.load(command.ID)
 							]);
-	
+
 							if (row.values.Last_Edit > stats.mtime) {
 								save = true;
 							}
@@ -75,11 +58,11 @@ module.exports = {
 								throw e;
 							}
 						}
-	
+
 						if (save) {
 							updated.push(command.Name);
 							row.values.Last_Edit = now;
-	
+
 							await Promise.all([
 								row.save(),
 								command.serialize({
@@ -89,9 +72,9 @@ module.exports = {
 							]);
 						}
 					});
-	
+
 					await Promise.all(promises);
-	
+
 					updated.sort();
 					const suffix = (updated.length === 0) ? "" : "s";
 					return {
@@ -99,42 +82,20 @@ module.exports = {
 							? `No changes detected, nothing was saved peepoNerdDank 👆`
 							: `Saved ${updated.length} command${suffix} into spm/commands peepoHackies`
 					};
-				}
-	
-				default: return {
-					success: false,
-					reply: "Unsupported spm dump operation provided!"
-				};
-			}
-		}
-		else if (operation === "load") {
-			try {
-				const result = await shell("git -C /code/spm pull origin master");
-				await context.channel.send(`git pull PepoG ${result.stdout}`);
-			}
-			catch (e) {
-				console.error("git pull error", e);
-				return {
-					success: false,
-					reply: `git pull failed monkaStare error is console`
-				};
-			}
-
-			switch (type) {
-				case "command":
-				case "commands": {
+				},
+				load: async function (fs, shell, ...args) {
 					const updated = [];
 					const commandDirs = (args.length > 0)
 						? args.map(i => sb.Command.get(i)?.Name ?? i)
 						: await fs.readdir("/code/spm/commands");
-	
+
 					const promises = commandDirs.map(async (command) => {
 						const commandFile = `/code/spm/commands/${command}/index.js`;
 						if (!await this.staticData.exists(commandFile)) {
 							console.warn(`index.js file for command ${command} does not exist!`);
 							return;
 						}
-	
+
 						// Fetch the latest commit for a given file
 						const commitHash = (await shell(sb.Utils.tag.trim `
 							git
@@ -143,29 +104,29 @@ module.exports = {
 							--pretty=format:%H
 							-- commands/${command}/index.js
 						`)).stdout;
-	
+
 						// Command file has no git history, skip
 						if (!commitHash)   {
 							console.log(`Command ${command}: no Git history`);
 							return;
 						}
-	
+
 						const currentCommand = sb.Command.get(command);
 						if (!currentCommand) { // New command - save
 							console.warn("New command detected - functionality not yet implemented");
 							return;
 						}
-	
+
 						const row = await sb.Query.getRow("chat_data", "Command");
 						await row.load(currentCommand.ID);
 						if (row.values.Latest_Commit === commitHash) {
 							console.log(`Command ${command}: no change`);
 							return;
 						}
-	
+
 						delete require.cache[require.resolve(commandFile)];
 						const definition = require(commandFile);
-	
+
 						const jsonify = ["Aliases"];
 						const functionStringify = ["Static_Data", "Code", "Dynamic_Description"];
 						for (const [key, value] of Object.entries(definition)) {
@@ -182,19 +143,19 @@ module.exports = {
 										lines[i] = lines[i].slice(1);
 									}
 								}
-	
+
 								row.values[key] = lines.join("\n");
 							}
 							else {
 								row.values[key] = value;
 							}
 						}
-	
+
 						row.values.Latest_Commit = commitHash;
 						await row.save();
 						updated.push(currentCommand.Name);
 					});
-	
+
 					await Promise.all(promises);
 
 					// If the spm command is tasked with reloading itself, don't call reloadSpecific immediately,
@@ -212,7 +173,7 @@ module.exports = {
 					if (updated.length > 0) {
 						await sb.Command.reloadSpecific(...updated);
 					}
-	
+
 					updated.sort();
 					const suffix = (updated.length === 1) ? "" : "s";
 					return {
@@ -221,19 +182,215 @@ module.exports = {
 							: `Loaded ${updated.length} command${suffix} (${updated.join(", ")}) from spm/commands peepoHackies`
 					};
 				}
+			},
+			{
+				name: "chat-module",
+				aliases: ["chatmodule", "chatmodules", "chat-modules"],
+				dump: async function (fs, shell, ...args) {
+					const now = new sb.Date();
+					const updated = [];
+					const modules = (args.length > 0)
+						? args.map(i => sb.ChatModule.get(i)).filter(Boolean)
+						: sb.ChatModule.data;
+
+					if (modules.length === 0) {
+						return {
+							success: false,
+							reply: "No valid commands provided!"
+						};
+					}
+
+					const promises = modules.map(async (module) => {
+						const dir = `/code/spm/chat-modules/${module.Name}`;
+						if (!await this.staticData.exists(dir)) {
+							await fs.mkdir(dir);
+						}
+
+						let row = await sb.Query.getRow("chat_data", "Chat_Module");
+						let save = false;
+
+						try {
+							// Only allow the overwrite of an existing command when
+							// the database definition changed more recently than the file
+							const [stats] = await Promise.all([
+								fs.stat(`${dir}/index.js`),
+								row.load(module.ID)
+							]);
+
+							if (row.values.Last_Edit > stats.mtime) {
+								save = true;
+							}
+						}
+						catch (e) {
+							if (e.message.includes("ENOENT")) {
+								save = true;
+							}
+							else {
+								throw e;
+							}
+						}
+
+						if (save) {
+							updated.push(module.Name);
+							row.values.Last_Edit = now;
+
+							await Promise.all([
+								row.save(),
+								module.serialize({
+									overwrite: true,
+									filePath: `${dir}/index.js`
+								})
+							]);
+						}
+					});
+
+					await Promise.all(promises);
+
+					updated.sort();
+					const suffix = (updated.length === 0) ? "" : "s";
+					return {
+						reply: (updated.length === 0)
+							? `No changes detected, nothing was saved peepoNerdDank 👆`
+							: `Saved ${updated.length} chat-module${suffix} into spm/chat-modules peepoHackies`
+					};
+				},
+				load: async function (fs, shell, ...args) {
+					const updated = [];
+					const commandDirs = (args.length > 0)
+						? args.map(i => sb.Command.get(i)?.Name ?? i)
+						: await fs.readdir("/code/spm/chat-modules");
+
+					const promises = commandDirs.map(async (command) => {
+						const commandFile = `/code/spm/commands/${command}/index.js`;
+						if (!await this.staticData.exists(commandFile)) {
+							console.warn(`index.js file for command ${command} does not exist!`);
+							return;
+						}
+
+						// Fetch the latest commit for a given file
+						const commitHash = (await shell(sb.Utils.tag.trim `
+							git
+							-C /code/spm
+							log -n 1
+							--pretty=format:%H
+							-- commands/${command}/index.js
+						`)).stdout;
+
+						// Command file has no git history, skip
+						if (!commitHash)   {
+							console.log(`Command ${command}: no Git history`);
+							return;
+						}
+
+						const currentCommand = sb.Command.get(command);
+						if (!currentCommand) { // New command - save
+							console.warn("New command detected - functionality not yet implemented");
+							return;
+						}
+
+						const row = await sb.Query.getRow("chat_data", "Command");
+						await row.load(currentCommand.ID);
+						if (row.values.Latest_Commit === commitHash) {
+							console.log(`Command ${command}: no change`);
+							return;
+						}
+
+						delete require.cache[require.resolve(commandFile)];
+						const definition = require(commandFile);
+
+						const jsonify = ["Aliases"];
+						const functionStringify = ["Static_Data", "Code", "Dynamic_Description"];
+						for (const [key, value] of Object.entries(definition)) {
+							if (value === null) {
+								row.values[key] = null;
+							}
+							else if (jsonify.includes(key)) {
+								row.values[key] = JSON.stringify(value);
+							}
+							else if (functionStringify.includes(key)) {
+								const lines = `(${value})`.split("\n");
+								for (let i = 0; i < lines.length; i++) {
+									if (lines[i].startsWith("\t")) {
+										lines[i] = lines[i].slice(1);
+									}
+								}
+
+								row.values[key] = lines.join("\n");
+							}
+							else {
+								row.values[key] = value;
+							}
+						}
+
+						row.values.Latest_Commit = commitHash;
+						await row.save();
+						updated.push(currentCommand.Name);
+					});
+
+					await Promise.all(promises);
+
+					if (updated.length > 0) {
+						await sb.ChatModule.reloadData();
+					}
+
+					updated.sort();
+					const suffix = (updated.length === 1) ? "" : "s";
+					return {
+						reply: (updated.length === 0)
+							? `No changes detected, no chat modules were loaded peepoNerdDank 👆`
+							: `Loaded ${updated.length} chat module${suffix} (${updated.join(", ")}) from spm/chat-modules peepoHackies`
+					};
+				}
+			}
+		]
+	})),
+	Code: (async function spm (context, ...args) {
+		const operation = args.shift()?.toLowerCase();
+		if (!operation) {
+			return {
+				success: false,
+				reply: "No spm operation provided"
+			};
+		}
+		else if (!this.staticData.operations.incudes(operation)) {
+			return {
+				success: false,
+				reply: "Invalid spm operation provided"
+			};
+		}
 	
-				default: return {
+		const type = args.shift()?.toLowerCase();
+		const definition = this.staticData.commands.find(i => i.name === type || i.aliases.includes(type));
+		if (!type) {
+			return {
+				success: false,
+				reply: "Invalid operation target provided"
+			};
+		}
+		else if (!definition[operation]) {
+			return {
+				success: false,
+				reply: `spm ${type} does not have functionality for the "${operation}" operation`
+			};
+		}
+	
+		const fs = require("fs").promises;
+		const shell = require("util").promisify(require("child_process").exec);
+		if (operation === "load") {
+			try {
+				const result = await shell("git -C /code/spm pull origin master");
+				await context.channel.send(`git pull PepoG ${result.stdout}`);
+			}
+			catch (e) {
+				console.error("git pull error", e);
+				return {
 					success: false,
-					reply: "Unsupported spm load operation provided!"
+					reply: `git pull failed monkaStare error is in console`
 				};
 			}
 		}
-		else {
-			return {
-				success: false,
-				reply: "Invalid spm operation provided!"
-			};
-		}
+
+		return await definition[operation](fs, shell, ...args);
 	}),
 	Dynamic_Description: null
 };
