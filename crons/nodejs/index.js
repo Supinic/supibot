30239ce418b2866da56ed63852a338be76eab010
Supinic <supinic@protonmail.com>
2020-10-24 22:12:26 +0200

    now sends out system reminders
    users subscribed to node.js version ping that are not around when it is announced (based on Redis user cache) will receive a system reminder from the bot, so that they don't miss it

--- crons/nodejs/index.js
+++ crons/nodejs/index.js
@@ -7,24 +7,46 @@ module.exports = {
 		const rawData = await sb.Got.instances.GitHub({
 			url: "repos/nodejs/node/releases",
 		}).json();
-	
+
 		const latest = rawData.sort((a, b) => new sb.Date(b.created_at) - new sb.Date(a.created_at)).shift();
-	
+		const nodeString = `New Node.js version detected! PagChomp 👉 ${latest.tag_name} Changelog: ${latest.html_url}`;
+
 		if (latest.tag_name !== sb.Config.get("LATEST_NODE_JS_VERSION")) {
 			console.log("New nodejs version!", sb.Config.get("LATEST_NODE_JS_VERSION"), latest.tag_name);
 			sb.Config.set("LATEST_NODE_JS_VERSION", latest.tag_name);
 			
-			const pingedUsers = (await sb.Query.getRecordset(rs => rs
+			const userNames = await sb.Query.getRecordset(rs => rs
 				.select("User_Alias.Name AS Username")
 				.from("chat_data", "Event_Subscription")
 				.join("chat_data", "User_Alias")
 				.where("Type = %s", "Node.js updates")
 				.where("Active = %b", true)
 				.flat("Username")
-			)).map(i => `@${i}`).join(" ");
-			
-			const channelData = sb.Channel.get(38);
-			channelData.send(`${pingedUsers} New Node.js version detected! PagChomp 👉 ${latest.tag_name} Changelog: ${latest.html_url}`);
+			);
+
+			const uncachedUsers = await Promise.all(userNames.map(async (name) => {
+				const key = sb.User.createCacheKey({ name });
+				const cache = await sb.Cache.getByPrefix(key);
+				return (cache === null)
+					? await sb.User.get(name)
+					: null
+			}));
+
+			await Promise.all(uncachedUsers.map(userData => (
+				sb.Reminder.create({
+					Channel: null,
+					User_From: 1127,
+					User_To: userData.ID,
+					Text: `${nodeString} (You were not around when it was announced)`,
+					Schedule: null,
+					Created: new sb.Date(),
+					Private_Message: true,
+					Platform: 1
+				}, true)
+			)));
+
+			const pingedUsers = userNames.map(i => `@${i}`).join(" ");
+			await sb.Channel.get(38).send(`${pingedUsers} ${nodeString}`);
 		}
 	})
 };
