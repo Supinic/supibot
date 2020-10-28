55249471efebbd0dbc03504c4c336b5b3a4c647c
Supinic <supinic@protonmail.com>
2020-10-28 17:49:25 +0100

    initial implementation
    implements S#5595 - https://supinic.com/data/suggestion/5595

---
+++ chat-modules/streamer-health-notification/index.js
@@ -0,0 +1,25 @@
+module.exports = {
+	Name: "streamer-health-notification",
+	Events: ["online", "offline"],
+	Description: "Sets up a periodic \"health notification\" when the channel goes live, and removes it when going offline.",
+	Code: (async (context, options = {}) => {
+		if (typeof this.data.intervals === "undefined") {
+			this.data.intervals = {};
+		}
+
+		const { minuteDelay = 30 } = options;
+		const { channel, event } = context;
+		const { intervals } = this.data;
+		if (event === "online" && !intervals[channel.ID]) {
+			intervals[channel.ID] = setInterval(
+				() => channel.send("Make sure to stay hydrated 💧 and stretch 💪 regularly!"),
+				minuteDelay * 60_000
+			);
+		}
+		else if (event === "offline" && intervals[channel.ID]) {
+			clearInterval(intervals[channel.ID]);
+			intervals[channel.ID] = null;
+		}
+	}),
+	Author: "supinic"
+};
