6895c1cf7ba092f4799db85caef3633436d8926c
Supinic <supinic@protonmail.com>
2020-10-28 18:10:55 +0100

    updated table init scripts to reflect current usage
    fixes S#5415 - https://supinic.com/data/suggestion/5415

---
+++ init/definitions/chat_data/tables/Banphrase_API_Denial_Log.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS `chat_data`.`Banphrase_API_Denial_Log` (
+	`ID` INT(11) NOT NULL AUTO_INCREMENT,
+	`Message` MEDIUMTEXT NOT NULL COLLATE 'utf8mb4_general_ci',
+	`Response` MEDIUMTEXT NOT NULL COLLATE 'utf8mb4_general_ci',
+	`Channel` INT(10) UNSIGNED NOT NULL,
+	`Platform` INT(10) UNSIGNED NOT NULL,
+	`API` VARCHAR(128) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`Timestamp` DATETIME(3) NOT NULL DEFAULT current_timestamp(3),
+	PRIMARY KEY (`ID`) USING BTREE,
+	INDEX `FK_Banphrase_API_Denial_Log_Channel` (`Channel`) USING BTREE,
+	INDEX `FK_Banphrase_API_Denial_Log_Platform` (`Platform`) USING BTREE,
+	CONSTRAINT `FK_Banphrase_API_Denial_Log_Channel` FOREIGN KEY (`Channel`) REFERENCES `chat_data`.`Channel` (`ID`) ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT `FK_Banphrase_API_Denial_Log_Platform` FOREIGN KEY (`Platform`) REFERENCES `chat_data`.`Platform` (`ID`) ON UPDATE CASCADE ON DELETE CASCADE
+)
+COLLATE='utf8mb4_general_ci'
+ENGINE=InnoDB;
--- init/definitions/chat_data/tables/Command.sql
+++ init/definitions/chat_data/tables/Command.sql
@@ -1,15 +1,21 @@
 CREATE TABLE IF NOT EXISTS `chat_data`.`Command` (
-  `ID` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `Name` VARCHAR(50) NOT NULL,
-  `Aliases` text DEFAULT NULL COMMENT 'JSON array of strings that will be this command\'s aliases.',
-  `Flags` SET('archived','block','developers','opt-out','owner-override','mention','pipe','read-only','rollback','skip-banphrase','system','whitelist') DEFAULT NULL,
-  `Description` VARCHAR(300) DEFAULT NULL,
-  `Cooldown` INT(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT 'Command cooldown given in milliseconds', 
-  `Whitelist_Response` varchar(300) DEFAULT NULL COMMENT 'If the command is Whitelisted, this is the reply that will be sent to non-whitelisted users trying to invoke it.',
-  `Static_Data` MEDIUMTEXT DEFAULT NULL COMMENT 'Persistent data stored as a Javascript object or function which returns an object.',
-  `Code` TEXT NOT NULL COMMENT 'Javascript command code. Must be a function, ideally async function if async operations are expected. First argument is context, the rest is rest-arguments from the user, split by space.',
-  `Dynamic_Description` TEXT DEFAULT NULL COMMENT 'Javascript function that returns command\'s description on website. Usually async function. First argument = command prefix (string).',
-  `Source` VARCHAR(300) NOT NULL COLLATE 'utf8mb4_general_ci',
-  PRIMARY KEY (`ID`),
-  UNIQUE KEY `Name` (`Name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+	`ID` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+	`Name` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`Aliases` TEXT(65535) NULL DEFAULT NULL COMMENT 'JSON array of strings that will be this command\'s aliases.' COLLATE 'utf8mb4_general_ci',
+	`Flags` SET('archived','block','developer','link-only','mention','opt-out','owner-override','ping','pipe','read-only','rollback','skip-banphrase','system','use-params','whitelist') NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+	`Description` VARCHAR(300) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+	`Cooldown` INT(11) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'Command cooldown given in milliseconds',
+	`Whitelist_Response` VARCHAR(300) NULL DEFAULT NULL COMMENT 'If the command is Whietlisted, this is the reply that will be sent to non-whitelisted users trying to invoke it.' COLLATE 'utf8mb4_general_ci',
+	`Static_Data` MEDIUMTEXT NULL DEFAULT NULL COMMENT 'Persistent data stored as a Javascript object.' COLLATE 'utf8mb4_general_ci',
+	`Code` TEXT(65535) NOT NULL COMMENT 'Javascript command code. Must be a function, ideally async function if async operations are expected. First argument is context, the rest is rest-arguments from the user, split by space.' COLLATE 'utf8mb4_general_ci',
+	`Dynamic_Description` TEXT(65535) NULL DEFAULT NULL COMMENT 'Javascript function that returns command\'s description on website. Usually async function. First argument = command prefix (string).' COLLATE 'utf8mb4_general_ci',
+	`Source` VARCHAR(300) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`Author` VARCHAR(100) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+	`Last_Edit` DATETIME NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+	`Latest_Commit` VARCHAR(100) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+	PRIMARY KEY (`ID`) USING BTREE,
+	UNIQUE INDEX `Name` (`Name`) USING BTREE
+)
+COLLATE='utf8mb4_general_ci'
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4;;
