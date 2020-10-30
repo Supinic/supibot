58f4bb84bb2ba3559a8894000e24c93c0df5fa44
Supinic <supinic@protonmail.com>
2020-10-30 19:02:54 +0100

    now checks usernames' char-length, not byte-length
    fixes S#5609 - https://supinic.com/data/suggestion/5609

--- controllers/discord.js
+++ controllers/discord.js
@@ -55,7 +55,7 @@ module.exports = class Discord extends require("./template.js") {
 				user
 			} = this.parseMessage(messageObject);
 
-			if (user.length > 32) {
+			if (Array.from(user).length > 32) {
 				console.warn("Invalid Discord username! user.length > 32, skipping", {
 					chan, discordID, guild, msg, user, userLength: user.length
 				});
