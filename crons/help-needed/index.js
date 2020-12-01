91d2beeb5eb0425d76310223d266de882292af83
Supinic <supinic@protonmail.com>
2020-12-01 21:57:03 +0100

    initial implementation

---
+++ crons/help-needed/index.js
@@ -0,0 +1,15 @@
+module.exports = {
+	Name: "discord",
+	Expression: "0 30 */2 * * *",
+	Description: "Posts some text to spread awareness and encourage chatters to check something out. It depends..",
+	Defer: null,
+	Type: "Bot",
+	Code: (async function announceDiscordLink () {
+		const channelData = sb.Channel.get("supinic", "twitch");
+		await channelData.send(sb.Utils.tag.trim `
+			Supi is currently looking for ideas to put together a FAQ section about Supibot!
+			If you have any ideas, or would like to see something mentioned there, make a new suggestion
+			with the $suggest command, and make sure to mention "FAQ" or the suggestion "5853".
+		`);
+	})
+};
