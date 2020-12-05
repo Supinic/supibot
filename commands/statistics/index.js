b972c3062bebbd71c18e6a4c3b5865ec020ac7e6
Supinic <supinic@protonmail.com>
2020-12-05 20:31:02 +0100

    adding reminder stats

--- commands/statistics/index.js
+++ commands/statistics/index.js
@@ -321,6 +321,28 @@ module.exports = {
 					reply: "Check Supibot's statistics here: https://supinic.com/bot/stats"
 				})
 			},
+			{
+				name: "reminder",
+				aliases: ["reminders"],
+				description: "Shows how many times you or someone else have used reminders.",
+				execute: async (context) => {
+					const data = await sb.Query.getRecordset(rs => rs
+					    .select("SUM(Schedule IS NULL) AS Unscheduled")
+					    .select("SUM(Schedule IS NOT NULL) AS Scheduled")
+					    .from("chat_data", "Reminder")
+						.where("User_From = %n", context.user.ID)
+						.single()
+					);
+
+					return {
+						reply: sb.Utils.tag.trim `
+							So far, you have created ${data.Unscheduled} direct reminders
+							and ${data.Scheduled} timed reminders,
+							for a total of ${data.Unscheduled + $data.Scheduled}.
+						`
+					};
+				}
+			},
 		]
 	})),
 	Code: (async function statistics (context, type, ...args) {
