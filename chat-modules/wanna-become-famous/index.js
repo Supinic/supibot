dfc314b03329badf775817d057bb59b0104eba38
Supinic <supinic@protonmail.com>
2020-12-05 20:26:29 +0100

    initial implementation

---
+++ chat-modules/wanna-become-famous/index.js
@@ -0,0 +1,33 @@
+module.exports = {
+	Name: "wanna-become-famous",
+	Events: ["message"],
+	Description: "Bans bots that spam the follower site promotion",
+	Code: (async function (context) {
+		const msg = sb.Utils.removeAccents(context.message).toLowerCase();
+		if (!msg.includes("wanna become famous?") && !msg.includes("bigfollows")) {
+			return;
+		}
+
+		if (!context.user && context.raw?.user) {
+			const name = context.raw.user;
+			context.platform.client.privmsg(context.channel.Name, `/ban ${name}`);
+			await channelData.send("NOIDONTTHINKSO I don't wanna become famous");
+			return;
+		}
+		
+		const messsageCount = await sb.Query.getRecordset(rs => rs
+		    .select("Message_Count")
+		    .from("chat_data", "Message_Meta_User_Alias")
+			.where("Channel = %n", context.channel.ID)
+			.where("User_Alias = %n", context.user.ID)
+			.single()
+			.flat("Message_Count")
+		);
+
+		if (typeof messsageCount === "undefined" || messageCount <= 1) {
+			context.platform.client.privmsg(context.channel.Name, `/ban ${context.user.Name}`);
+			await channelData.send("NOIDONTTHINKSO");
+		}
+	}),
+	Author: "supinic"
+};
