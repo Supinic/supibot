f6128d3ce186cd14ec0d2dce9d1296b9d4aea858
Supinic <supinic@protonmail.com>
2020-12-08 21:02:58 +0100

    reworked to use a strict 50 uses/day limit
    - long per-user global cooldown
    - using timed cache to keep track of uses/day
    - removed whitelist
    - added non-nullable flag

--- commands/youtubesearch/index.js
+++ commands/youtubesearch/index.js
@@ -2,45 +2,94 @@ module.exports = {
 	Name: "youtubesearch",
 	Aliases: ["ys"],
 	Author: "supinic",
-	Cooldown: 15000,
-	Description: "Searches Youtube for video(s) with your query. Respects safe search for each platform.",
-	Flags: ["mention","pipe","whitelist"],
+	Cooldown: 60000,
+	Description: "Searches Youtube for video(s) with your query. Only a certain amount of uses are available daily.",
+	Flags: ["mention","non-nullable","pipe"],
 	Whitelist_Response: "Temporarily disabled",
-	Static_Data: null,
+	Static_Data: (() => ({
+		maxUses: 50,
+		getClosestPacificMidnight: () => {
+			const now = new sb.Date().discardTimeUnits("m", "s", "ms");
+			const result = now.clone().discardTimeUnits("h").addHours(9);
+
+			if (now.hours >= 9) {
+				result.addDays(1);
+			}
+
+			return result;
+		}
+	})),
 	Code: (async function youtubeSearch (context, ...args) {
 		const query = args.join(" ");
 		if (!query) {
 			return {
 				success: false,
-				reply: `No query provided!`
+				reply: `No query provided!`,
+				cooldown: 5000
 			};
 		}
-	
-		let safeSearch = "strict";
-		if (context.platform.name === "discord") {
-			if (!context.channel || context.channel.NSFW) {
-				safeSearch = "off";
-			}
-			else {
-				safeSearch = "moderate";
-			}
+
+		const { getClosestPacificMidnight, maxUses } = this.staticData;
+		let remainingUsesToday = await this.getCacheData("remaining-uses");
+		let cacheRecordExists = true;
+		if (remainingUsesToday === null) {
+			remainingUsesToday = maxUses;
+			cacheRecordExists = false;
 		}
-	
+
+		if (remainingUsesToday <= 0) {
+			const when = sb.Utils.timeDelta(getClosestPacificMidnight());
+			return {
+				success: false,
+				reply: `No more Youtube searches available today! Reset happens at midnight PT, which is ${when}.`
+			};
+		}
+
 		const track = await sb.Utils.searchYoutube(
 			query,
 			sb.Config.get("API_GOOGLE_YOUTUBE"),
 			{ single: true }
 		);
+
+		remainingUsesToday--;
+
+		// @todo: Resolve a possible race-condition with multiple command invocations at the same time
+		if (cacheRecordExists) {
+			await this.setCacheData({
+				keys: "remaining-uses",
+				value: remainingUsesToday,
+				keepTTL: true
+			})
+		}
+		else {
+			await this.setCacheData({
+				keys: "remaining-uses",
+				value: remainingUsesToday,
+				expiresAt: getClosestPacificMidnight()
+			});
+		}
 		
 		if (!track) {
 			return {
 				success: false,
-				reply: "No videos found for that query!"
+				reply: "No videos found for that query!",
+				cooldown: {
+					length: this.Cooldown,
+					user: context.user.ID,
+					channel: null,
+					platform: null
+				}
 			};
 		}
 		else {
 			return {
-				reply: `https://youtu.be/${track.ID}`
+				reply: `https://youtu.be/${track.ID}`,
+				cooldown: {
+				length: this.Cooldown,
+					user: context.user.ID,
+					channel: null,
+					platform: null
+				}
 			};
 		}
 	}),
