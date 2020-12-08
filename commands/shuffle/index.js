55497e2fe260f8976b952844566d6d1258228051
Supinic <supinic@protonmail.com>
2020-12-08 18:56:10 +0100

    divided "normal" and "fancy" usages via a parameter

--- commands/shuffle/index.js
+++ commands/shuffle/index.js
@@ -4,7 +4,7 @@ module.exports = {
 	Author: "supinic",
 	Cooldown: 10000,
 	Description: "Shuffles the provided message, word by word.",
-	Flags: ["non-nullable","pipe"],
+	Flags: ["non-nullable","pipe","use-params"],
 	Whitelist_Response: null,
 	Static_Data: null,
 	Code: (async function shuffle (context, ...args) {
@@ -14,16 +14,31 @@ module.exports = {
 				reply: "No input provided!"
 			};
 		}
-	
-		const result = [];
-		const message = args.join(" ").split(/\b|(?:(\s))/).filter(Boolean);
-		while (message.length > 0) {
-			const randomIndex = sb.Utils.random(0, message.length - 1);
-			result.push(message[randomIndex]);
-			message.splice(randomIndex, 1);
+
+		let reply;
+		if (context.params.fancy === "true") {
+			const result = [];
+			const message = args.join(" ").split(/\b|(?:(\s))/).filter(Boolean);
+			while (message.length > 0) {
+				const randomIndex = sb.Utils.random(0, message.length - 1);
+				result.push(message[randomIndex]);
+				message.splice(randomIndex, 1);
+			}
+
+			reply = result.join(" ").replace(/\s+/g, " ");
 		}
-	
-		const reply = result.join(" ").replace(/\s+/g, " ");
+		else {
+			const result = [];
+			const message = [...args];
+			while (message.length > 0) {
+				const randomIndex = sb.Utils.random(0, message.length - 1);
+				result.push(message[randomIndex]);
+				message.splice(randomIndex, 1);
+			}
+
+			reply = result.join(" ");
+		}
+
 		return { 
 			reply,
 			cooldown: {
@@ -31,5 +46,17 @@ module.exports = {
 			}
 		};
 	}),
-	Dynamic_Description: null
+	Dynamic_Description: (async (prefix) => {
+		return [
+			"For a given message, shuffles the words around.",
+			"",
+
+			`<code>${prefix}shuffle this is a random message`
+			`a random is message this`,
+			"",
+
+			`<code>${prefix}shuffle fancy:true (this) isn't a random! message`
+			`) isn a ' ! random this ( message`,
+		];
+	})
 };
