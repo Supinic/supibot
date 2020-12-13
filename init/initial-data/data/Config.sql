58e292eb19f09d5fc7855efbb5df6e9bff5df4d5
Supinic <supinic@protonmail.com>
2020-12-13 12:27:49 +0100

    whoops

--- init/initial-data/data/Config.sql
+++ init/initial-data/data/Config.sql
@@ -6,7 +6,7 @@ VALUES
 ('COMMAND_PREFIX',NULL,'string',NULL,0,1,NULL),
 ('CYTUBE_BOT_PASSWORD',NULL,'string',NULL,1,0,NULL),
 ('DISCORD_BOT_TOKEN',NULL,'string',NULL,1,0,NULL),
-('DEFAULT_BANPHRASE_API_RESPONSE', '[Banphrased]',NULL,0,1,"If a banphrase API rejects a message, and no Banphrase object exists to react to that, this message will be printed instead.")
+('DEFAULT_BANPHRASE_API_RESPONSE', '[Banphrased]',NULL,0,1,"If a banphrase API rejects a message, and no Banphrase object exists to react to that, this message will be printed instead."),
 ('DEFAULT_USER_AGENT','Custom fork of Supibot: github.com/supinic/supibot','string',NULL,0,0,NULL),
 ('LINK_REGEX','/(https?:\\/\\/)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&\\/\\/=]*)/gi','regex',NULL,0,1,'Determines if a message contains a website link.'),
 ('LOG_COMMAND_ENABLED','1','boolean',NULL,0,1,'If true, every successful command execution will be logged into chat_data.Command_Execution.'),
