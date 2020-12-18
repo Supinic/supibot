d1835d2d53b6909ca35f8bb3797959072599e639
Supinic <supinic@protonmail.com>
2020-12-18 19:06:11 +0100

    help-announcement cron posts in other channels
    follows restrictions:
    - channel must be on Twitch
    - must be offline
    - must have supibot VIP'd/modded
    - must have some activity within 1 hour
    - must not have had the announcement sent within last week

--- crons/help-needed/index.js
+++ crons/help-needed/index.js
@@ -5,11 +5,48 @@ module.exports = {
 	Defer: null,
 	Type: "Bot",
 	Code: (async function announceHelpNeeded () {
-		const channelData = sb.Channel.get("supinic", "twitch");
-		await channelData.send(sb.Utils.tag.trim `
-			Supi is currently looking for ideas to put together a FAQ section about Supibot!
-			If you have any ideas, or would like to see something mentioned there, make a new suggestion
-			with the $suggest command, and make sure to mention "FAQ" or the suggestion "5853".
-		`);
+		const threshold = new sb.Date().addMinutes(-60);
+		const eligibleChannels = sb.Channel.data.filter(i => (
+			i.Platform.Name === "twitch"
+			&& ["VIP", "Moderator"].includes(i.Mode)
+			&& i.sessionData
+			&& i.sessionData.live === false
+			&& i.sessionData.lastActivity
+			&& i.sessionData.lastActivity.date >= threshold
+		));
+
+		let channelData;
+		const cacheKey = {
+			action: "help-announcement-sent",
+			type: "faq-ideas"
+		};
+
+		while (!channelData && eligibleChannels.length > 0) {
+			const index = sb.Utils.random(0, eligibleChannels.length);
+			channelData = eligibleChannels[index];
+
+			const announced = await channelData.getCacheData(cacheKey);
+			if (announced) {
+				eligibleChannels.splice(index, 1);
+				channelData = null;
+			}
+			else {
+				await channelData.setCacheData(cacheKey, { sent: sb.Date.now() }, {
+					expiry: 7 * 864e5 // 1 week
+				});
+			}
+		}
+
+		if (!channelData) {
+			console.log("No eligible help announcement targets found", { channelData, eligibleChannels });
+		}
+		else {
+			console.log("Sending help announcement", { channelData, eligibleChannels });
+			await channelData.send(sb.Utils.tag.trim `
+				Supi is currently looking for ideas to put together a FAQ section about Supibot!
+				If you have any ideas, or would like to see something mentioned there, make a new suggestion
+				with the $suggest command, and make sure to mention "FAQ" or the suggestion "5853".
+			`);
+		}
 	})
 };
