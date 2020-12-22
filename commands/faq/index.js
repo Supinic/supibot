9121a918d6b5f25cfcff16b6aad527b95b619309
Supinic <supinic@protonmail.com>
2020-12-22 21:22:59 +0100

    initial implementation

---
+++ commands/faq/index.js
@@ -0,0 +1,16 @@
+module.exports = {
+    Name: "faq",
+    Aliases: [],
+    Author: "supinic",
+    Cooldown: 10000,
+    Description: "Posts the link to Supibot's FAQ on the supinic.com website.",
+    Flags: ["mention","pipe"],
+    Whitelist_Response: null,
+    Static_Data: null,
+    Code: (async function faq () {
+        return {
+            reply: `https://supinic.com/data/faq/list`
+        };
+    }),
+    Dynamic_Description: null
+};
