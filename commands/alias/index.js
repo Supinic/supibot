c2d10024567016e859b92c7ab9f1593ee1c5ea8a
Supinic <supinic@protonmail.com>
2021-01-30 21:23:15 +0100

    escape user-submitted strings
    this is then passed to a site link, so we don't want special URL characters (like &, ?) in there

--- commands/alias/index.js
+++ commands/alias/index.js
@@ -12,22 +12,22 @@ module.exports = {
 			regex: /^[-\w\u00a9\u00ae\u2000-\u3300\ud83c\ud000-\udfff\ud83d\ud000-\udfff\ud83e\ud000-\udfff]{2,25}$/,
 			response: "Your alias should only contain letters, numbers and be 2-25 characters long."
 		},
-	
+
 		applyParameters: (context, aliasArguments, commandArguments) => {
 			const resultArguments = [];
 			const numberRegex = /\${(?<order>\d+)(-(?<range>\d+))?(?<rest>\+?)?}/g;
 			const keywordRegex = /\${(channel|executor)}/g;
-	
+
 			for (const arg of aliasArguments) {
 				if (numberRegex.test(arg)) {
 					let result = arg;
 					const iteratorRegex = new RegExp(numberRegex, "g");
 					const iterator = arg.matchAll(iteratorRegex);
-	
+
 					for (const match of iterator) {
 						const { groups } = match;
 						const item = match[0];
-	
+
 						const order = Number(groups.order);
 						if (!sb.Utils.isValidInteger(order)) {
 							return {
@@ -35,14 +35,14 @@ module.exports = {
 								reply: `Invalid parameter order for argument "${arg}"!`
 							};
 						}
-	
+
 						if (groups.rest && groups.range) {
 							return {
 								success: false,
 								reply: `Can't combine range and rest parameters in argument "${arg}"!`
 							};
 						}
-	
+
 						let replacement = null;
 						if (groups.rest) {
 							replacement = commandArguments.slice(order).join(" ");
@@ -61,16 +61,16 @@ module.exports = {
 									reply: `The end of a range must be greater than the start of it in argument "${arg}"!`
 								};
 							}
-	
+
 							replacement = commandArguments.slice(order, range + 1).join(" ");
 						}
 						else {
 							replacement = commandArguments[order] ?? "";
 						}
-	
+
 						result = result.replace(item, replacement);
 					}
-	
+
 					resultArguments.push(...result.split(" "));
 				}
 				else if (keywordRegex.test(arg)) {
@@ -92,7 +92,7 @@ module.exports = {
 					resultArguments.push(arg);
 				}
 			}
-	
+
 			return {
 				success: true,
 				resultArguments
@@ -104,7 +104,7 @@ module.exports = {
 			args = [type, ...args]; // This the command name
 			type = "run"; // This is the implicit subcommand
 		}
-	
+
 		if (!type) {
 			return {
 				reply: sb.Utils.tag.trim `
@@ -116,16 +116,16 @@ module.exports = {
 				`
 			};
 		}
-	
+
 		if (!context.user.Data.aliasedCommands) {
 			context.user.Data.aliasedCommands = {};
 			await context.user.saveProperty("Data");
 		}
-	
+
 		let changed = false;
 		let reply = "Unexpected reply! Contact @Supinic about this.";
 		const wrapper = new Map(Object.entries(context.user.Data.aliasedCommands));
-	
+
 		type = type.toLowerCase();
 		switch (type) {
 			case "add":
@@ -149,7 +149,7 @@ module.exports = {
 						reply: `Your alias name is not valid! ${this.staticData.nameCheck.response}`
 					};
 				}
-	
+
 				const commandCheck = sb.Command.get(command);
 				if (!commandCheck) {
 					return {
@@ -157,7 +157,7 @@ module.exports = {
 						reply: `Cannot create alias! The command "${command}" does not exist.`
 					};
 				}
-	
+
 				changed = true;
 				wrapper.set(name, {
 					name,
@@ -166,11 +166,11 @@ module.exports = {
 					created: new sb.Date().toJSON(),
 					lastEdit: null
 				});
-	
+
 				reply = `Your alias "${name}" has been created successfully.`;
 				break;
 			}
-	
+
 			case "check": {
 				const [name] = args;
 				if (!name) {
@@ -184,7 +184,7 @@ module.exports = {
 						reply: "You don't have an alias with that name!"
 					};
 				}
-	
+
 				const { invocation, args: commandArgs } = wrapper.get(name);
 				return {
 					reply: `Your alias "${name}" has this definition: ${invocation} ${commandArgs.join(" ")}`
@@ -252,7 +252,7 @@ module.exports = {
 						reply: `You don't have an alias with this name!`
 					};
 				}
-	
+
 				const commandCheck = sb.Command.get(command);
 				if (!commandCheck) {
 					return {
@@ -260,18 +260,18 @@ module.exports = {
 						reply: `Cannot edit alias! The command "${command}" does not exist.`
 					};
 				}
-	
+
 				const obj = wrapper.get(name);
 				obj.invocation = command;
 				obj.args = rest;
 				obj.lastEdit = new sb.Date().toJSON();
-	
+
 				changed = true;
 				reply = `Your alias "${name}" has been successfully edited.`;
-	
+
 				break;
 			}
-	
+
 			case "list": {
 				return {
 					reply: `Check your aliases here: https://supinic.com/bot/user/${context.user.Name}/alias/list`
@@ -293,14 +293,14 @@ module.exports = {
 						reply: "You don't have an alias with that name!"
 					};
 				}
-	
+
 				changed = true;
 				wrapper.delete(name);
 				reply = `Your alias "${name}" has been succesfully removed.`;
-	
+
 				break;
 			}
-	
+
 			case "rename": {
 				const [oldName, newName] = args;
 				if (!oldName || !newName) {
@@ -315,18 +315,18 @@ module.exports = {
 						reply: "Can't rename - you don't have an alias with that name!"
 					};
 				}
-	
+
 				changed = true;
 				wrapper.set(newName, wrapper.get(oldName));
 				wrapper.get(newName).lastEdit = new sb.Date().toJSON();
 				wrapper.get(newName).name = newName;
 				wrapper.delete(oldName);
-	
+
 				reply = `Your alias "${oldName}" has been succesfully renamed to "${newName}".`;
-	
+
 				break;
 			}
-	
+
 			case "run": {
 				const [name] = args;
 				if (!name) {
@@ -341,13 +341,13 @@ module.exports = {
 						reply: "You don't have an alias with that name!"
 					};
 				}
-	
+
 				const { invocation, args: aliasArguments } = wrapper.get(name);
 				const { success, reply, resultArguments } = this.staticData.applyParameters(context, aliasArguments, args.slice(1));
 				if (!success) {
 					return { success, reply };
 				}
-	
+
 				const commandData = sb.Command.get(invocation);
 				if (context.append.pipe && !commandData.Flags.pipe) {
 					return {
@@ -355,7 +355,7 @@ module.exports = {
 						reply: `Cannot use command ${invocation} inside of a pipe, despite being wrapped in an alias!`
 					};
 				}
-	
+
 				const aliasCount = (context.append.aliasCount ?? 0) + 1;
 				if (aliasCount > this.staticData.aliasLimit) {
 					return {
@@ -367,7 +367,7 @@ module.exports = {
 						`
 					};
 				}
-	
+
 				const result = await sb.Command.checkAndExecute(
 					invocation,
 					resultArguments,
@@ -383,13 +383,13 @@ module.exports = {
 						partialExecute: true
 					}
 				);
-	
+
 				return {
 					...result,
 					aliased: true
 				};
 			}
-	
+
 			case "spy": {
 				const [targetUser, targetAlias] = args;
 				if (!targetUser) {
@@ -398,7 +398,7 @@ module.exports = {
 						reply: "No target user provided!"
 					};
 				}
-	
+
 				const target = await sb.User.get(targetUser);
 				if (!target) {
 					return {
@@ -408,14 +408,14 @@ module.exports = {
 				}
 
 				const suffix = (targetAlias)
-					? `?columnName=${targetAlias}`
+					? `?columnName=${encodeURIComponent(targetAlias)}`
 					: "";
 
 				return {
-					reply: `Check their aliases here: https://supinic.com/bot/user/${target.Name}/alias/list${suffix}`
+					reply: `Check their aliases here: https://supinic.com/bot/user/${encodeURIComponent(target.Name)}/alias/list${suffix}`
 				};
 			}
-	
+
 			default: return {
 				success: false,
 				reply: sb.Utils.tag.trim `
@@ -425,12 +425,12 @@ module.exports = {
 				`
 			}
 		}
-	
+
 		if (changed) {
 			context.user.Data.aliasedCommands = Object.fromEntries(wrapper);
 			await context.user.saveProperty("Data");
 		}
-	
+
 		return { reply };
 	}),
 	Dynamic_Description: (async (prefix) => {
@@ -438,24 +438,24 @@ module.exports = {
 			"Meta-command that lets you create aliases (or shorthands) for existing commands or their combinations.",
 			"You have to first create an alias, and then run it. You can manage your aliases by listing, checking, removing and adding.",
 			"",
-	
+
 			`<h5>What's an alias?</h5>`,
 			`An alias is a word that lets you quickly use a command without typing the entirity of it.`,
 			`E.g.: You don't want to type <code>${prefix}weather New York, USA</code> every time, so you create an alias called <code>ny</code>.`,
 			`Then, you can simply use the alias like so: <code>${prefix}$ ny</code>`,
 			"",
-			
+
 			`<h5>Usage</h5>`,
 			`<code>${prefix}alias add (name) (definition)</code>`,
 			`<code>${prefix}alias create (name) (definition)</code>`,
 			`Creates your command alias, e.g.:`,
 			`<code>${prefix}alias add <u>hello</u> translate to:german Hello!</code>`,
 			"",
-			
+
 			`<code>${prefix}$ (name)</code>`,
 			`<code>${prefix}alias run (name)</code>`,
 			"Runs your command alias!, e.g.:",
-			
+
 			`<code>${prefix}$ <u>hello</u></code> => Hallo!`,
 			`<code>${prefix}alias run <u>hello</u></code> => Hallo!`,
 			"",
@@ -464,39 +464,39 @@ module.exports = {
 			"Takes someone else's alias, and attempts to copy it with the same name for you. Behaves like <code>add</code>, so if you have an alias with the same name, the copying will fail",
 			`<code>${prefix}alias copy supinic lol</code>`,
 			"",
-			
+
 			`<code>${prefix}alias check (name)</code>`,
 			"Posts the definition of given alias to chat, e.g.:",
 			`<code>${prefix}alias check <u>hello</u></code> => "translate to:german Hello!"`,
 			"",
-	
+
 			`<code>${prefix}alias edit (name)</code>`,
 			"Edits an existing alias, without the need of removing and re-adding it.",
 			`<code>${prefix}alias edit <u>hello</u></code> => "translate to:italian Hello!"`,
 			"",
-	
+
 			`<code>${prefix}alias rename (old-name) (new-name)</code>`,
 			"Renames your command alias from old-name to new-name.",
 			"",
-	
+
 			`<code>${prefix}alias list</code>`,
 			"Lists all your currently active aliases.",
 			`You can also check them in <a href="/user/alias/list">this list</a> - after you log in.`,
 			"",
-			
+
 			`<code>${prefix}alias delete (name)</code>`,
 			`<code>${prefix}alias remove (name)</code>`,
 			"Removes your command alias with the given name.",
 			"",
-	
+
 			`<code>${prefix}alias spy (user)</code>`,
 			"Lists all active aliases the target person currently has.",
 			"",
-	
+
 			`<code>${prefix}alias spy (user) (name)</code>`,
 			"Posts the definition of the alias with given name, that belongs to given person.",
-			"",		
-	
+			"",
+
 			"<h5>Replacements</h5>",
 			"Replaces a symbol in your alias with a value depending on its name.",
 			`<ul>
