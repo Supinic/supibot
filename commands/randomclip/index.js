541795ad515c0fd9215e43fc919f5755f663d25c
Supinic <supinic@protonmail.com>
2021-01-30 20:47:16 +0100

    refactor - useParams + linkOnly

--- commands/randomclip/index.js
+++ commands/randomclip/index.js
@@ -4,7 +4,7 @@ module.exports = {
 	Author: "supinic",
 	Cooldown: 30000,
 	Description: "Posts a random clip from either the current channel or the specified channel. You can specify a parameter period, with options day/week/month/all, for example: period:week",
-	Flags: ["mention","non-nullable","pipe"],
+	Flags: ["link-only","mention","non-nullable","pipe","use-params"],
 	Whitelist_Response: null,
 	Static_Data: null,
 	Code: (async function randomClip (context, ...args) {
@@ -15,50 +15,48 @@ module.exports = {
 	
 		if (!channel && (context.privateMessage || context.platform.Name !== "twitch")) {
 			return {
+				success: false,
 				reply: "You must specify the target channel if you're in PMs or not on Twitch!"
 			};
 		}
 	
-		let period = "all";
-		const allowedPeriods = ["day", "week", "month", "all"];
-		const regex = new RegExp("period:(" + allowedPeriods.join("|") + ")", "i");
-		for (const token of args) {
-			const match = token.match(regex);
-			if (match) {
-				period = match[1].toLowerCase();
-			}
-		}
-	
-		try {
-			const data = await sb.Got("Kraken", {
-				url: "clips/top",
-				searchParams: new sb.URLParams()
-					.set("period", period)
-					.set("channel", channel || context.channel.Name)
-					.set("limit", "100")
-					.toString()
-			}).json();
-			
-			if (data.clips.length === 0) {
-				return { reply: "No clips found!" };
-			}
-	
-			const clip = sb.Utils.randArray(data.clips);
-			const delta = sb.Utils.timeDelta(new sb.Date(clip.created_at));
-			const url = "https://clips.twitch.tv/" + clip.slug;
-	
+		const period = context.params.period ?? "all";
+		if (!["day", "week", "month", "all"].includes(period))  {
 			return {
-				reply: `${clip.title} - ${clip.duration} sec, clipped by ${clip.curator.name}, ${delta}: ${url}`
+				success: false,
+				reply: `Invalid clip creation period! Use one of: day, week, month, all`
 			};
 		}
-		catch (e) {
-			if (e.statusCode === 404) {
-				return { reply: "That user does not exist" };
-			}
-	
-			console.error(e);
-			return { reply: "Something else messed up!" };
+
+		const { statusCode, body: data } = await sb.Got("Kraken", {
+			url: "clips/top",
+			searchParams: new sb.URLParams()
+				.set("period", period)
+				.set("channel", channel || context.channel.Name)
+				.set("limit", "100")
+				.toString(),
+			throwHttpErrors: false
+		});
+
+		if (statusCode === 404) {
+			return {
+				success: false,
+				reply: "That user does not exist!"
+			};
 		}
+		else if (data.clips.length === 0) {
+			return {
+				reply: "No clips found!"
+			};
+		}
+
+		const clip = sb.Utils.randArray(data.clips);
+		const delta = sb.Utils.timeDelta(new sb.Date(clip.created_at));
+		const link = "https://clips.twitch.tv/" + clip.slug;
+		return {
+			reply: `${clip.title} - ${clip.duration} sec, clipped by ${clip.curator.name}, ${delta}: ${link}`,
+			link
+		};
 	}),
 	Dynamic_Description: null
 };
