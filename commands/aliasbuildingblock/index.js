fcf608fd7b76b61e15c6dcc3750aff4699b158a1
Supinic <supinic@protonmail.com>
2021-02-19 17:30:38 +0100

    enforce a large cooldown on abb chatter

--- commands/aliasbuildingblock/index.js
+++ commands/aliasbuildingblock/index.js
@@ -110,6 +110,12 @@ module.exports = {
                     }
 
                     return {
+                        cooldown: {
+                            user: context.user.ID,
+                            channel: null,
+                            command: this.ID,
+                            length: 60_000
+                        },
                         reply: sb.Utils.randArray(users)
                     };
                 }
