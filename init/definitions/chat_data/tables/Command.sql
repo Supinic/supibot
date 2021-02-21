4843488b0ea8b3238fdbc691c3cce58113bdf54e
Supinic <supinic@protonmail.com>
2021-02-21 14:26:51 +0100

    add Command.Params definition to SQL init script

--- init/definitions/chat_data/tables/Command.sql
+++ init/definitions/chat_data/tables/Command.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS `chat_data`.`Command` (
 	`Name` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
 	`Aliases` TEXT(65535) NULL DEFAULT NULL COMMENT 'JSON array of strings that will be this command\'s aliases.' COLLATE 'utf8mb4_general_ci',
 	`Flags` SET('archived','block','developer','link-only','mention','opt-out','owner-override','ping','pipe','read-only','rollback','skip-banphrase','system','use-params','whitelist') NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+    `Params` TEXT NULL DEFAULT NULL COMMENT 'JSON object with the definition of the command\'s parameters.' COLLATE 'utf8mb4_general_ci',
 	`Description` VARCHAR(300) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
 	`Cooldown` INT(11) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'Command cooldown given in milliseconds',
 	`Whitelist_Response` VARCHAR(300) NULL DEFAULT NULL COMMENT 'If the command is Whietlisted, this is the reply that will be sent to non-whitelisted users trying to invoke it.' COLLATE 'utf8mb4_general_ci',
