e5f6795d4ad5a804157b6c5206aef910227085b6
Supinic <supinic@protonmail.com>
2021-02-26 17:45:53 +0100

    initial implementation
    resolves S#6828

---
+++ commands/randomemoji/index.js
@@ -0,0 +1,46 @@
+module.exports = {
+	Name: "randomemoji",
+	Aliases: ["re"],
+	Author: "supinic",
+	Cooldown: 5000,
+	Description: "Fetches a random emoji. If a number is provided, rolls that many emojis.",
+	Flags: ["pipe"],
+	Params: null,
+	Whitelist_Response: null,
+	Static_Data: (() => ({
+		limit: 10
+	})),
+	Code: (async function randomEmoji (context, number = 1) {
+		const repeats = Number(number);
+		if (!repeats || repeats > this.staticData.limit || repeats < 1 || Math.trunc(repeats) !== repeats) {
+			return {
+				reply: "Invalid or too high amount of emojis!"
+			};
+		}
+
+		const emojis = sb.Config.get("EMOJI_LIST").split(" ");
+		return {
+			reply: [...Array(repeats)].map(() => sb.Utils.randArray(emojis)).join(" ")
+		};
+	}),
+	Dynamic_Description: (async (prefix, values) => {
+		const { limit } = values.getStaticData();
+		const list = sb.Config.get("EMOJI_LIST").split(" ");
+	
+		return [
+			"Returns a random word from a list of " + list.length + " pre-determined emojis.",
+			`Maximum amount of words: ${limit}`,
+			"",
+			
+			`<code>${prefix}re</code>`,
+			"(one random emoji)",
+			"",
+	
+			`<code>${prefix}re 10</code>`,
+			"(ten random emojis)",
+			"",
+	
+			"Emoji list: <br>" + list.join("<br>")
+		];
+	})
+};
