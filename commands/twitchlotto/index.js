138fb9c896e67f58b60a94502b389fc17beebc18
Supinic <supinic@protonmail.com>
2021-03-05 18:21:51 +0100

    added the plural form of exludeChannel param

--- commands/twitchlotto/index.js
+++ commands/twitchlotto/index.js
@@ -6,7 +6,8 @@ module.exports = {
 	Description: "Fetches a random Imgur image from a Twitch channel (based off Twitchlotto) and checks it for NSFW stuff via an AI. The \"nudity score\" is posted along with the link.",
 	Flags: ["mention","whitelist"],
 	Params: [
-		{ name: "excludeChannel", type: "string" }
+		{ name: "excludeChannel", type: "string" },
+		{ name: "excludeChannels", type: "string" }
 	],
 	Whitelist_Response: "This command can't be executed here!",
 	Static_Data: (() => {
