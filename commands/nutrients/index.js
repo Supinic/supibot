1f7e4297318667570867d2c175edbb7391d01480
Supinic <supinic@protonmail.com>
2021-03-05 18:14:46 +0100

    refactoring out legacy stuff
    - now uses success: false
    - always prepends "100g of" if not numeric values are provided
    - changed the output creation to use the tag.trim method instead of array dankness

--- commands/nutrients/index.js
+++ commands/nutrients/index.js
@@ -10,9 +10,17 @@ module.exports = {
 	Static_Data: null,
 	Code: (async function nutrients (context, ...args) {
 		if (args.length === 0) {
-			return { reply: "No food provided!" };
+			return {
+				success: false,
+				reply: "No food provided!"
+			};
 		}
-	
+
+		let query = args.join(" ");
+		if (!query.test(/\d/)) {
+			query = `100g of ${query}`;
+		}
+
 		const data = await sb.Got({
 			method: "POST",
 			url: "https://trackapi.nutritionix.com/v2/natural/nutrients",
@@ -21,32 +29,34 @@ module.exports = {
 				"x-app-key": sb.Config.get("API_NUTRITIONIX"),
 				"x-remote-user-id": 0
 			},
-			json: {
-				query: args.join(" ")
-			},
+			json: { query },
 			throwHttpErrors: false
 		}).json();
 	
 		if (data.message) {
-			return { reply: data.message };
+			return {
+				success: false,
+				reply: data.message
+			};
 		}
 		else if (data.foods.length > 1) {
-			return { reply: "Only one food is supported at a time (for now)" };
+			return {
+				success: false,
+				reply: "Only one food is supported at a time (for now)"
+			};
 		}
-	
+
 		const food = data.foods[0];
 		return {
-			reply: [
-				food.serving_qty,
-				food.serving_unit,
-				food.food_name,
-				"(" + food.serving_weight_grams + "g)",
-				"contains",
-				food.nf_calories + " kcal,",
-				food.nf_total_fat + "g of fat (" + (food.nf_saturated_fat || 0) + "g saturated),",
-				food.nf_total_carbohydrate + "g of carbohydrates (" + (food.nf_sugars || 0) + "g sugar),",
-				food.nf_protein + "g protein"
-			].join(" ")
+			reply: sb.Utils.tag.trim `
+				${food.serving_qty} ${food.serving_unit} ${food.food_name}
+				(${food.serving_weight_grams}g)
+				contains
+				${food.nf_calories} kcal,
+				${food.nf_total_fat}g of fat (${food.nf_saturated_fat ?? 0}g saturated),
+				${food.nf_total_carbohydrate}g of carbohydrates (${food.nf_sugars ?? 0}g sugar),
+				${food.nf_protein}g protein
+			`
 		};
 	}),
 	Dynamic_Description: null
