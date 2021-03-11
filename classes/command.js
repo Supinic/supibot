6db9ca43cedfb2d5a01636744981dc2c44c31886
Supinic <supinic@protonmail.com>
2021-03-11 20:05:54 +0100

    support for object-type parameters

--- classes/command.js
+++ classes/command.js
@@ -504,7 +504,22 @@ module.exports = class Command extends require("./template.js") {
 						};
 					}
 
-					context.params[name] = parsedValue;
+					if (type === "object") {
+						if (typeof context.params[name] === "undefined") {
+							context.params[name] = {};
+						}
+						else if (typeof context.params[name][parsedValue.key]) {
+							return {
+								success: false,
+								reply: `Cannot use multiple values for parameter "${name}", key ${parsedValue.key}!`
+							};
+						}
+
+						context.params[name][parsedValue.key] = parsedValue.value;
+					}
+					else {
+						context.params[name] = parsedValue;
+					}
 				}
 			}
 
@@ -891,6 +906,10 @@ module.exports = class Command extends require("./template.js") {
 		}
 		else if (type === "date") {
 			return new sb.Date(value);
+		}
+		else if (type === "object") {
+			const [key, outputValue] = value.split("=");
+			return { key, value: outputValue };
 		}
 		else {
 			return null;
