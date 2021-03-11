de4bc7f63449f7c4e8a6551ff20df4883be9dce7
Supinic <supinic@protonmail.com>
2021-03-11 21:17:20 +0100

    access video URI

--- commands/speedrun/index.js
+++ commands/speedrun/index.js
@@ -146,7 +146,7 @@ module.exports = {
 			runner = runnerData.data;
 		}
 
-		const link = run.videos?.links[0] ?? run.weblink;
+		const link = run.videos?.links[0]?.uri ?? run.weblink;
 		const date = new sb.Date(run.date).format("Y-m-d");
 		const time = sb.Utils.formatTime(run.times.primary_t);
 		return {
