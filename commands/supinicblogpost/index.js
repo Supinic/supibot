146d7b23020d325d62e1a9637e3c8ebc8c2a7118
Supinic <supinic@protonmail.com>
2021-03-17 17:45:20 +0100

    changed param to just "text"
    added caching
    added more benevolent paste expiry (2 weeks)

--- commands/supinicblogpost/index.js
+++ commands/supinicblogpost/index.js
@@ -4,15 +4,13 @@ module.exports = {
 	Author: "supinic",
 	Cooldown: 10000,
 	Description: "Posts the timestamp and link of the latest blogpost Supinic keeps up on his Discord.",
-	Flags: ["mention","use-params"],
-	Params: [
-		{ name: "fullResponse", type: "boolean" }
-	],
+	Flags: ["mention"],
+	Params: null,
 	Whitelist_Response: null,
 	Static_Data: (() => ({
 		channel: "748955843415900280"
 	})),
-	Code: (async function supinicBlogPost (context) {
+	Code: (async function supinicBlogPost (context, type) {
 		const client = sb.Platform.get("discord").client;
 		const channel = await client.channels.fetch(this.staticData.channel);
 		const messageID = channel.lastMessageID;
@@ -25,12 +23,25 @@ module.exports = {
 
 		const messageData = await channel.messages.fetch(messageID);
 		const date = new sb.Date(messageData.createdTimestamp);
-		if (context.params.fullResponse) {
+		if (type === "text") {
+			const key = { messageID };
+			const cacheData = await this.getCacheData(key);
+			if (cacheData) {
+				return {
+					reply: cacheData
+				};
+			}
+
 			const text = messageData.content.replace(/<@&\d+>\s*/g, "");
 			const paste = await sb.Pastebin.post(text, {
 				name: "Supibot update post " + date.format("Y-m-d"),
-				format: "markdown"
+				format: "markdown",
+				expiration: "2 weeks"
 			});
+
+			await this.setCacheData(key, paste, {
+				expiration: 14 * 864e5
+			})
 
 			return {
 				reply: paste
@@ -53,7 +64,7 @@ module.exports = {
 			"Posts the time of posting, and the direct link to the message on Discord.",
 			"",
 
-			`<code>${prefix}sbp fullResponse:true</code>`,
+			`<code>${prefix}sbp text</code>`,
 			"Posts a Pastebin link to the full message."
 		]
 	})
