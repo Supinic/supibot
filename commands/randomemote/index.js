f7dc2a921fe3a862be15e12a6a05e303180d5dcd
Supinic <supinic@protonmail.com>
2021-03-25 19:11:33 +0100

    use "name" instead of "token"

--- commands/randomemote/index.js
+++ commands/randomemote/index.js
@@ -59,7 +59,7 @@ module.exports = {
 		});
 
 		return {
-			reply: [...Array(repeats)].map(() => sb.Utils.randArray(emotes).token).join(" ")
+			reply: [...Array(repeats)].map(() => sb.Utils.randArray(emotes).name).join(" ")
 		};
 	}),
 	Dynamic_Description: (async (prefix, values) => {
