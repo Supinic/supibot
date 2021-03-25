ca01fcc6d0b0b36352dc9d9131a634bc1f847daf
Supinic <supinic@protonmail.com>
2021-03-25 18:25:48 +0100

    adding fail: true param to fail the command
    fail = responding with success: false on "no" and "edge" coinflip results

--- commands/coinflip/index.js
+++ commands/coinflip/index.js
@@ -4,8 +4,10 @@ module.exports = {
 	Author: "supinic",
 	Cooldown: 2500,
 	Description: "Flips a coin.",
-	Flags: ["mention","pipe","skip-banphrase"],
-	Params: null,
+	Flags: ["mention","pipe","skip-banphrase","use-params"],
+	Params: [
+		{ name: "fail", type: "boolean" }
+	],
 	Whitelist_Response: null,
 	Static_Data: null,
 	Code: (async function coinflip () {
@@ -13,8 +15,15 @@ module.exports = {
 		const number =  sb.Utils.random(1, 6000);
 		const flipResult = (number === 3000) ? null : Boolean(number < 3000);
 		const replyMap = { true: "Heads! (yes)", false: "Tails! (no)", null: "The coin landed on its edge!!" };
-	
+
+		// if fail:true, then fail the command with success: false on the "no" and "edge" results
+		let success = true;
+		if (context.params.fail === true) {
+			success = Boolean(flipResult);
+		}
+
 		return {
+			success,
 			reply: replyMap[flipResult]
 		};
 	}),
