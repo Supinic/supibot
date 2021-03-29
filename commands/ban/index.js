41a4cfe065900ab803595624c1f0d968e4016bb1
Supinic <supinic@protonmail.com>
2021-03-29 18:43:33 +0200

    refactored to use params instead of parsing input
    also: added support for Filter.Invocation

--- commands/ban/index.js
+++ commands/ban/index.js
@@ -4,92 +4,122 @@ module.exports = {
 	Author: "supinic",
 	Cooldown: 5000,
 	Description: "Bans/unbans any combination of channel, user, and command from being executed. Only usable by administrators, or Twitch channel owners.",
-	Flags: ["mention","system"],
-	Params: null,
+	Flags: ["mention","system","use-params"],
+	Params: [
+        { name: "channel", type: "string" },
+        { name: "command", type: "string" },
+        { name: "invocation", type: "string" },
+        { name: "user", type: "string" },
+
+    ],
 	Whitelist_Response: null,
 	Static_Data: null,
-	Code: (async function ban (context, ...args) {
+	Code: (async function ban (context) {
 		// if (context.platform.Name !== "twitch") {
 		// 	return {
 		// 		success: false,
 		// 		reply: "Not available outside of Twitch!"
 		// 	};
 		// }
-	
+
 		const { invocation } = context;
 		const options = {
 			Channel: null,
 			User_Alias: null,
 			Command: null,
+			Invocation: null,
 			Type: "Blacklist",
 			Response: "None",
 			Reason: null,
 			Issued_By: context.user.ID
 		};
-	
-		for (let i = 0; i < args.length; i++) {
-			const token = args[i];
-			const value = token.split(":")[1];
-	
-			if (token.includes("channel:")) {
-				const channelData = sb.Channel.get(value);
-				if (!channelData) {
-					return {
-						success: false,
-						reply: "Channel was not found!"
-					};
-				}
-	
-				options.Channel = channelData.ID;
-			}
-			else if (token.includes("command:")) {
-				const commandData = sb.Command.get(value);
-				if (!commandData) {
-					return {
-						success: false,
-						reply: "Command does not exist!"
-					};
-				}
-				else if (commandData === this) {
-					return {
-						success: false,
-						reply: `You can't ${invocation} the ${value} command!`
-					};
-				}
-	
-				options.Command = commandData.ID;
-			}
-			else if (token.includes("user:")) {
-				const userData = await sb.User.get(value);
-				if (!userData) {
-					return {
-						success: false,
-						reply: "User was not found!"
-					};
-				}
-				else if (userData === context.user) {
-					return {
-						success: false,
-						reply: `You can't ${invocation} yourself!`
-					};
-				}
-	
-				options.User_Alias = userData.ID;
-			}
-		}
-	
-		/** @type {string|null} */
+
+		if (context.params.channel) {
+            const channelData = sb.Channel.get(context.params.channel);
+            if (!channelData) {
+                return {
+                    success: false,
+                    reply: "Channel was not found!"
+                };
+            }
+
+            options.Channel = channelData.ID;
+        }
+        if (context.params.command) {
+            const commandData = sb.Command.get(context.params.command);
+            if (!commandData) {
+                return {
+                    success: false,
+                    reply: "Command does not exist!"
+                };
+            }
+            else if (commandData === this) {
+                const emote = await context.platform.getBestAvailableEmote(context.channel, ["PepeLaugh", "pepeLaugh", "4Head"], "😅");
+                return {
+                    success: false,
+                    reply: `You can't ${invocation} the ${commandData.Name} command! ${emote}`
+                };
+            }
+
+            options.Command = commandData.ID;
+        }
+        if (context.params.invocation) {
+            const commandData = sb.Command.get(context.params.invocation);
+            if (!commandData) {
+                return {
+                    success: false,
+                    reply: "No command found for given invocation!"
+                };
+            }
+            else if (commandData === this) {
+                const emote = await context.platform.getBestAvailableEmote(context.channel, ["PepeLaugh", "pepeLaugh", "4Head"], "😅");
+                return {
+                    success: false,
+                    reply: `You can't ${invocation} the ${commandData.Name} command's invocation! ${emote}`
+                };
+            }
+
+            if (!options.Command) {
+                options.Command = commandData.ID;
+            }
+            else if (options.Command !== commandData.ID) {
+                return {
+                    success: false,
+                    reply: "Invalid command + invocation provided! Either keep command: empty, or use the command that belongs to the provided invocation"
+                };
+            }
+
+            options.Invocation = context.params.invocation;
+        }
+        if (context.params.user) {
+            const userData = await sb.User.get(context.params.user);
+            if (!userData) {
+                return {
+                    success: false,
+                    reply: "User was not found!"
+                };
+            }
+            else if (userData === context.user) {
+                return {
+                    success: false,
+                    reply: `You can't ${invocation} yourself!`
+                };
+            }
+
+            options.User_Alias = userData.ID;
+        }
+
 		let level = null;
 		if (context.user.Data.administrator) {
 			level = "administrator";
 		}
 		else if (
-			context.channel 
+			context.channel
 			&& (context.channel.isUserChannelOwner(context.user) || context.channel.isUserAmbassador(context.user))
 		) {
 			level = "channel-owner";
 		}
-	
+
 		if (!options.Channel) {
 			if (level === "administrator") {
 				// OK.
@@ -116,7 +146,7 @@ module.exports = {
 				return { success: false, reply: "Can't do that for provided channel" }
 			}
 		}
-	
+
 		// if a channel owner doesn't provide a user or a command, then fail
 		// if an admin doesn't provide user, command or a channel, then fail
 		if (
@@ -128,19 +158,20 @@ module.exports = {
 				reply: "Not enough data provided to create a ban!"
 			};
 		}
-	
+
 		if (level === "channel-owner") {
 			options.Response = "Reason";
 			options.Reason = "Banned by channel owner.";
 		}
-	
+
 		const existing = sb.Filter.data.find(i =>
 			i.Type === "Blacklist"
 			&& i.Channel === options.Channel
 			&& i.Command === options.Command
+			&& i.Invocation === options.Invocation
 			&& i.User_Alias === options.User_Alias
 		);
-	
+
 		if (existing) {
 			if (existing.Issued_By !== context.user.ID && level !== "administrator") {
 				return {
@@ -154,9 +185,9 @@ module.exports = {
 					reply: `That combination is already ${invocation}ned!`
 				};
 			}
-	
+
 			await existing.toggle();
-	
+
 			const [prefix, suffix] = (existing.Active) ? ["", " again"] : ["un", ""];
 			return {
 				reply: `Succesfully ${prefix}banned${suffix}.`
@@ -169,7 +200,7 @@ module.exports = {
 					reply: "This combination has not been banned yet, so it cannot be unbanned!"
 				};
 			}
-	
+
 			const ban = await sb.Filter.create(options);
 			return {
 				reply: `Succesfully banned (ID ${ban.ID})`
@@ -182,30 +213,30 @@ module.exports = {
 			"Only usable by admins or Twitch channel owners. Channel owners can only ban combinations in their channel.",
 			"All following examples assume the command is executed by a channel owner.",
 			"",
-	
+
 			`<code>${prefix}ban user:test command:remind</code>`,
 			"Bans user <u>test</u> from executing the command <u>remind</u> in the current channel.",
 			"",
-	
+
 			`<code>${prefix}ban command:remind</code>`,
 			"Bans <u>everyone</u> from executing the command <u>remind</u> in the current channel.",
 			"",
-	
+
 			`<code>${prefix}ban user:test</code>`,
 			"Bans user <u>test</u> from executing <u>any</u> commands in the current channel.",
 			"",
-	
+
 			"---",
 			"",
-	
+
 			`<code>${prefix}unban user:test command:remind</code>`,
 			"If banned before, user <u>test</u> will be unbanned from executing the command <u>remind</u> in the current channel.",
 			"",
-	
+
 			`<code>${prefix}unban command:remind</code>`,
 			"If banned before, <u>everyone</u> will be unbanned from executing the command <u>remind</u> in the current channel.",
 			"",
-	
+
 			`<code>${prefix}unban user:test</code>`,
 			"If banned before, user <u>test</u> will be unbanned from executing <u>any</u> commands in the current channel."
 		];
