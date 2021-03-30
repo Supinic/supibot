42fbeff9874c700913b43523554bbc78dc728145
Supinic <supinic@protonmail.com>
2021-03-30 19:40:39 +0200

    updating the description to mention new types

--- commands/ban/index.js
+++ commands/ban/index.js
@@ -192,14 +192,21 @@ module.exports = {
 			};
 		}
 	}),
-	Dynamic_Description: (async (prefix) => {
+	Dynamic_Description: (async (prefix, values) => {
+		const { availableTypes } = values.geStaticData();
+
 		return [
 			"Bans or unbans any combination of user/channel/command.",
 			"Only usable by admins or Twitch channel owners. Channel owners can only ban combinations in their channel.",
 			"All following examples assume the command is executed by a channel owner.",
 			"",
 
+			"Available types: <code>" + availableTypes.join(" ") + "</code>",
+			`You can change the type. The default type is <code>Blacklist</code>.`,
+			"",
+
 			`<code>${prefix}ban user:test command:remind</code>`,
+			`<code>${prefix}ban user:test command:remind type:blacklist</code>`,
 			"Bans user <u>test</u> from executing the command <u>remind</u> in the current channel.",
 			"",
 
@@ -207,23 +214,32 @@ module.exports = {
 			"Bans <u>everyone</u> from executing the command <u>remind</u> in the current channel.",
 			"",
 
+			`<code>${prefix}ban invocation:rq</code>`,
+			"Bans <u>everyone</u> from executing the command invocation <u>rq</u> (but! not rl, or randomline) in the current channel.",
+			"",
+
 			`<code>${prefix}ban user:test</code>`,
 			"Bans user <u>test</u> from executing <u>any</u> commands in the current channel.",
+			"",
+
+			`<code>${prefix}ban type:offline-only (...)</code>`,
+			"For any previously mentioned combination, the result will be that the command will only be available while the channel is live.",
+			"You can still use any combination of invocation/command/user/channel to be specific.",
+			"",
+
+			`<code>${prefix}ban type:online-only (...)</code>`,
+			"Just like <code>offline-only</code>, but reverse - result will be available only in online channels.",
 			"",
 
 			"---",
 			"",
 
-			`<code>${prefix}unban user:test command:remind</code>`,
-			"If banned before, user <u>test</u> will be unbanned from executing the command <u>remind</u> in the current channel.",
-			"",
-
-			`<code>${prefix}unban command:remind</code>`,
-			"If banned before, <u>everyone</u> will be unbanned from executing the command <u>remind</u> in the current channel.",
-			"",
-
-			`<code>${prefix}unban user:test</code>`,
-			"If banned before, user <u>test</u> will be unbanned from executing <u>any</u> commands in the current channel."
+			`<code>${prefix}unban (...)</code>`,
+			`<code>${prefix}unban type:blacklist (...)</code>`,
+			`<code>${prefix}unban type:offline-only (...)</code>`,
+			`<code>${prefix}unban type:online-only (...)</code>`,
+			"Unbans any previously mentioned combination.",
+			"Make sure to use the correct type - <code>Blacklist</code> is again default."
 		];
 	})
 };
