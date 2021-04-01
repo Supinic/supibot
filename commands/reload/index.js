67492cc359340a0f9d047288c2866b59ae059f0e
Supinic <supinic@protonmail.com>
2021-04-01 18:33:07 +0200

    refactor - add reloadSpecific where availabler

--- commands/reload/index.js
+++ commands/reload/index.js
@@ -9,42 +9,56 @@ module.exports = {
 	Whitelist_Response: null,
 	Static_Data: null,
 	Code: (async function reload (context, target, ...rest) {
+		let result = null;
+
 		switch (target) {
 			case "afks": await sb.AwayFromKeyboard.reloadData(); break;
-	
-			case "bans":
+			case "afk": result  = await sb.AwayFromKeyboard.reloadData(); break;
+
 			case "filters": await sb.Filter.reloadData(); break;
-	
+			case "filter": result = await sb.Filter.reloadSpecific(...rest); break;
+
 			case "banphrases": await sb.Banphrase.reloadData(); break;
-	
+			case "banphrase": result = await sb.Banphrase.reloadSpecific(...rest); break;
+
 			case "channels": await sb.Channel.reloadData(); break;
-			case "channel": await sb.Channel.reloadSpecific(...rest); break;
+			case "channel": result = await sb.Channel.reloadSpecific(...rest); break;
 
 			case "chat-modules":
 			case "chatmodules": await sb.ChatModule.reloadData(); break;
 			case "chat-module":
-			case "chatmodule": await sb.ChatModule.reloadSpecific(...rest); break;
+			case "chatmodule": result = await sb.ChatModule.reloadSpecific(...rest); break;
 
 			case "commands": await sb.Command.reloadData(); break;
-			case "command": await sb.Command.reloadSpecific(...rest); break;
+			case "command": result = await sb.Command.reloadSpecific(...rest); break;
 
 			case "config": await sb.Config.reloadData(); break;
 	
-			case "cron": await sb.Cron.reloadData(); break;
-	
-			case "extranews": await sb.ExtraNews.reloadData(); break;
+			case "crons": await sb.Cron.reloadData(); break;
+			case "cron": result = await sb.Cron.reloadSpecific(...rest); break;
 	
 			case "got": await sb.Got.reloadData(); break;
 	
 			case "reminders": await sb.Reminder.reloadData(); break;
+			case "reminder": result = await sb.Reminder.reloadSpecific(...rest); break;
 	
 			case "users": await sb.User.reloadData(); break;
 	
-			default: return { reply: "Unrecognized module!" };
+			default: return {
+				success: false,
+				reply: "Unrecognized module!"
+			};
 		}
-	
+
+		if (result === false) {
+			return {
+				success: false,
+				reply: `No ${target}s reloaded!`
+			};
+		}
+
 		return {
-			reply: "Reloaded successfully."
+			reply: `Successfully reloaded ${target}.`
 		};
 	}),
 	Dynamic_Description: null
