2d126e8ddad579459e8fbd86925d674d2607152c
Supinic <supinic@protonmail.com>
2021-04-01 21:11:16 +0200

    refactored to use channel_chat_module params

--- chat-modules/stream-points-redemptions/index.js
+++ chat-modules/stream-points-redemptions/index.js
@@ -2,34 +2,22 @@ module.exports = {
 	Name: "stream-points-redemptions",
 	Events: ["message"],
 	Description: "Reacts to redemptions",
-	Code: (async function streamPointsRedemption (context) {
-		if (this.data.config === null) {
+	Code: (async function streamPointsRedemption (context, ...args) {
+		if (args.length === 0) {
 			return;
 		}
-		else if (typeof this.data.config === "undefined") {
-			this.data.config = [
-				{
-					name: "Javascript rant",
-					redemption: "44df442f-3fe8-4417-a377-112ff9c3708a",
-					channel: 38,
-					callback: (context) => {
-						context.channel.send("@Supinic, time to rant about JS FeelsJavascriptMan");
-					}
-				}
-			];
-		}
 
-		const { messageData } = context;
+		const { channel, messageData } = context;
 		if (!messageData?.customRewardID) {
 			return;
 		}
 
-		const redemption = this.data.config.find(i => i.redemption === messageData.customRewardID);
+		const redemption = args.find(i => i.redemption === messageData.customRewardID);
 		if (!redemption) {
 			return;
 		}
 
-		await redemption.callback(context);
+		await channel.send(redemption.message);
 	}),
 	Author: "supinic"
 };
