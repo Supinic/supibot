cdab583f0e2d04449ec90f99587bf6ee4138df62
Supinic <supinic@protonmail.com>
2021-04-02 18:53:24 +0200

    adding multiple options to check messages

--- chat-modules/message-react/index.js
+++ chat-modules/message-react/index.js
@@ -19,18 +19,46 @@ module.exports = {
 		for (const item of args) {
 			let adjustedMessage = message;
 			let check = item.check;
-			if (item.ignoreCase) {
-				check = check.toLowerCase();
-				adjustedMessage = adjustedMessage.toLowerCase();
+			let passed = false;
+
+			if (typeof check === "string") {
+				if (item.ignoreCase) {
+					check = check.toLowerCase();
+					adjustedMessage = adjustedMessage.toLowerCase();
+				}
+
+				passed = (adjustedMessage === check);
+			}
+			else if (check instanceof RegExp) {
+				passed = check.test(message);
+			}
+			else if (typeof check === "function") {
+				passed = await check(context, message);
+			}
+			else {
+				console.warn("Incorrect chat-module check type", {
+					chatModule: this.Name,
+					channel: channel.ID,
+					item
+				});
 			}
 
-			if (adjustedMessage === check) {
-				if (typeof item.response === "string") {
-					await channel.send(item.response);
-				}
-				else if (typeof item.callback === "function") {
-					await item.callback(context, item);
-				}
+			if (!passed) {
+				return;
+			}
+
+			if (typeof item.response === "string") {
+				await channel.send(item.response);
+			}
+			else if (typeof item.callback === "function") {
+				await item.callback(context, item);
+			}
+			else {
+				console.warn("Incorrect chat-module response type", {
+					chatModule: this.Name,
+					channel: channel.ID,
+					item
+				});
 			}
 		}
 	}),
