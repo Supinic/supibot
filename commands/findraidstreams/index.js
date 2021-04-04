da5e4375fe7c0ce4f9f6dd13ca61ed1886cdfc64
Supinic <supinic@protonmail.com>
2021-04-04 19:17:18 +0200

    refactor to use channel's getStreamData()

--- commands/findraidstreams/index.js
+++ commands/findraidstreams/index.js
@@ -8,23 +8,25 @@ module.exports = {
 	Params: null,
 	Whitelist_Response: null,
 	Static_Data: (() => ({
-		viewerThreshold: 100,
-		ignoredChannels: [ 42 ]
+		viewerThreshold: 100
 	})),
 	Code: (async function findRaidStreams () {
-		const raidable = sb.Channel.data
-			.filter(i => (
-				i.sessionData?.live
-				&& !this.staticData.ignoredChannels.includes(i.ID)
-			))
-			.map(channel => {
-				const { stream } = channel.sessionData;
+		const channels = sb.Channel.getJoinableForPlatform("twitch");
+		const channelStreamData = await Promise.all(channels.map(async (channel) => {
+			const data = await channel.getStreamData();
+			return { channel, data };
+		}));
+
+		const raidable = channelStreamData
+			.filter(i => i.data.live)
+			.map(i => {
+				const { channel, data } = i;
 				return {
 					name: channel.Name,
-					game: stream.game,
-					status: stream.status,
-					viewers: stream.viewers,
-					online: sb.Utils.timeDelta(stream.since, true)
+					game: data.stream.game,
+					status: data.stream.status,
+					viewers: data.stream.viewers,
+					online: sb.Utils.timeDelta(new sb.Date(data.stream.since), true)
 				};
 			})
 			.filter(i => i.viewers < this.staticData.viewerThreshold)
