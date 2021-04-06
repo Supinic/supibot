b6c67e391eac5829a00dfc22dc16c6229fe6a29a
Supinic <supinic@protonmail.com>
2021-04-06 23:31:08 +0200

    replaced discord+github crons with a single one

---
+++ crons/supinic-advert/index.js
@@ -0,0 +1,24 @@
+module.exports = {
+	Name: "supinic-advert",
+	Expression: "0 30 * * * *",
+	Description: "Posts a reminder how to use titlechange_bot in #supinic",
+	Defer: {
+		start: 0,
+		end: 600_000
+	},
+	Type: "Bot",
+	Code: (async function announceDiscordLink () {
+		const channelData = sb.Channel.get("supinic", "twitch");
+		const streamData = await channelData.getStreamData();
+		if (!streamData.live) {
+			return;
+		}
+
+		await channelData.send(sb.Utils.tag.trim `
+			Chatterino users supiniOkay 👉
+			To get binged 🔔 when Supi goes live, use !notifyme live
+			or use !events to get a list of things to get binged 🔔 about
+			(e.g. going live, changing title, ...)
+		`);
+	})
+};
