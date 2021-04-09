b34926eafb3ad1935b171c50dd018d72c8e8476e
Supinic <supinic@protonmail.com>
2021-04-09 19:46:06 +0200

    adding $stats markov

--- commands/statistics/index.js
+++ commands/statistics/index.js
@@ -340,6 +340,37 @@ module.exports = {
 					};
 				}
 			},
+			{
+				name: "markov",
+				aliases: [],
+				description: "Returns quick stats about a markov module in a given channel.",
+				execute: async (context, type, channelName) => {
+					const module = sb.ChatModule.get("async-markov-experiment");
+					if (!module) {
+						return {
+							success: false,
+							reply: `No Markov module is currently available!`
+						};
+					}
+
+					const channelData = sb.Channel.get(channelName ?? "forsen");
+					const markov = module.data.markovs.get(channelData);
+					if (!markov) {
+						return {
+							success: false,
+							reply: "This channel does not have a markov-chain module configured!"
+						};
+					}
+
+					return {
+						reply: sb.Utils.tag.trim `
+							Markov module for channel ${channelData.Name} currently has:
+							${markov.size} unique words,
+							and ${markov.edges ?? "(unknown)"} connections between words. 
+						`
+					};
+				}
+			},
 		]
 	})),
 	Code: (async function statistics (context, type, ...args) {
