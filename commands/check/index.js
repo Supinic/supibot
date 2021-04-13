7930500259924ad407ffe5c6e1ee1b9b04c4348f
Supinic <supinic@protonmail.com>
2021-04-13 18:19:55 +0200

    added $check userdatalength (udl)

--- commands/check/index.js
+++ commands/check/index.js
@@ -548,6 +548,43 @@ module.exports = {
 							: `Currently blacklisted flags in this channel: ${flags.join(", ")}`
 					};
 				}
+			},
+			{
+				name: "userdatalength",
+				aliases: ["udl"],
+				description: "Checks the size of a user's (or yours) Data within Supibot.",
+				execute: async (context, user) => {
+					const userData = await sb.User.get(user ?? context.user);
+					if (!userData) {
+						return {
+							success: false,
+							reply: "Invalid user provided!"
+						};
+					}
+
+					const length = await sb.Query.getRecordset(rs => rs
+					    .select("LENGTH(IFNULL(Data, '')) AS Size")
+					    .from("chat_data", "User_Alias")
+						.where("ID = %n", userData.ID)
+						.limit(1)
+						.single()
+						.flat("Size")
+					);
+
+					if (typeof length !== "number") {
+						return {
+							success: false,
+							reply: `Could not retrieve the user data length!`
+						};
+					}
+					else {
+						const prefix = (context.user === userData) ? "Your" : "Their";
+						const size = sb.Utils.formatByteSize(length);
+						return {
+							reply: `${prefix} user data currently occupies ${size} in Supibot's database.`
+						};
+					}
+				}
 			}
 		]
 	})),
