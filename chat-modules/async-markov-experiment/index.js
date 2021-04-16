a25e2366566a27734b89a476118d0f6c24512bc6
Supinic <supinic@protonmail.com>
2021-04-16 19:49:34 +0200

    bump up threshold to 10k

--- chat-modules/async-markov-experiment/index.js
+++ chat-modules/async-markov-experiment/index.js
@@ -26,7 +26,7 @@ module.exports = {
 			this.data.regex = /^[\p{Emoji}\x20-\x7e]+$/ui;
 		}
 		if (typeof this.data.threshold !== "number") {
-			this.data.threshold = 5000;
+			this.data.threshold = 10_000;
 		}
 
 		const markov = this.data.markovs.get(context.channel);
