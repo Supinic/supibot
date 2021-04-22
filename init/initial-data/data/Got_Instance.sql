e35869a0a886baaff4eed5269dc41f3625b2b6ca
Supinic <supinic@protonmail.com>
2021-04-22 18:16:24 +0200

    updated to reflect new, easier syntax by using Global sb.Got
    also adjusted formatting

--- init/initial-data/data/Got_Instance.sql
+++ init/initial-data/data/Got_Instance.sql
@@ -1,53 +1,68 @@
 INSERT INTO `data`.`Got_Instance` 
-(`Name`,`Options_Type`,`Options`,`Parent`,`Description`)
+(`Name`,`Parent`,`Description`,`Options_Type`,`Options`)
 VALUES
-('Twitch','function','(() => ({
-	prefixUrl: "https://twitch.tv",
-	responseType: "json",
-	headers: {
-		Authorization: sb.Config.get("TWITCH_OAUTH", false),
-		"User-Agent": sb.Config.get("DEFAULT_USER_AGENT")
-	}
-}))',NULL,'Catch-all Got instance for all Twitch API endpoints'),
-('Helix','function','(() => ({
-	prefixUrl: "https://api.twitch.tv/helix",
-	responseType: "json",
-	headers: {
-		"Client-ID": sb.Config.get("TWITCH_CLIENT_ID", false),
-		Authorization: `Bearer ${sb.Config.get("TWITCH_APP_ACCESS_TOKEN", false)}`,
-		"User-Agent": sb.Config.get("DEFAULT_USER_AGENT")
-	}
-}))',NULL,'Twitch/Helix instance'),
-('Kraken','function','(() => ({
-	prefixUrl: "https://api.twitch.tv/kraken",
-	responseType: "json",
-	resolveBodyOnly: true,
-	headers: {
-		"Accept": "application/vnd.twitchtv.v5+json",
-		"Client-ID": sb.Config.get("TWITCH_CLIENT_ID", false),
-		"User-Agent": sb.Config.get("DEFAULT_USER_AGENT")
-	},
-	mutableDefaults: true
-}))',NULL,'Twitch/V5 instance'),
-('Leppunen','function','(() => ({
-    responseType: "json",
-    prefixUrl: "https://api.ivr.fi",
-    headers: {
-        "User-Agent": sb.Config.get("DEFAULT_USER_AGENT")
-    },
-    throwHttpErrors: false
-}))',NULL,'Leppunen\'s API instance'),
-('Supibot','function','(() => {
-    const secure = sb.Config.get("SUPIBOT_API_SECURE", false) ?? false;
-    const protocol = (secure) ? "https" : "http";
-    const port = sb.Config.get("SUPIBOT_API_PORT", false) ?? 80;
-
-    return {
+(
+    'Global',
+    NULL,
+    'Global definition - isn\'t used on its own, just used as template for all others',
+    'function',
+    '(() => ({
         responseType: "json",
-        prefixUrl: `${protocol}://localhost:${port}`,
+        retry: 0,
+        timeout: 5000,
+        mutableDefaults: true,
+        throwHttpErrors: true,
         headers: {
             "User-Agent": sb.Config.get("DEFAULT_USER_AGENT")
-        },
-        throwHttpErrors: false
-    };
-})',NULL,'Supibot internal API instance');
+        }
+    }))'
+),
+(
+    'Helix',
+    'Global',
+    NULL,
+    'function',
+    '(() => ({
+     	prefixUrl: "https://api.twitch.tv/helix",
+     	headers: {
+     		"Client-ID": sb.Config.get("TWITCH_CLIENT_ID"),
+     		"Authorization": `Bearer ${sb.Config.get("TWITCH_APP_ACCESS_TOKEN")}`
+     	}
+     }))'
+),
+(
+    'Kraken',
+    'Global',
+    NULL,
+    'function',
+    '(() => ({
+     	prefixUrl: "https://api.twitch.tv/kraken",
+     	headers: {
+		    "Accept": "application/vnd.twitchtv.v5+json",
+     		"Client-ID": sb.Config.get("TWITCH_CLIENT_ID")
+     	}
+     }))'
+),
+(
+    'Leppunen',
+    'Global',
+    NULL,
+    'JSON',
+    '{ "prefixUrl": "https://api.ivr.fi" }'
+),
+(
+    'Supibot',
+    'Global',
+    'This is the internal API provided by Supibot and configured via sb.Config',
+    'function',
+    '(() => {
+        const secure = sb.Config.get("SUPIBOT_API_SECURE", false) ?? false;
+        const protocol = (secure) ? "https" : "http";
+        const port = sb.Config.get("SUPIBOT_API_PORT", false) ?? 80;
+
+        return {
+            prefixUrl: `${protocol}://localhost:${port}`
+        };
+    })'
+)
+;
