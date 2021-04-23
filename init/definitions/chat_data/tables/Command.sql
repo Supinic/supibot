e7b5c2e68219700b322be8e395ba0bd8f49486ff
Supinic <supinic@protonmail.com>
2021-04-23 19:14:49 +0200

    new flag for commands - external-input
    includes an "update script" for existing tables

--- init/definitions/chat_data/tables/Command.sql
+++ init/definitions/chat_data/tables/Command.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS `chat_data`.`Command` (
 	`ID` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
 	`Name` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
 	`Aliases` TEXT(65535) NULL DEFAULT NULL COMMENT 'JSON array of strings that will be this command\'s aliases.' COLLATE 'utf8mb4_general_ci',
-	`Flags` SET('archived','block','developer','link-only','mention','opt-out','owner-override','ping','pipe','read-only','rollback','skip-banphrase','system','use-params','whitelist') NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+	`Flags` SET('archived','block','developer','external-input','link-only','mention','opt-out','owner-override','ping','pipe','read-only','rollback','skip-banphrase','system','use-params','whitelist') NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
     `Params` TEXT NULL DEFAULT NULL COMMENT 'JSON object with the definition of the command\'s parameters.' COLLATE 'utf8mb4_general_ci',
 	`Description` VARCHAR(300) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
 	`Cooldown` INT(10) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'Command cooldown given in milliseconds',
---
+++ init/updates/0548566.sql
@@ -0,0 +1,7 @@
+-- Applies changes from supinic/supibot-package-manager#0548566
+ALTER TABLE `chat_data`.Command`
+CHANGE COLUMN `Flags` `Flags`
+SET('archived','block','developer','external-input','link-only','mention','non-nullable','opt-out','owner-override','ping','pipe','read-only','rollback','skip-banphrase','system','use-params','whitelist') NULL
+DEFAULT NULL
+COLLATE 'utf8mb4_general_ci'
+AFTER `Aliases`;
