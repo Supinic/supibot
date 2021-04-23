136a8c4931c48b3c74c407567f741039eb2a76ef
Supinic <supinic@protonmail.com>
2021-04-23 19:56:22 +0200

    no longer relies on unix- and rpi- specific calls
    temperature and memory checks will simply result in "N/A" or "0/0" if not present on the OS.

--- commands/ping/index.js
+++ commands/ping/index.js
@@ -26,18 +26,22 @@ module.exports = {
 		const readFile = require("fs").promises.readFile;
 		const exec = promisify(require("child_process").exec);
 		const chars = {a: "e", e: "i", i: "o", o: "u", u: "y", y: "a"};
-	
-		const [temperature, memory] = await Promise.all([
+
+		const [temperatureResult, memoryResult] = await Promise.allSettled([
 			exec("/opt/vc/bin/vcgencmd measure_temp"),
 			readFile("/proc/meminfo")
 		]);
-	
-		const memoryData = String(memory).split("\n").filter(Boolean).map(i => Number(i.split(/:\s+/)[1].replace(/kB/, "")) * 1000);
+
+		const temperature = (temperatureResult.value)
+			? temperatureResult.value.stdout.match(/([\d.]+)/)[1] + "°C"
+			: "N/A";
+
+		const memoryData = (memoryResult.value)
+			? String(memoryResult.value).split("\n").filter(Boolean).map(i => Number(i.split(/:\s+/)[1].replace(/kB/, "")) * 1000)
+			: [...Array(3)].fill(0);
+
 		const pong = "P" + chars[context.invocation[1]] + "ng!";
-	
-		// const [swapTotal, swapFree] = memoryData.slice(14, 16);
-		// const swapUsed = (swapTotal - swapFree);
-	
+
 		const [min1, min5] = getLoadAverages();
 		const loadRatio = (min1 / min5);
 		const loadDelta = Math.abs(1 - loadRatio);
@@ -47,7 +51,7 @@ module.exports = {
 		const uptime = sb.Runtime?.started ?? new sb.Date().addSeconds(-process.uptime());
 		const data = {
 			Uptime: sb.Utils.timeDelta(uptime).replace("ago", "").trim(),
-			Temperature: temperature.stdout.match(/([\d.]+)/)[1] + "°C",
+			Temperature: temperature,
 			"Free memory": sb.Utils.formatByteSize(memoryData[2], 0) + "/" + sb.Utils.formatByteSize(memoryData[0], 0),
 			"CPU usage": (min5 === 0)
 				? "No stats available"
@@ -55,43 +59,43 @@ module.exports = {
 			// Swap: sb.Utils.formatByteSize(swapUsed, 0) + "/" + sb.Utils.formatByteSize(swapTotal, 0),
 			"Commands used": await sb.Runtime.commands
 		};
-	
+
 		if (sb.Cache) {
 			data.Redis = (sb.Cache.active)
 				? String(await sb.Cache.server.dbsize()) + " keys"
 				: "not online"
 		}
-	
+
 		if (context.channel) {
 			const type = context.channel.Banphrase_API_Type;
 			const url = context.channel.Banphrase_API_URL;
-	
+
 			if (type && url) {
 				const ping = await this.staticData.checkLatency(
 					async () => sb.Banphrase.executeExternalAPI("test", type, url)
 				);
-	
+
 				const result = (ping === null)
 					? "No response from API"
 					: `${Math.trunc(ping)}ms`;
-	
+
 				data["Banphrase API"] = `Using ${type} API: ${url} (${result})`;
 			}
 			else {
 				data["Banphrase API"] = "Not connected";
 			}
 		}
-	
+
 		if (context.platform.Name === "twitch") {
 			const ping = await this.staticData.checkLatency(
 				async () => context.platform.client.ping()
 			);
-	
+
 			data["Latency to TMI"] = (ping === null)
 				? "No response from Twitch (?)"
 				: `${Math.trunc(ping)}ms`;
 		}
-	
+
 		return {
 			reply: pong + " " + Object.entries(data).map(([name, value]) => name + ": " + value).join("; ")
 		};
