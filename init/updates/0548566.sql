07cdb37c76bd88ea4adafdf4f3f20bcbfc079140
Supinic <supinic@protonmail.com>
2021-04-25 21:28:54 +0200

    typo fix

--- init/updates/0548566.sql
+++ init/updates/0548566.sql
@@ -1,5 +1,5 @@
 -- Applies changes from supinic/supibot-package-manager#0548566
-ALTER TABLE `chat_data`.Command`
+ALTER TABLE `chat_data`.`Command`
 CHANGE COLUMN `Flags` `Flags`
 SET('archived','block','developer','external-input','link-only','mention','non-nullable','opt-out','owner-override','ping','pipe','read-only','rollback','skip-banphrase','system','use-params','whitelist') NULL
 DEFAULT NULL
