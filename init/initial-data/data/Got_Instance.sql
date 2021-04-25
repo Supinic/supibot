f312ad68f37b77daa6f2ca2419ed0d346781bebf
Supinic <supinic@protonmail.com>
2021-04-25 19:21:12 +0200

    updated Helix + Kraken sb.Got instances
    added checks for missing sb.Config values - if they're missing, sb.Got would crash with an almost nonsensical error - complaining about resetting headers. now, crashes with a more legible error

--- init/initial-data/data/Got_Instance.sql
+++ init/initial-data/data/Got_Instance.sql
@@ -22,26 +22,43 @@ VALUES
     'Global',
     NULL,
     'function',
-    '(() => ({
-     	prefixUrl: "https://api.twitch.tv/helix",
-     	headers: {
-     		"Client-ID": sb.Config.get("TWITCH_CLIENT_ID"),
-     		"Authorization": `Bearer ${sb.Config.get("TWITCH_APP_ACCESS_TOKEN")}`
+    '(() => {
+     	if (!sb.Config.has("TWITCH_CLIENT_ID")) {
+     		throw new Error("Helix sb.Got instance cannot initialize - missing client-id");
      	}
-     }))'
+
+     	const token = sb.Config.get("TWITCH_APP_ACCESS_TOKEN", false) ?? sb.Config.get("TWITCH_OAUTH", false);
+     	if (!token) {
+     		throw new Error("Helix sb.Got instance cannot initialize - missing token");
+     	}
+
+     	return {
+     		prefixUrl: "https://api.twitch.tv/helix",
+     		headers: {
+     			"Client-ID": sb.Config.get("TWITCH_CLIENT_ID"),
+     			"Authorization": `Bearer ${token}`
+     		}
+     	};
+     })'
 ),
 (
     'Kraken',
     'Global',
     NULL,
     'function',
-    '(() => ({
-     	prefixUrl: "https://api.twitch.tv/kraken",
-     	headers: {
-		    "Accept": "application/vnd.twitchtv.v5+json",
-     		"Client-ID": sb.Config.get("TWITCH_CLIENT_ID")
+    '(() => {
+     	if (!sb.Config.has("TWITCH_CLIENT_ID")) {
+     		throw new Error("Helix sb.Got instance cannot initialize - missing client-id");
      	}
-     }))'
+
+     	return {
+     		prefixUrl: "https://api.twitch.tv/kraken",
+     		headers: {
+     			"Accept": "application/vnd.twitchtv.v5+json",
+     			"Client-ID": sb.Config.get("TWITCH_CLIENT_ID")
+     		}
+     	};
+     })'
 ),
 (
     'Leppunen',
