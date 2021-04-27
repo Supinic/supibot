c8d4dd6b63a9db3cc4db8db6ed9cfe285d4b4332
Supinic <supinic@protonmail.com>
2021-04-27 19:02:34 +0200

    now saves read-only status in DB + channel data

--- chat-modules/offline-only-mode/index.js
+++ chat-modules/offline-only-mode/index.js
@@ -3,11 +3,21 @@ module.exports = {
 	Events: ["online", "offline"],
 	Description: "Makes Supibot go into Read-only mode when the channel is online. Reverts back when the channel goes offline.",
 	Code: (async function offlineOnlyMode (context) {
-		if (context.event === "online" && context.channel.Mode !== "Read") {
-			context.channel.Mode = "Read";
+		const { event, channel } = context;
+
+		if (event === "online" && channel.Mode !== "Read" && !channel.Data.offlineOnlyData) {
+			channel.Data.offlineOnlyData = {
+				started: new sb.Date().sqlDateTime()
+			};
+
+			await channel.saveProperty("Data");
+			await channel.saveProperty("Mode", "Read");
 		}	
-		else if (context.event === "offline" && context.channel.Mode === "Read") {
-			context.channel.Mode = "Write";
+		else if (event === "offline" && channel.Mode === "Read" && channel.Data.offlineOnlyData) {
+			delete channel.Data.offlineOnlyData;
+
+			await channel.saveProperty("Data");
+			await channel.saveProperty("Mode", "Write");
 		}
 	}),
 	Author: "supinic"
