20b5baa06983edd827305c463d92e9ccb48d0c9c
Supinic <supinic@protonmail.com>
2021-04-27 19:09:38 +0200

    refactored to save the mirror status permanently (db)

--- chat-modules/offline-only-mirror/index.js
+++ chat-modules/offline-only-mirror/index.js
@@ -3,24 +3,27 @@ module.exports = {
 	Events: ["online", "offline"],
 	Description: "This module manages channel mirrors so that they are only in effect when the channel is offline.",
 	Code: (async function offlineOnlyMirror (context) {
-		if (typeof this.data.mirrors === "undefined") {
-			this.data.mirrors = new Map();
-		}
-		
 		const { event, channel } = context;
-		if (event === "online" && channel.Mirror !== null) {
-			this.data.mirrors.set(channel, channel.Mirror);
-			channel.Mirror = null;
+		
+		if (event === "online" && channel.Mirror !== null && !channel.Data.offlineOnlyMirror) {
+			channel.Data.offlineOnlyMirror = channel.Mirror;
+
+			await channel.saveProperty("Data");
+			await channel.saveProperty("Mirror", null);
 		}
-		else if (event === "offline" && this.data.mirrors.has(channel)) {
-			channel.Mirror = this.data.mirrors.get(channel);
-			this.data.mirrors.delete(channel);
+		else if (event === "offline" && this.data.mirrors.has(channel) && channel.Data.offlineOnlyMirror) {
+			const mirror = channel.Data.offlineOnlyMirror;
+			delete channel.Data.offlineOnlyMirror;
+
+			await channel.saveProperty("Data");
+			await channel.saveProperty("Mirror", mirror);
 		}
 		else {
 			console.warn("Invalid combination of channel, event and mirror status", {
 				channel: channel.ID,
 				event,
-				mirror: channel.Mirror
+				mirror: channel.Mirror,
+				offlineOnlyMirror: channel.Data.offlineOnlyMirror
 			});
 		}
 	}),
