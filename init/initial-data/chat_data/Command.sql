28bcd966e9d8aad773b38ba3f5718b15e5ec9311
Supinic <supinic@protonmail.com>
2021-04-30 17:27:12 +0200

    updating reload command to its latest revision

--- init/initial-data/chat_data/Command.sql
+++ init/initial-data/chat_data/Command.sql
@@ -81,48 +81,55 @@ VALUES
 	}
 })', NULL),
 ('reload',NULL,'pipe,skip-banphrase,system,whitelist','Reloads a database definition or hotloads an updated script',5000,'(async function reload (context, target, ...rest) {
+    let result = null;
     switch (target) {
         case "afks": await sb.AwayFromKeyboard.reloadData(); break;
+        case "afk": result  = await sb.AwayFromKeyboard.reloadData(); break;
 
-        case "bans":
         case "filters": await sb.Filter.reloadData(); break;
+        case "filter": result = await sb.Filter.reloadSpecific(...rest); break;
 
         case "banphrases": await sb.Banphrase.reloadData(); break;
+        case "banphrase": result = await sb.Banphrase.reloadSpecific(...rest); break;
 
         case "channels": await sb.Channel.reloadData(); break;
+        case "channel": result = await sb.Channel.reloadSpecific(...rest); break;
+
+        case "chat-modules":
+        case "chatmodules": await sb.ChatModule.reloadData(); break;
+        case "chat-module":
+        case "chatmodule": result = await sb.ChatModule.reloadSpecific(...rest); break;
 
         case "commands": await sb.Command.reloadData(); break;
-        case "command": {
-            try {
-                await sb.Command.reloadSpecific(target, ...rest);
-            }
-            catch {
-                return {
-                    success: false,
-                    reply: "No valid commands provided!"
-                };
-            }
-
-            break;
-        }
+        case "command": result = await sb.Command.reloadSpecific(...rest); break;
 
         case "config": await sb.Config.reloadData(); break;
 
-        case "cron": await sb.Cron.reloadData(); break;
-
-        case "extranews": await sb.ExtraNews.reloadData(); break;
+        case "crons": await sb.Cron.reloadData(); break;
+        case "cron": result = await sb.Cron.reloadSpecific(...rest); break;
 
         case "got": await sb.Got.reloadData(); break;
 
         case "reminders": await sb.Reminder.reloadData(); break;
+        case "reminder": result = await sb.Reminder.reloadSpecific(...rest); break;
 
         case "users": await sb.User.reloadData(); break;
 
-        default: return { reply: "Unrecognized module!" };
+        default: return {
+            success: false,
+            reply: "Unrecognized module!"
+        };
+    }
+
+    if (result === false) {
+        return {
+            success: false,
+            reply: `No ${target}s reloaded!`
+        };
     }
 
     return {
-        reply: "Reloaded successfully."
+        reply: `Successfully reloaded ${target}.`
     };
 })', 'This command is whitelisted! Make sure to add a Filter object for yourself in order to be able to use this command.'),
 ('pipe',NULL,'mention,system','Pipes the result of one command to another, and so forth. Each command will be used as if used separately, so each will be checked for cooldowns and banphrases. Use the character "|" or ">" to separate each command.',5000,'(async function pipe (context, ...args) {
