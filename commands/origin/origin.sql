054053a20d5eaabb89da6ea5df53dd3d3f2844ff
Supinic <supinic@protonmail.com>
2021-04-30 19:18:01 +0200

    added `data.Origin` definition + readme

---
+++ commands/origin/origin.sql
@@ -0,0 +1,30 @@
+CREATE TABLE IF NOT EXISTS `data`.`Origin` (
+	`ID` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+	`Emote_ID` VARCHAR(64) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+	`Name` VARCHAR(100) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`Text` VARCHAR(500) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`Tier` ENUM('1','2','3') NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+	`Type` ENUM('Twitch - Global','Twitch - Sub','Twitch - Bits','BTTV','FFZ','Discord','Other') NULL DEFAULT 'Twitch - Global' COLLATE 'utf8mb4_general_ci',
+	`Raffle` DATE NULL DEFAULT NULL,
+	`Raffle_Winner` INT(11) NULL DEFAULT NULL,
+	`User_Alias` INT(11) NOT NULL DEFAULT '1',
+	`Author` INT(11) NULL DEFAULT NULL,
+	`Todo` TINYINT(1) NOT NULL DEFAULT '0',
+	`Replaced` TINYINT(1) NOT NULL DEFAULT '0',
+	`Emote_Added` DATE NULL DEFAULT NULL,
+	`Record_Added` DATETIME NULL DEFAULT current_timestamp(),
+	`Notes` TEXT NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+	`Available` ENUM('Backup','Local','None','Original') NULL DEFAULT 'Original' COLLATE 'utf8mb4_general_ci',
+	`Backup_Link` VARCHAR(300) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+	PRIMARY KEY (`ID`) USING BTREE,
+	UNIQUE INDEX `Emote_ID` (`Emote_ID`) USING BTREE,
+	INDEX `fk_user_alias_origin` (`User_Alias`) USING BTREE,
+	INDEX `FK_Origin_chat_data.User_Alias` (`Raffle_Winner`) USING BTREE,
+	INDEX `FK_Origin_chat_data.User_Alias_2` (`Author`) USING BTREE,
+	CONSTRAINT `FK_Origin_chat_data.User_Alias` FOREIGN KEY (`Raffle_Winner`) REFERENCES `chat_data`.`User_Alias` (`ID`) ON UPDATE CASCADE ON DELETE SET NULL,
+	CONSTRAINT `FK_Origin_chat_data.User_Alias_2` FOREIGN KEY (`Author`) REFERENCES `chat_data`.`User_Alias` (`ID`) ON UPDATE CASCADE ON DELETE SET NULL,
+	CONSTRAINT `fk_user_alias_origin` FOREIGN KEY (`User_Alias`) REFERENCES `chat_data`.`User_Alias` (`ID`) ON UPDATE CASCADE ON DELETE CASCADE
+)
+COLLATE='utf8mb4_general_ci'
+ENGINE=InnoDB
+;
