7f88b850811b059234488759ad41383c5e62fa41
Supinic <supinic@protonmail.com>
2021-05-13 18:49:05 +0200

    added internal api `/channel/add`
    used to add and join new channels to given platforms
    platform controllers govern this behaviour with the `dynamicChannelAddition` flag

--- api/channel.js
+++ api/channel.js
@@ -59,6 +59,54 @@ module.exports = {
 			data: { message: "OK" }
 		};
 	},
+	add: async (req, res, url) => {
+		const channelName = url.searchParams.get("name");
+		const platformName = url.searchParams.get("platform");
+		const botChannelMode = url.searchParams.get("mode") ?? "Write";
+		const announcement = url.searchParams.get("announcement") ?? null;
+
+		if (!channelName || !platformName) {
+			return {
+				statusCode: 400,
+				data: { message: "No channel or platform provided" }
+			};
+		}
+
+		const platformData = sb.Platform.get(platformName);
+		if (!platformData) {
+			return {
+				statusCode: 400,
+				data: { message: "Invalid platform provided" }
+			};
+		}
+		else if (!platformData.controller.dynamicChannelAddition) {
+			return {
+				statusCode: 400,
+				data: { message: "Provided platform cannot dynamically add new channels" }
+			};
+		}
+
+		const channelID = await platformData.controller.getUserID(channelName);
+		if (!channelID) {
+			return {
+				success: false,
+				reply: "Provided channel does not exist on the provided platform!"
+			};
+		}
+
+		await sb.Channel.add(channelName, platformData, botChannelMode, channelID);
+		await platformData.client.join(channelName);
+
+		if (announcement) {
+			const channelData = sb.Channel.get(channelName, platformData);
+			await channelData.send(announcement);
+		}
+
+		return {
+			statusCode: 200,
+			data: { message: "Channel joined succesfully" }
+		};
+	},
 	join: async (req, res, url) => partOrJoin("join", url),
 	part: async (req, res, url) => partOrJoin("part", url)
 };
