8ebec0d539bcfcd3c1513e17792deb7f6cb64a6e
Supinic <supinic@protonmail.com>
2021-05-13 17:24:34 +0200

    added string-search for `$faq`
    resolves S#7994

--- commands/faq/index.js
+++ commands/faq/index.js
@@ -8,10 +8,30 @@ module.exports = {
 	Params: null,
 	Whitelist_Response: null,
 	Static_Data: null,
-	Code: (async function faq () {
-	        return {
-	            reply: `FAQ list here: https://supinic.com/data/faq/list For discussions, check https://github.com/Supinic/supibot/discussions/ or make a suggestion with the $suggest command.`
-	        };
-	    }),
+	Code: (async function faq (context, ...args) {
+		if (args.length > 0) {
+			const query = args.join(" ").toLowerCase()
+			const data = await sb.Query.getRecordset(rs => rs
+			    .select("Question", "Answer")
+			    .from("data", "FAQ")
+				.where("LOWER(Question) %*like*", query)
+			);
+
+			if (data.length === 1) {
+				const row = data[0];
+				return {
+					reply: `Q: ${row.Question} A: ${row.Answer}`
+				};
+			}
+		}
+
+		return {
+			reply: sb.Utils.tag.trim `
+				FAQ list here: https://supinic.com/data/faq/list
+				For discussions, check https://github.com/Supinic/supibot/discussions/
+			 	or make a suggestion with the $suggest command.
+			`
+		};
+	}),
 	Dynamic_Description: null
 };
