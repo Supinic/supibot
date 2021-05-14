4f0b726f5b78fd1d6318176b2dd66c13553609cd
Supinic <supinic@protonmail.com>
2021-05-14 19:29:08 +0200

    updating `chat_data.Filter` SQL specification
    - added new `Type`s
    - updated formatting a little

--- init/definitions/chat_data/tables/Filter.sql
+++ init/definitions/chat_data/tables/Filter.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS `chat_data`.`Filter` (
   `Command` INT(10) UNSIGNED DEFAULT NULL,
   `Platform` INT(10) UNSIGNED DEFAULT NULL,
   `Invocation` VARCHAR(50) DEFAULT NULL COMMENT 'This is specific command invocation - must always be coupled with Command',
-  `Type` ENUM('Blacklist','Whitelist','Opt-out','Block','Unping') NOT NULL DEFAULT 'Blacklist' COMMENT 'Determines the type of filter:\r\nBlacklist - given combo WILL NOT be able to use commands. By setting all combo fields to NULL, you will blacklist everyone everywhere. Not recommended.\r\nWhitelist - given combo WILL be able to use commands. Command must NOT be null.\r\nOpt-out - given Command will not accept User_Alias as its first argument. Other combo fields are ignored.\r\nBlock - given Command will not accept User_Alias as its first argument, only for user Blocked_User. Other combo fields are ignored.\r\nUnping - given Command will attempt to un-ping (insert ZWSP characters in their name) User_Alias. Other combo fields are ignored.',
+  `Type` ENUM('Blacklist','Whitelist','Opt-out','Block','Unping','Unmention','Cooldown','Flags','Offline-only','Online-only','Arguments') NOT NULL DEFAULT 'Blacklist' COMMENT 'Determins the type of filter:\r\nBlacklist - given combo WILL NOT be able to use commands. By setting all combo fields to NULL, you will blacklist everyone everywhere. Not recommended.\r\nWhitelist - given combo WILL be able to use commands. Command must NOT be null.\r\nOpt-out - given Command will not accept User_Alias as its first argument. Other combo fields are ignored.\r\nBlock - given Command will not accept User_Alias as its first argument, only for user Blocked_User. Other combo fields are ignored.\r\nUnping - given Command will attempt to un-ping (insert ZWSP characters in their name) User_Alias. Other combo fields are ignored.' COLLATE 'utf8mb4_general_ci',
   `Blocked_User` INT(10) UNSIGNED DEFAULT NULL,
   `Data` text DEFAULT NULL,
   `Active` TINYINT(1) UNSIGNED NOT NULL DEFAULT 1,
@@ -29,4 +29,8 @@ CREATE TABLE IF NOT EXISTS `chat_data`.`Filter` (
   CONSTRAINT `Filter_ibfk_2` FOREIGN KEY (`Command`) REFERENCES `Command` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `Filter_ibfk_3` FOREIGN KEY (`User_Alias`) REFERENCES `User_Alias` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `Filter_ibfk_4` FOREIGN KEY (`Issued_By`) REFERENCES `User_Alias` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Filters specific usages of commands.\r\nIn the following descriptions, "Combo fields" refer to the unique index User_Alias-Channel-Platform-Command-Type. If either is null, it is considered as global.\r\nE.g.: \r\ncombo 1-2-NULL-3-Blacklist will ban User_Alias 1 from using command 3 in Channel 2.\r\ncombo NULL-NULL-1-3-Blacklist will ban *EVERYONE* from using command 3 in platform 1.\r\ncombo 1-NULL-NULL-3-Whitelist will allow User_Alias 1 to use command 3 *EVERYWHERE*.\r\ncombo NULL-NULL-NULL-NULL-Blacklist will ban *EVERYONE* *EVERYWHERE*. Not recommended.';
+)
+ ENGINE=InnoDB
+ DEFAULT CHARSET=utf8mb4
+ COMMENT='Filters specific usages of commands.\r\nIn the following descriptions, "Combo fields" refer to the unique index User_Alias-Channel-Platform-Command-Type. If either is null, it is considered as global.\r\nE.g.: \r\ncombo 1-2-NULL-3-Blacklist will ban User_Alias 1 from using command 3 in Channel 2.\r\ncombo NULL-NULL-1-3-Blacklist will ban *EVERYONE* from using command 3 in platform 1.\r\ncombo 1-NULL-NULL-3-Whitelist will allow User_Alias 1 to use command 3 *EVERYWHERE*.\r\ncombo NULL-NULL-NULL-NULL-Blacklist will ban *EVERYONE* *EVERYWHERE*. Not recommended.'
+;
