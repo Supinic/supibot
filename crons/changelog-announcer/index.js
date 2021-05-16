faf2506194d0e963d911bd8d4dd8b40b6427f43c
Supinic <supinic@protonmail.com>
2021-05-16 17:46:44 +0200

    added subscription reminder support

--- crons/changelog-announcer/index.js
+++ crons/changelog-announcer/index.js
@@ -26,6 +26,55 @@ module.exports = {
 			return;
 		}
 
+		const subscriptions = await sb.Query.getRecordset(rs => rs
+		    .select("User_Alias", "Platform")
+		    .from("data", "Event_Subscription")
+			.where("Type = %s", "Changelog")
+			.where("Active = %b", true)
+		);
+
+		if (subscriptions.length > 0) {
+			let message;
+			if (data.length === 1) {
+				const link = `https://supinic.com/data/changelog/detail/${data[0].ID}`;
+				message = `New changelog entry detected! ${data[0].Type}: ${data[0].Title} Detail: ${link}`;
+			}
+			else {
+				const params = data.map(i => `ID=${i.ID}`).join("&");
+				const relay = await sb.Got("Supinic", {
+					method: "POST",
+					url: "relay",
+					throwHttpErrors: false,
+					json: {
+						url: `/data/changelog/lookup/${params}`
+					}
+				});
+
+				let link;
+				if (relay.statusCode === 200) {
+					link = relay.body.data.link;
+				}
+				else {
+					link = "Multiple IDs: " + data.map(i => i.ID).join(", ");
+				}
+
+				message = `New changelog entries detected! Details: ${link}`;
+			}
+
+			for (const sub of subscriptions) {
+				sb.Reminder.create({
+					Channel: null,
+					User_From: 1127,
+					User_To: sub.User_Alias,
+					Text: message,
+					Schedule: null,
+					Created: new sb.Date(),
+					Private_Message: true,
+					Platform: sub.Platform ?? 1
+				}, true)
+			}
+		}
+
 		const discord = sb.Platform.get("discord");
 		const discordUpdatesRole = "748957148439904336";
 		const discordChannel = await discord.client.channels.fetch("748955843415900280");
