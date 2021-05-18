708946b5be7a4c9d50afcc7c6a8095d731af640d
Supinic <supinic@protonmail.com>
2021-05-18 18:51:42 +0200

    added context params to test - for testing

--- commands/test/index.js
+++ commands/test/index.js
@@ -4,14 +4,15 @@ module.exports = {
 	Author: "supinic",
 	Cooldown: 5000,
 	Description: "?",
-	Flags: ["developer","pipe","skip-banphrase","system"],
-	Params: null,
+	Flags: ["developer","pipe","skip-banphrase","system","use-params"],
+	Params: [
+		{ name: "string", type: "string" }
+	],
 	Whitelist_Response: "For debugging purposes only :)",
 	Static_Data: null,
 	Code: (async function test () {
 		return {
-			success: false,
-			reply: `test`
+			reply: `param: ${context.params.string ?? "(none)"}`
 		};
 	}),
 	Dynamic_Description: null
