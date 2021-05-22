9c3a75e8b3cabe3d26eb940e1b646a4c90690c87
Supinic <supinic@protonmail.com>
2021-05-23 01:20:53 +0200

    split regex into two - avoid evil regex

--- commands/downloadclip/index.js
+++ commands/downloadclip/index.js
@@ -16,14 +16,17 @@ module.exports = {
 			};
 		}
 
-		const slug = rawSlug.match(/^[a-zA-z]+(-[_\-a-zA-Z0-9]{16})?$/)?.[0];
-		if (!slug) {
+		const legacyClipRegex = /[a-z]+/i;
+		const modernClipRegex = /[a-z]+-[-\w]{16}/i;
+		const match = rawSlug.match(modernClipRegex) ?? rawSlug.match(legacyClipRegex);
+		if (!match) {
 			return {
 				success: false,
 				reply: "Invalid clip slug provided! Only letters are allowed."
 			};
 		}
 
+		const [slug] = match;
 		const data = await sb.Got("Leppunen", `twitch/clip/${slug}`).json();
 		if (data.status === 404) {
 			return {
