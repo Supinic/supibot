3f2098d366f1ef14935035ad148de2dfacdfcaf7
Supinic <supinic@protonmail.com>
2021-05-23 01:31:16 +0200

    now accepts both clip names and clip URLs

--- commands/downloadclip/index.js
+++ commands/downloadclip/index.js
@@ -3,21 +3,23 @@ module.exports = {
 	Aliases: ["dlclip"],
 	Author: "supinic",
 	Cooldown: 30000,
-	Description: "Takes a Twitch clip name, and sends a download link to it into whispers.",
+	Description: "Takes a Twitch clip name or link, and sends a download link to it into whispers.",
 	Flags: ["mention","non-nullable","pipe"],
 	Params: null,
 	Whitelist_Response: null,
 	Static_Data: null,
-	Code: (async function downloadClip (context, rawSlug) {
-		if (!rawSlug) {
+	Code: (async function downloadClip (context, input) {
+		if (!input) {
 			return {
 				success: false,
-				reply: "No clip slug provided!"
+				reply: "No clip provided!"
 			};
 		}
 
+		const rawSlug = sb.Utils.parseURL(input).path;
 		const legacyClipRegex = /[a-z]+/i;
 		const modernClipRegex = /[a-z]+-[-\w]{16}/i;
+
 		const match = rawSlug.match(modernClipRegex) ?? rawSlug.match(legacyClipRegex);
 		if (!match) {
 			return {
