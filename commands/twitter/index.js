12b52edaef91eb8b798983d07e3926a7d722b91e
Supinic <supinic@protonmail.com>
2021-05-24 00:13:23 +0200

    full command refactor - no longer relies on `sb.Twitter`

--- commands/twitter/index.js
+++ commands/twitter/index.js
@@ -17,40 +17,79 @@ module.exports = {
 				reply: "No user provided!"
 			};
 		}
-	
-		const { error, data } = await sb.Twitter.fetchTweets({
-			username: user,
-			count: 1
+
+		let bearerToken = await this.getCacheData("bearer-token");
+		if (!bearerToken) {
+			const key = sb.Config.get("TWITTER_CONSUMER_KEY", false);
+			const secret = sb.Config.get("TWITTER_CONSUMER_SECRET", false);
+			if (!key || !secret) {
+				return {
+					success: false,
+					reply: `Cannot fetch any tweets - Twitter configuration is missing!`
+				};
+			}
+
+			const credentials = Buffer.from(`${key}:${secret}`, "utf-8").toString("base64");
+			const response = await sb.Got("GenericAPI", {
+				method: "POST",
+				url: "https://api.twitter.com/oauth2/token",
+				headers: {
+					Authorization: `Basic ${credentials}`,
+					"Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
+				},
+				body: "grant_type=client_credentials",
+				responseType: "json"
+			});
+
+			bearerToken = response.body.access_token;
+			await this.setCacheData("bearer-token", bearerToken, {
+				expiry: 30 * 864e5 // 30 days
+			});
+		}
+
+		const response = await sb.Got({
+			method: "GET",
+			url: "https://api.twitter.com/1.1/statuses/user_timeline.json",
+			headers: {
+				Authorization: `Bearer ${bearerToken}`
+			},
+			searchParams: {
+				screen_name: user,
+				count: "1",
+				exclude_replies: "true"
+			}
 		});
-	
-		if (error) {
-			let errorString = null;
-			if (error.error === "Not authorized.") {
-				errorString = "This account is either suspended or private!";
-			}
-			else if (error.errors?.[0].code === 34) {
-				errorString = "This account does not exist!";
-			}
-	
+
+		if (response.statusCode === 401) {
 			return {
 				success: false,
-				reply: errorString
+				reply: `This account is either suspended, private or has their replies limited to followers only!`
 			};
 		}
-	
-		const [tweet] = data;
+		else if (response.statusCode === 404) {
+			return {
+				success: false,
+				reply: `That Twitter account does not exist!`
+			};
+		}
+
+		const [tweet] = response.body;
 		if (!tweet) {
 			return {
-				reply: `That account has not tweeted so far.`
+				reply: "That account has not tweeted so far."
 			};
 		}
-	
-		const delta = sb.Utils.timeDelta(new sb.Date(tweet.created_at));
+
 		const fixedText = sb.Utils.fixHTML(tweet.text);
+		if (context.params.textOnly) {
+			return {
+				reply: fixedText
+			};
+		}
+
+		const delta = sb.Utils.timeDelta(new sb.Date(tweet.created_at));
 		return {
-			reply: (context.params.textOnly)
-				? fixedText
-				: `${fixedText} (posted ${delta})`
+			reply: `${fixedText} (posted ${delta})`
 		};
 	}),
 	Dynamic_Description: null
