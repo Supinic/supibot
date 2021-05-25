a4b56d2e0128cd4e5b206ed4b096b15a8f183dec
Supinic <supinic@protonmail.com>
2021-05-25 17:37:24 +0200

    added lines/hr calculation

--- commands/countlinetotal/index.js
+++ commands/countlinetotal/index.js
@@ -16,7 +16,7 @@ module.exports = {
 			.where("TABLE_SCHEMA = %s", "chat_line")
 			.single()
 		);
-	
+
 		const history = await sb.Query.getRecordset(rs => rs
 			.select("Executed", "Result")
 			.from("chat_data", "Command_Execution")
@@ -29,15 +29,19 @@ module.exports = {
 
 		let historyText = "";
 		const currentSize = sb.Utils.round(data.Bytes / (10 ** 9), 3);
-		
+
 		if (history) {
 			const days = (sb.Date.now() - history.Executed) / 864.0e5;
+
+			const originalLines = Number(history.Result.match(/logging([\d ])+lines/));
+			const linesPerHour = sb.Utils.round((data.Chat_Lines - originalLines) / days, 3);
+
 			const originalSize = Number(history.Result.match(/([\d.]+) GB of space/)[1]);
 			const rate = sb.Utils.round((currentSize - originalSize) / days, 3);
 			const megabytesPerHour = sb.Utils.round(rate * 1024 / 24, 3);
 			const fillDate = new sb.Date().addDays((220 - currentSize) / rate); // 238 GB minus an estimate of ~18GB of other stuff
 
-			historyText = `Lines are added at a rate of ~${megabytesPerHour} MB/hr. `;
+			historyText = `Lines are added at a rate of ~${megabytesPerHour} MB/hr and ${sb.Utils.groupDigits(linesPerHour, 3)} lines/hr. `;
 			historyText += (megabytesPerHour === 0)
 				? `At this rate, its impossible to calculate when Supibot's hard drive will fill.`
 				: `At this rate, Supibot's hard drive will run out of space approximately on ${fillDate.format("Y-m-d")}.`;
