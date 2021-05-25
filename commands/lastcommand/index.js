da03503d6fbe30b736ae62aa14513e71945aea3d
Supinic <supinic@protonmail.com>
2021-05-25 19:51:54 +0200

    added `input` parameter

--- commands/lastcommand/index.js
+++ commands/lastcommand/index.js
@@ -5,30 +5,33 @@ module.exports = {
 	Cooldown: 5000,
 	Description: "Posts your last command executed in the current channel. Only goes back up to 1 minute.",
 	Flags: ["pipe"],
-	Params: null,
+	Params: [
+		{ name: "input", type: "boolean" }
+	],
 	Whitelist_Response: null,
 	Static_Data: null,
 	Code: (async function lastCommand (context, user) {
 		const targetUser = (user)
 			? await sb.User.get(user)
 			: context.user;
-	
+
 		if (!targetUser) {
 			return {
 				success: false,
 				reply: `Provided user does not exist!`
 			};
 		}
-	
+
 		if (targetUser !== context.user && !context.channel) {
 			return {
 				success: false,
 				reply: "Cannot check other users' last command here!"
 			};
 		}
-	
+
 		const data = await sb.Query.getRecordset(rs => {
-			rs.select("Result")
+			rs
+				.select("Result", "Invocation", "Arguments")
 				.from("chat_data", "Command_Execution")
 				.where("Command <> %n", this.ID)
 				.where("User_Alias = %n", targetUser.ID)
@@ -37,22 +40,34 @@ module.exports = {
 				.orderBy("Executed DESC")
 				.limit(1)
 				.single();
-	
+
 			if (context.channel) {
 				rs.where("Channel = %n", context.channel.ID);
 			}
 			else {
 				rs.where("Channel IS NULL").where("Platform = %n", context.platform.ID);
 			}
-	
+
 			return rs;
 		});
-	
-		return {
-			reply: (data?.Result)
-				? String(data.Result)
-				: "No recent command execution found!"
-		};
+
+		if (!data.Result) {
+			return {
+				success: false,
+				reply: "No recent command execution found!"
+			};
+		}
+		else if (context.params.input) {
+			const args = JSON.parse(data.Arguments).join(" ");
+			return {
+				reply: `${data.Invocation} ${args}`
+			};
+		}
+		else {
+			return {
+				reply: String(data.Result)
+			};
+		}
 	}),
 	Dynamic_Description: null
 };
