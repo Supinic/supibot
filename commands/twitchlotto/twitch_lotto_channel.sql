b99e4a636b516af3967908f568bf4f90e5679d53
Supinic <supinic@protonmail.com>
2021-06-08 21:14:28 +0200

    updated definition table

--- commands/twitchlotto/twitch_lotto_channel.sql
+++ commands/twitchlotto/twitch_lotto_channel.sql
@@ -1,6 +1,8 @@
 CREATE TABLE IF NOT EXISTS `data`.`Twitch_Lotto_Channel` (
 	`Name` VARCHAR(100) NOT NULL COLLATE 'utf8mb4_general_ci',
 	`Amount` INT(11) NULL DEFAULT NULL,
+	`Scored` INT(11) NULL DEFAULT NULL,
+	`Tagged` INT(11) NULL DEFAULT NULL,
 	PRIMARY KEY (`Name`) USING BTREE
 )
 COLLATE='utf8mb4_general_ci'
