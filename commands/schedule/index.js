ba1b6a54527dc24769377d156479e55a3038f9f6
Supinic <supinic@protonmail.com>
2021-06-08 19:21:06 +0200

    added best available emote fetch for the "Weird" emote

--- commands/schedule/index.js
+++ commands/schedule/index.js
@@ -16,46 +16,49 @@ module.exports = {
 		else if (context.platform.Name === "twitch" && context.channel) {
 			channelName = context.channel.Name;
 		}
-	
+
 		if (!channelName) {
 			return {
 				success: false,
 				reply: `No channel provided, and there is no default channel to be used!`
 			};
 		}
-	
+
 		const data = await sb.Got("Leppunen", `twitch/streamschedule/${channelName}`).json();
 		if (data.status === 200 && data.nextStream) {
 			let extra = "";
 			if (data.interruption) {
 				const { endAt, reason } = data.interruption;
 				const end = new sb.Date(endAt);
-	
+
 				if (sb.Date.now() <= end) {
 					extra = `Stream schedule is interrupted - reason: ${reason}, will be back ${sb.Utils.timeDelta(end)}.`;
 				}
 			}
-	
+
 			const game = (data.nextStream.game === "No game set")
 				? "(no category)"
 				: data.nextStream.game;
-	
+
 			const title = (data.nextStream.title === "")
 				? "(no title)"
 				: data.nextStream.title;
-	
+
 			let target = `${channelName}'s`;
 			if (channelName.toLowerCase() === context.user.Name) {
 				target = "Your";
 				extra += " (shouldn't you know when you're supposed to stream? 😉)";
 			}
-	
+
 			const isLive = !(await sb.Command.get("si").execute({}, channelName)).reply.includes("offline");
 			const nextStream = new sb.Date(data.nextStream.startsAt);
-			const lateString = (!isLive && sb.Date.now() > nextStream)
-				? "The stream seems to be late FeelsWeirdMan"
-				: "";
-	
+
+			let lateString = "";
+			if (!isLive && sb.Date.now() > nextStream) {
+				const emote = await context.getBestAvailableEmote(["Weirdga", "WeirdChamp", "FeelsWeirdMan"], "🤨");
+				lateString = `The stream seems to be late ${emote}`;
+			}
+
 			const time = sb.Utils.timeDelta(new sb.Date(data.nextStream.startsAt));
 			return {
 				reply: `${target} next stream: ${game} - ${title}, starting ${time}. ${lateString} ${extra}`
