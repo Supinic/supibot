9af9a8bfca97d213e96b4fa434e9e301470e27a4
Supinic <supinic@protonmail.com>
2021-06-22 21:19:43 +0200

    reverted back to 100 emotesets in one API call
    - API endpoint was fixed

--- controllers/twitch.js
+++ controllers/twitch.js
@@ -866,7 +866,7 @@ module.exports = class TwitchController extends require("./template.js") {
 	 */
 	static async fetchTwitchEmotes (sets) {
 		const data = [];
-		const sliceLength = 50 - 1;
+		const sliceLength = 100;
 		let index = 0;
 
 		while (index < sets.length) {
