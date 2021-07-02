8358178fec18b4b6bbf3b3c0e27a9fcde3b52c87
Supinic <supinic@protonmail.com>
2021-07-02 17:17:57 +0200

    added seconds to VOD duration parsing

--- commands/vod/index.js
+++ commands/vod/index.js
@@ -13,7 +13,7 @@ module.exports = {
 			type = target;
 			target = null;
 		}
-	
+
 		if (!target) {
 			if (context.platform.Name === "twitch") {
 				if (!context.channel) {
@@ -32,7 +32,7 @@ module.exports = {
 				};
 			}
 		}
-	
+
 		const { controller } = sb.Platform.get("twitch");
 		const channelID = await controller.getUserID(target);
 		if (!channelID) {
@@ -40,42 +40,42 @@ module.exports = {
 				reply: "Invalid channel provided!"
 			};
 		}
-	
+
 		const vod = await sb.Got("Helix", {
 			url: "videos",
 			searchParams: `user_id=${channelID}`
 		}).json();
-	
+
 		if (vod.data.length === 0) {
 			return {
 				reply: "Target channel has no VODs saved!"
 			};
 		}
-	
+
 		let liveString = "";
 		const isLive = (await sb.Command.get("streaminfo").execute(context, target)).reply;
 		if (isLive && !isLive.includes("not exist") && !isLive.includes("offline")) {
 			liveString = " 🔴";
 		}
-	
+
 		const data = vod.data[0];
 		const delta = sb.Utils.timeDelta(new sb.Date(data.created_at));
-		const prettyDuration = data.duration.match(/\d+[hm]/g).join(", ");
-	
+		const prettyDuration = data.duration.match(/\d+[hms]/g).join(", ");
+
 		if (type === "current" || type === "exact") {
 			if (!liveString) {
 				return {
 					reply: `Channel is not currently live, no current/exact timestamp supported.`
 				};
 			}
-	
+
 			const offset = (type === "current") ? 90 : 0;
 			const stamp = sb.Utils.parseDuration(data.duration, { target: "sec" }) - offset;
 			return {
 				reply: `${sb.Utils.capitalize(type)} VOD timestamp: ${data.url}?t=${(stamp < 0) ? 0 : stamp}s`
 			};
 		}
-	
+
 		return {
 			reply: `${data.title} (length: ${prettyDuration}) - published ${delta} ${data.url}${liveString}`
 		};
