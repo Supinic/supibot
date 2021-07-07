dd12f146976f63a6d1b03341bc9b186adb10879c
Supinic <supinic@protonmail.com>
2021-07-07 18:57:08 +0200

    added `$stats twitchlotto`

--- commands/statistics/index.js
+++ commands/statistics/index.js
@@ -4,8 +4,10 @@ module.exports = {
 	Author: "supinic",
 	Cooldown: 10000,
 	Description: "Posts various statistics regarding you or other users, e.g. total afk time.",
-	Flags: ["mention","pipe"],
-	Params: null,
+	Flags: ["mention","pipe","use-params"],
+	Params: [
+		{ name: "recalculate", type: "boolean" }
+	],
 	Whitelist_Response: null,
 	Static_Data: (() => ({
 		types: [
@@ -522,6 +524,100 @@ module.exports = {
 							Global suggestion stats: https://supinic.com/data/suggestion/stats
 						`
 					};
+				}
+			},
+			{
+				name: "twitchlotto",
+				aliases: ["tl"],
+				description: "Posts stats for the $twitchlotto command - globally, or for a selected channel. You can use recalculate:true to force an update for the statistics in a specific channel.",
+				execute: async (context, type, channel) => {
+					if (channel) {
+						const lottoData = await sb.Query.getRecordset(rs => rs
+							.select("Channel", "Amount", "Scored", "Tagged")
+							.from("data", "Twitch_Lotto_Channel")
+							.where("Name = %s", channel)
+							.single()
+						);
+
+						if (!lottoData) {
+							return {
+								success: false,
+								reply: `Provided channel does not exists in the twitchlotto command!`
+							};
+						}
+
+						const obj = {
+							total: lottoData.Amount,
+							scored: lottoData.Scored,
+							tagged: lottoData.Tagged
+						};
+
+						if (context.params.recalculate) {
+							const [scored, tagged, deleted] = await Promise.all([
+								sb.Query.getRecordset(rs => rs
+									.select("COUNT(*) AS Count")
+									.from("data", "Twitch_Lotto")
+									.where("Name = %s", channel)
+									.where("Score IS NOT NULL")
+									.where("Available = %b OR Available IS NULL", true)
+									.single()
+									.flat("Count")
+								),
+								sb.Query.getRecordset(rs => rs
+									.select("COUNT(*) AS Count")
+									.from("data", "Twitch_Lotto")
+									.where("Name = %s", channel)
+									.where("Score IS NOT NULL")
+									.where("Adult_Flags IS NOT NULL")
+									.where("Available = %b OR Available IS NULL", true)
+									.single()
+									.flat("Count")
+								),
+								sb.Query.getRecordset(rs => rs
+									.select("COUNT(*) AS Count")
+									.from("data", "Twitch_Lotto")
+									.where("Name = %s", channel)
+									.where("Available = %b", false)
+									.single()
+									.flat("Count")
+								)
+							]);
+
+							await sb.Query.getRecordUpdater(ru => ru
+								.update("data", "Twitch_Lotto_Channel")
+								.set("Scored", scored)
+								.set("Tagged", tagged)
+								.set("Unavailable", deleted)
+								.where("Channel = %s", channel)
+							);
+
+							obj.scored = scored;
+							obj.tagged = tagged;
+						}
+
+						return {
+							reply: sb.Utils.tag.trim `
+								Channel "${channel}" has ${sb.Utils.groupDigits(obj.total)} TwitchLotto images in total.
+								${sb.Utils.groupDigits(obj.scored)} have been rated by the NSFW AI,
+								and out of those, ${sb.Utils.groupDigits(obj.tagged)} have been flagged by contributors.
+							`
+						};
+					}
+					else {
+						const lottoData = await sb.Query.getRecordset(rs => rs
+							.select("SUM(Amount) AS Amount", "SUM(Scored) AS Scored", "SUM(Tagged) AS Tagged")
+							.from("data", "Twitch_Lotto_Channel")
+							.single()
+						);
+
+						return {
+							reply: sb.Utils.tag.trim `
+								The TwitchLotto database has ${sb.Utils.groupDigits(lottoData.Amount)} images in total.
+								${sb.Utils.groupDigits(lottoData.Scored)} have been rated by the NSFW AI,
+								and out of those, ${sb.Utils.groupDigits(lottoData.Tagged)} have been flagged by contributors.
+							`
+						};
+					}
 				}
 			}
 		]
