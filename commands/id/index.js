dc6c9ca41729319d22f57d0fcdce6ee2a3572be9
Supinic <supinic@protonmail.com>
2021-07-15 19:38:01 +0200

    minor response adjustments - now uses `their` vs `your`
    - also uses best available emotes

--- commands/id/index.js
+++ commands/id/index.js
@@ -14,14 +14,14 @@ module.exports = {
 		const targetUser = (user)
 			? await sb.User.get(user)
 			: context.user;
-	
+
 		if (!targetUser) {
 			return {
 				success: false,
 				reply: "No data for given user name!"
 			};
 		}
-	
+
 		let idString = "That person's ID is";
 		let pronoun = "they were";
 		if (sb.User.bots.has(targetUser.ID)) {
@@ -32,27 +32,32 @@ module.exports = {
 			idString = "Your ID is";
 			pronoun = "you were";
 		}
-	
+
 		const temporalReply = (targetUser.Name === context.platform.Self_Name)
 			? "first brought to life as an mIRC bot"
 			: ((targetUser.ID < this.staticData.botID)
 				? "first mentioned in logs (predating Supibot)"
 				: "first seen");
-	
+
 		const delta = sb.Utils.timeDelta(targetUser.Started_Using);
 		const now = new sb.Date();
 		const { year, month, day } = new sb.Date(targetUser.Started_Using);
-	
+
 		let birthdayString = "";
 		if (now.year > year && now.month === month && now.day === day) {
+			const birthdayFace = await context.getBestAvailableEmote(["FeelsBirthdayMan"], "🥳");
+			const robotFace = await context.getBestAvailableEmote(["MrDestructoid"], "🤖");
+			const sideEmote = await context.getBestAvailableEmote(["Clap"], "🎈");
+
 			if (targetUser.Name === context.platform.Self_Name) {
-				birthdayString = "It's my birthday! FeelsBirthdayMan MrDestructoid";
+				birthdayString = `It's my birthday! ${birthdayFace} ${robotFace}`;
 			}
 			else {
-				birthdayString = `It's your account's ${now.year - year}. anniversary in my database! FeelsBirthdayMan Clap`;
+				const who = (targetUser === context.user) ? "your" : "their";
+				birthdayString = `It's ${who} account's ${now.year - year}. anniversary in my database! ${birthdayFace} ${sideEmote}`;
 			}
 		}
-	
+
 		return {
 			reply: `${idString} ${targetUser.ID} and ${pronoun} ${temporalReply} ${delta}. ${birthdayString}`
 		};
