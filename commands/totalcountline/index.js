c55414db9a7ac185508f77e191ba7ae85cbf0d73
Supinic <supinic@protonmail.com>
2021-08-10 18:27:47 +0200

    reduced cooldown, added reduced cooldown on failure
    30s -> 10s
    failure cooldown: 2.5s

--- commands/totalcountline/index.js
+++ commands/totalcountline/index.js
@@ -2,7 +2,7 @@ module.exports = {
 	Name: "totalcountline",
 	Aliases: ["acl","tcl"],
 	Author: "supinic",
-	Cooldown: 30000,
+	Cooldown: 10000,
 	Description: "Fetches the total amount of a user's (or yours, if nobody was specified) chat lines in all tracked channels summed together.",
 	Flags: ["mention","pipe","skip-banphrase"],
 	Params: null,
@@ -12,27 +12,29 @@ module.exports = {
 		if (!target) {
 			target = context.user;
 		}
-	
+
 		const userData = await sb.User.get(target);
 		if (!userData) {
 			return {
-				reply: `That user was not found in the database!`
+				success: false,
+				reply: `That user was not found in the database!`,
+				cooldown: 2500
 			};
 		}
-	
+
 		const data = (await sb.Query.getRecordset(rs => rs
 			.select("SUM(Message_Count) AS Total")
 			.from("chat_data", "Message_Meta_User_Alias")
 			.where("User_Alias = %n", userData.ID)
 			.single()
 		));
-		
+
 		if (data.Total === null) {
 			return {
 				reply: `That user is being tracked, but they have not said any lines in the channels I watch.`
 			};
 		}
-	
+
 		const who = (context.user.ID === userData.ID) ? "You have" : "That user has";
 		return {
 			reply: `${who} sent ${sb.Utils.groupDigits(data.Total)} chat lines across all tracked channels so far.`
