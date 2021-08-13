7b882da20ac349b2361468ff588067612664b491
Supinic <supinic@protonmail.com>
2021-08-13 18:05:10 +0200

    added params: `startsWith` `endsWith` `regex`

--- commands/randomword/index.js
+++ commands/randomword/index.js
@@ -4,8 +4,12 @@ module.exports = {
 	Author: "supinic",
 	Cooldown: 5000,
 	Description: "Fetches a random word. If a number is provided, rolls that many words.",
-	Flags: ["pipe"],
-	Params: null,
+	Flags: ["pipe","use-params"],
+	Params: [
+		{ name: "endsWith", type: "string" },
+		{ name: "regex", type: "regex" },
+		{ name: "startsWith", type: "string" }
+	],
 	Whitelist_Response: null,
 	Static_Data: (() => ({
 		limit: 10
@@ -13,11 +17,43 @@ module.exports = {
 	Code: (async function randomWord (context, number = 1) {
 		const repeats = Number(number);
 		if (!repeats || repeats > this.staticData.limit || repeats < 1 || Math.trunc(repeats) !== repeats) {
-			return { reply: "Invalid or too high amount of words!" };
+			return {
+				success: false,
+				reply: "Invalid or too high amount of words!"
+			};
 		}
-	
+
+		const { endsWith, regex, startsWith } = context.params;
+
+		// performance "save" - skip filtering if not needed
+		let wordList;
+		if (!endsWith && !regex && !startsWith) {
+			wordList = sb.Config.get("WORD_LIST");
+		}
+		else {
+			wordList = sb.Config.get("WORD_LIST").filter(word => {
+				let result = true;
+				if (endsWith) {
+					result &&= word.endsWith(endsWith);
+				}
+				if (startsWith) {
+					result &&= word.startsWith(startsWith);
+				}
+				if (regex) {
+					result &&= regex.test(word);
+				}
+
+				return result;
+			});
+		}
+
+		const words = [];
+		for (let i = 0; i < repeats; i++) {
+			words.push(sb.Utils.randArray(wordList));
+		}
+
 		return {
-			reply: [...new Array(repeats)].map(() => sb.Utils.randArray(sb.Config.get("WORD_LIST"))).join(" ")
+			reply: words.join(" ")
 		};
 	}),
 	Dynamic_Description: (async (prefix, values) => {
@@ -29,15 +65,27 @@ module.exports = {
 			"Highly recommended for its usage in pipe, for example into urban or translate...",
 			`Maximum amount of words: ${limit}`,
 			"",
-			
+
 			`<code>${prefix}rw</code>`,
 			"(one random word)",
 			"",
-	
+
 			`<code>${prefix}rw 10</code>`,
 			"(ten random words)",
 			"",
-	
+
+			`<code>${prefix}rw startsWith:(text)</code>`,
+			"One random word that starts with the specified text",
+			"",
+
+			`<code>${prefix}rw endsWith:(text)</code>`,
+			"One random word that ends with the specified text",
+			"",
+
+			`<code>${prefix}rw regex:(regular expression)</code>`,
+			"One random word that satisfies the provided regular expression",
+			"",
+
 			`<a href="https://pastebin.com/gUxBX1BL">Word list (Pastebin)</a>`
 		];
 	})
