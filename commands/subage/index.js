e9b3d73c6e2ddbbee4a75b9b69637d31b4473539
Supinic <supinic@protonmail.com>
2021-08-17 20:54:26 +0200

    initial implementation

---
+++ commands/subage/index.js
@@ -0,0 +1,130 @@
+module.exports = {
+	Name: "subage",
+	Aliases: ["sa"],
+	Author: "supinic",
+	Cooldown: 10000,
+	Description: "Fetches the subscription data for a given user on a given channel on Twitch.",
+	Flags: ["mention"],
+	Params: null,
+	Whitelist_Response: null,
+	Static_Data: null,
+	Code: (async function subAge (context, user, channel) {
+		const userID = await sb.Utils.getTwitchID(user ?? context.user.Name);
+		if (!userID) {
+			return {
+				success: false,
+				reply: `Provided user does not exist on Twitch!`
+			};
+		}
+
+		let channelID;
+		if (channel) {
+			channelID = await sb.Utils.getTwitchID(channel);
+		}
+		else {
+			if (context.platform.Name !== "twitch") {
+				return {
+					success: false,
+					reply: `When not in a Twitch channel, a specific channel name must be provided!`
+				};
+			}
+			else if (context.privateMessage) {
+				return {
+					success: false,
+					reply: `When in private messages, a specific channel name must be provided!`
+				};
+			}
+
+			channelID = await sb.Utils.getTwitchID(context.channel.Name);
+		}
+
+		if (!channelID) {
+			return {
+				success: false,
+				reply: `Provided channel does not exist on Twitch!`
+			};
+		}
+
+		const channelName = channel ?? context.channel.Name;
+		const userName = user ?? context.user.Name;
+
+		const response = await sb.Got({
+			method: "POST",
+			url: "https://gql.twitch.tv/gql",
+			responseType: "json",
+			headers: {
+				Accept: "*/*",
+				"Accept-Language": "en-US",
+				Authorization: `OAuth ${sb.Config.get("TWITCH_GQL_OAUTH")}`,
+				"Client-ID": sb.Config.get("TWITCH_GQL_CLIENT_ID"),
+				"Client-Version": sb.Config.get("TWITCH_GQL_CLIENT_VERSION"),
+				"Content-Type": "text/plain;charset=UTF-8",
+				Referer: `https://www.twitch.tv/popout/${channelName}/viewercard/${userName}`,
+				"X-Device-ID": sb.Config.get("TWITCH_GQL_DEVICE_ID")
+			},
+			body: JSON.stringify([{
+				operationName: "ViewerCard",
+				extensions: {
+					persistedQuery: {
+						version: 1,
+						sha256Hash: "1ad9680b56b15e64eb05cf6a99b49793a788315d32cab241968b582cc5520ed4"
+					}
+				},
+				variables: {
+					channelID,
+					channelLogin: channelName,
+					giftRecipientLogin: userName,
+					hasChannelID: true,
+					isViewerBadgeCollectionEnabled: true,
+					withStandardGifting: true
+				}
+			}])
+		});
+
+		const [sub] = response.body;
+		if (!sub) {
+			return {
+				success: false,
+				reply: `No subscription data available!`
+			};
+		}
+
+		const { relationship } = sub.data.targetUser;
+		if (!relationship.cumulativeTenure) {
+			return {
+				success: false,
+				reply: "User has hidden their subscription status."
+			};
+		}
+
+		const { daysRemaining, months } = relationship.cumulativeTenure;
+		if (!relationship.subscriptionBenefit) {
+			if (daysRemaining === 0 && months === 0) {
+				return {
+					reply: `User ${userName} is not subscribed to ${channelName}, and never has been.`
+				};
+			}
+			else {
+				return {
+					reply: `User ${userName} is not subscribed to ${channelName}, but used to be subscribed for ${months} months in total.`
+				};
+			}
+		}
+		else {
+			const benefit = relationship.subscriptionBenefit;
+			const giftString = (benefit.gift.isGift) ? "gifted" : "";
+			const primeString = (benefit.purchasedWithPrime) ? "Prime" : "";
+			const tier = benefit.tier.replace("000", "");
+
+			return {
+				reply: sb.Utils.tag.trim `
+					User ${userName} is subscribed to ${channelName}
+					for ${months} months in total
+					with a Tier ${tier} ${giftString} ${primeString} subscription.
+					Sub will renew in ${daysRemaining} days.
+				`
+			};
+		}
+	}),
+	Dynamic_Description: null
+};
