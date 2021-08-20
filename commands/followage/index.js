3dfad3c74465770b59f70083fab0ab489774cf4d
Supinic <supinic@protonmail.com>
2021-08-20 17:00:49 +0200

    refactor to use Helix instead of Kraken

--- commands/followage/index.js
+++ commands/followage/index.js
@@ -13,99 +13,79 @@ module.exports = {
 			channel = user;
 			user = context.user.Name;
 		}
-	
+
 		if (!channel) {
 			if (!context.channel) {
 				return {
 					success: false,
-					reply: "There is no channel to use here!"
+					reply: "You must provide a specific channel in PMs!"
 				};
 			}
-	
-			if (context.platform.Name === "twitch") {
-				channel = context.channel.Name;
-			}
-			// If used in a mirrored channel outside of Twitch, use the mirror target channel instead.
-			else if (context.channel.Mirror) {
-				const mirrorChannel = sb.Channel.get(context.channel.Mirror);
-				if (mirrorChannel.Platform.Name === "twitch") {
-					channel = mirrorChannel.Name;
-				}
-			}
-	
-			if (!channel) {
+			else if (context.platform.Name !== "twitch") {
 				return {
 					success: false,
-					reply: "Could not find any associated Twitch channels! Please specify one."
+					reply: "You must provide a specific channel in non-Twitch channels!"
 				};
 			}
 		}
-	
-		if (!user) {
-			user = context.user.Name;
-		}
-	
+
+		channel ??= context.channel.Name;
+		user ??= context.user.Name;
+
 		if (user === channel.toLowerCase()) {
 			if (user === context.user.Name) {
-				const emote = await context.platform.getBestAvailableEmote(
-					context.channel,
-					["PepeLaugh", "pepeLaugh", "LULW", "LuL", "LUL", "4HEad", "4Head"],
-					"😀"
-				);
-
+				const emote = await context.getBestAvailableEmote(["PepeLaugh", "LULW", "LuL", "4Head"], "😀");
 				return {
 					success: false,
 					reply: `Good luck following yourself! ${emote}`
 				};
 			}
 			else {
-				const emote = await context.platform.getBestAvailableEmote(
-					context.channel,
-					["FeelsDankMan", "FailFish"],
-					"🙄"
-				);
-
+				const emote = await context.getBestAvailableEmote(["FeelsDankMan", "FailFish"], "🙄");
 				return {
 					success: false,
 					reply: `People can't follow themselves! ${emote}`
 				};
 			}
 		}
-	
+
 		const { controller } = sb.Platform.get("twitch");
 		const [userID, channelID] = await Promise.all([
 			controller.getUserID(user),
 			controller.getUserID(channel)
 		]);
-	
+
 		if (!userID || !channelID) {
 			return {
 				success: false,
 				reply: "One or both users were not found!"
 			};
 		}
-	
-		const { body: data } = await sb.Got("Kraken", {
-			url: `users/${userID}/follows/channels/${channelID}`,
-			throwHttpErrors: false
+
+		const response = await sb.Got("Helix", {
+			url: "users/follows",
+			searchParams: {
+				from_id: userID,
+				to_id: channelID
+			}
 		});
-	
+
 		const prefix = (user.toLowerCase() === context.user.Name) ? "You" : user;
 		const suffix = (channel.toLowerCase() === context.user.Name) ? "you" : channel;
 
-		if (data.error && data.status === 404) {
+		const [followData] = response.body.data;
+		if (!followData) {
 			const verb = (user.toLowerCase() === context.user.Name) ? "are" : "is";
 			return {
 				reply: `${prefix} ${verb} not following ${suffix}.`
 			};
 		}
-		else {
-			const verb = (user.toLowerCase() === context.user.Name) ? "have" : "has";
-			const delta = sb.Utils.timeDelta(new sb.Date(data.created_at), true);
-			return {
-				reply: `${prefix} ${verb} been following ${suffix} for ${delta}.`
-			};
-		}
+
+		const verb = (user.toLowerCase() === context.user.Name) ? "have" : "has";
+		const delta = sb.Utils.timeDelta(new sb.Date(followData.followed_at), true);
+		return {
+			reply: `${prefix} ${verb} been following ${suffix} for ${delta}.`
+		};
 	}),
 	Dynamic_Description: null
 };
