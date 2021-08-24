0ff16959fd92444699632db1bc373bc129946546
Supinic <supinic@protonmail.com>
2021-08-24 10:39:31 +0200

    adjusted regex to not mention on website mention

--- chat-modules/ping-supi/index.js
+++ chat-modules/ping-supi/index.js
@@ -4,18 +4,18 @@ module.exports = {
 	Description: "This module notifies Supinic whenever he is mentioned (in any channel, across platforms) via Twitch whispers.",
 	Code: (async function pingSupi (context) {
 		const { message, channel, user } = context;
-		const regex = /supi\b|supinic|bupi/i;
-	
+		const regex = /supi\b|supinic(?!\.com)|bupi/i;
+
 		if (typeof this.data.timeout === "undefined") {
 			this.data.timeout = 0;
 		}
-		
+
 		const now = sb.Date.now();
 		if (now > this.data.timeout && regex.test(message) && !user?.Data.skipGlobalPing) {
 			const userName = user?.Name ?? `❓${context.raw.user}`;
 
 			this.data.timeout = now + 1000;
-	
+
 			const pingMessage = `[#${channel.Description ?? channel.Name}] ${userName}: ${message}`;
 			await sb.Platform.get("twitch").pm(pingMessage, await sb.User.get("supinic"));
 		}
