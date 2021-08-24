42a234f7c726493462ee7e3051cfba6d957c11e5
Supinic <supinic@protonmail.com>
2021-08-24 19:11:50 +0200

    added `command` to the list of internal API endpoints

--- api/index.js
+++ api/index.js
@@ -11,6 +11,7 @@ module.exports = (function () {
 	const subroutes = [
 		["afk", "afk.js"],
 		["channel", "channel.js"],
+		["command", "command.js"],
 		["filter", "filter.js"],
 		["health", "health.js"],
 		["reminder", "reminder.js"],
