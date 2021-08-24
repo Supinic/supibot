239b8c8403b837c115d8726a33052945e8ddd785
Supinic <supinic@protonmail.com>
2021-08-24 17:33:15 +0200

    now supports multi-word inputs
    - reworded "word" to "phrase"

--- commands/dictionary/index.js
+++ commands/dictionary/index.js
@@ -35,23 +35,23 @@ module.exports = {
 				reply: "No word provided!"
 			};
 		}
-	
+
 		const index = (typeof context.params.index !== "undefined")
 			? Number(context.params.index)
 			: 0;
-	
+
 		if (!sb.Utils.isValidInteger(index) || index > 100) {
 			return {
 				success: false,
 				reply: "Invalid index number provided!"
 			};
 		}
-	
+
 		const languageIdentifier = context.params.lang ?? context.params.language ?? "en";
 		const language = (languageIdentifier.length < 5)
 			? languageIdentifier
 			: sb.Utils.modules.languageISO.getCode(languageIdentifier);
-	
+
 		if (!language) {
 			return {
 				success: false,
@@ -64,29 +64,29 @@ module.exports = {
 				reply: "Your provided language is not supported by the Dictionary API!"
 			};
 		}
-	
-		const word = encodeURIComponent(args.shift());
+
+		const phrase = encodeURIComponent(args.join(" "));
 		const { statusCode, body: data } = await sb.Got({
-			url: `https://api.dictionaryapi.dev/api/v1/entries/${language}/${word}`,
+			url: `https://api.dictionaryapi.dev/api/v1/entries/${language}/${phrase}`,
 			throwHttpErrors: false,
 			responseType: "json"
 		});
-	
+
 		if (statusCode !== 200) {
 			return {
 				success: false,
 				reply: data.message
 			};
 		}
-		
+
 		const records = data.flatMap(i => Object.entries(i.meaning));
 		const items = records.flatMap(([type, value]) => value.map(item => ({ type, definition: item.definition })));
 		if (items.length === 0) {
 			return {
-				reply: `${data[0].word} (${data[0].phonetic ?? "N/A"}) - no word meaning has been found!`
+				reply: `${data[0].word} (${data[0].phonetic ?? "N/A"}) - no phrase definition has been found!`
 			};
 		}
-	
+
 		const result = items[index];
 		if (!result) {
 			return {
@@ -94,7 +94,7 @@ module.exports = {
 				reply: `There is no item with that index! Maximum index: ${items.length}`
 			};
 		}
-	
+
 		const position = `(index ${index}/${items.length - 1})`;
 		return {
 			reply: `${position} ${data[0].word} (${result.type}): ${result.definition}`
@@ -103,25 +103,25 @@ module.exports = {
 	Dynamic_Description: (async (prefix, values) => {
 		const { languages } = values.getStaticData();
 		const list = languages.map(([code, name]) => `<li><code>${code}</code> - ${name}</li>`).join("");
-	
+
 		return [
-			"Fetches dictionary definitions of provided words, also in specific languages.",
+			"Fetches dictionary definitions of provided phrase, also in specific languages.",
 			"If there's multiple, you can check a different definition by appending the index:# parameter.",
 			"",
-	
+
 			`<code>${prefix}dictionary (word)</code>`,
-			"Will fetch the word's definition in the English language.",
+			"Will fetch the phrase's definition in the English language.",
 			"",
-	
+
 			`<code>${prefix}dictionary lang:fr (word)</code>`,
 			`<code>${prefix}dictionary language:French (word)</code>`,
-			"Both of these will fetch the word's definition in the French language.",
+			"Both of these will fetch the phrase's definition in the French language.",
 			"",
-	
+
 			`<code>${prefix}dictionary (word) index:3</code>`,
-			"Will fetch the word's 4th (counting starts from zero) definition in the English language.",
+			"Will fetch the phrase's 4th (counting starts from zero) definition in the English language.",
 			"",
-	
+
 			"List of supported languages:",
 			list
 		];
