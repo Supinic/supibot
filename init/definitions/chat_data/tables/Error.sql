ac08e1db59adc3870a839175232e63a4e7c44271
Supinic <supinic@protonmail.com>
2021-09-03 17:53:08 +0200

    updated table definition

--- init/definitions/chat_data/tables/Error.sql
+++ init/definitions/chat_data/tables/Error.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS `chat_data`.`Error` (
   `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `Type` enum('Backend','Command','Database','Website','Website - API','Other') NOT NULL,
+  `Origin` enum('Internal','External') DEFAULT NULL,
   `Message` mediumtext DEFAULT NULL,
   `Stack` text NOT NULL DEFAULT '',
   `Arguments` text DEFAULT NULL,
