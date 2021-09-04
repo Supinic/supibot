6d83651c9b47bdd9b5d1a5ba6248af75edbe16c8
Supinic <supinic@protonmail.com>
2021-09-05 00:29:48 +0200

    added `Target` property

--- init/definitions/chat_data/tables/Custom_Data_Property.sql
+++ init/definitions/chat_data/tables/Custom_Data_Property.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS `chat_data`.`Custom_Data_Property` (
 	`Name` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
 	`Type` ENUM('array','boolean','date','function','number','object','regex','string') NOT NULL COLLATE 'utf8mb4_general_ci',
+	`Target` SET('Channel','User') NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
 	PRIMARY KEY (`Name`) USING BTREE
 )
 COLLATE='utf8mb4_general_ci'
