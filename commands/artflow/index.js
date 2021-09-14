0c2b43bddbd71b5e323d364df93fd49945b111f2
Supinic <supinic@protonmail.com>
2021-09-14 20:23:04 +0200

    initial support for artflow generation through the command

--- commands/artflow/index.js
+++ commands/artflow/index.js
@@ -7,8 +7,113 @@ module.exports = {
 	Flags: ["mention","non-nullable","pipe","skip-banphrase"],
 	Params: null,
 	Whitelist_Response: null,
-	Static_Data: null,
+	Static_Data: (() => ({
+		generationUserID: "5555-7f7f-4747-a44b"
+	})),
 	Code: (async function artflow (context, word) {
+		if (context.params.prompt) {
+			return {
+				success: false,
+				reply: `Prompt generation is currently being tested, and should be available soon!`
+			};
+		}
+
+		if (context.params.prompt) {
+			this.data.pendingRequests ??= [];
+			const pending = this.data.pendingRequests.find(i => i.author === context.user.ID);
+			if (!pending) {
+				return {
+					success: false,
+					reply: `You already have a pending request!`
+				};
+			}
+
+			const artflowUserID = this.staticData.generationUserID;
+			const formData = new sb.Got.formData();
+			formData.append("user_id_val", artflowUserID);
+			formData.append("text_prompt", context.params.prompt);
+
+			const response = sb.Got("FakeAgent", {
+				url: "https://artflow.ai/add_to_generation_queue",
+				headers: {
+					"x-requested-with": "XMLHttpRequest",
+					...formData.getHeaders()
+				},
+				body: formData.getBuffer(),
+				referrer: "https://artflow.ai/"
+			});
+
+			if (response.statusCode !== 200) {
+				return {
+					success: false,
+					reply: `Image generation did not succeed! Please try again later.`
+				};
+			}
+
+			this.data.pendingRequests.push({
+				user: context.user.ID,
+				channel: context.channel?.ID ?? null,
+				platform: context.platform?.ID ?? null,
+				imageIndex: response.body.index,
+				prompt: context.params.prompt,
+				interval: setInterval(async function () {
+					const imageIndex = this.imageIndex;
+					const formData = new sb.Got.formData();
+					formData.append("my_work_id", imageIndex);
+
+					const check = await sb.Got("FakeAgent", {
+						url: "https://artflow.ai/check_status",
+						headers: {
+							"x-requested-with": "XMLHttpRequest",
+							...formData.getHeaders()
+						},
+						body: formData.getBuffer(),
+						referrer: "https://artflow.ai/"
+					});
+
+					if (check.statusCode !== 200) {
+						return; // api failed
+					}
+					else if (check.body.current_rank > -1) {
+						return; // still pending
+					}
+
+					const [result] = await sb.Utils.processArtflowData([{
+						filename: check.body.filename,
+						userID: artflowUserID,
+						text_prompt: this.prompt,
+						index: imageIndex,
+						status: "Finished"
+					}]);
+
+					clearInterval(this.interval);
+
+					await sb.Reminder.create({
+						Channel: null,
+						User_From: 1127,
+						User_To: this.user,
+						Text: `Your Artflow prompt "${this.prompt} has finished: ${result.link}`,
+						Schedule: null,
+						Created: new sb.Date(),
+						Private_Message: true,
+						Platform: this.platform ?? 1
+					}, true);
+				}, 300_000)
+			});
+
+			const range = [
+				Math.trunc(response.body.queue_length / 30),
+				Math.trunc(response.body.queue_length / 12)
+			];
+
+			return {
+				reply: sb.Utils.tag.trim `
+					Your Artflow image will be ready in around ${range[0]} to ${range[1]} minutes. 
+					You will receive a system reminder when it is finished
+				`
+			};
+		}
+
 		const imageData = await sb.Query.getRecordset(rs => {
 			rs.select("Prompt", "Upload_Link")
 				.from("data", "Artflow_Image", "Added")
@@ -48,6 +153,14 @@ module.exports = {
 		"",
 
 		`<code>${prefix}artflow (single word)</code>`,
-		"Posts a random picture where the prompt includes your chosen word."
+		"Posts a random picture where the prompt includes your chosen word.",
+		"",
+
+		`<code>${prefix}artflow prompt:(single word)</code>`,
+		`<code>${prefix}artflow prompt:"multiple word prompt"code>`,
+		"Creates an Artflow-generated picture based on your prompt.",
+		"This might take a long time, so you will only be notified via a reminder when it is finished.",
+		"Each user can only have one request pending at a time.",
+		""
 	])
 };
