b43b204904ef2864be90ef6df1bf20bb179ca6ac
Supinic <supinic@protonmail.com>
2021-09-28 18:56:29 +0200

    added `$check markov (channel)`

--- commands/check/index.js
+++ commands/check/index.js
@@ -281,6 +281,47 @@ module.exports = {
 				}
 			},
 			{
+				name: "markov",
+				aliases: [],
+				description: "Posts the link for the word list, for a specified channel's markov module.",
+				execute: async (context, channelName) => {
+					const module = sb.ChatModule.get("async-markov-experiment");
+					if (!module) {
+						return {
+							success: false,
+							reply: `No Markov module is currently available!`
+						};
+					}
+					else if (!channelName) {
+						return {
+							success: false,
+							reply: `No channel provided!`
+						};
+					}
+
+					const channelData = sb.Channel.get(channelName);
+					if (!channelData) {
+						return {
+							success: false,
+							reply: `Provided channel does not exist!`
+						};
+					}
+
+					const markov = module.data.markovs.get(channelData.ID);
+					if (!markov) {
+						return {
+							success: false,
+							reply: "This channel does not have a markov-chain module configured!"
+						};
+					}
+					else {
+						return {
+							reply: `Check this channel's markov word list here: https://supinic.com/data/other/markov/${channelData.ID}/words`
+						};
+					}
+				}
+			},
+			{
 				name: "poll",
 				aliases: [],
 				description: `Checks the currently running Supibot-related poll, if there is any.`,
