92914e303a3a888e9d59a86517a5d3466da01d5b
Supinic <supinic@protonmail.com>
2021-10-15 18:32:09 +0200

    added a heap of configurable parameters

--- commands/randomcommandalias/index.js
+++ commands/randomcommandalias/index.js
@@ -4,14 +4,37 @@ module.exports = {
 	Author: "supinic",
 	Cooldown: 5000,
 	Description: "Posts a random Supibot command alias. Can be configured to create a somewhat precise search query.",
-	Flags: ["mention","non-nullable","pipe"],
-	Params: null,
+	Flags: ["mention","non-nullable","pipe","use-params"],
+	Params: [
+		{ name: "command", type: "string" },
+		{ name: "createdAfter", type: "date" },
+		{ name: "createdBefore", type: "date" },
+		{ name: "invocation", type: "string" },
+		{ name: "name", type: "string" },
+		{ name: "user", type: "string" }
+	],
 	Whitelist_Response: null,
 	Static_Data: null,
-	Code: (async function randomCommandAlias () {
+	Code: (async function randomCommandAlias (context) {
+		const targetUserAlias = (context.params.user)
+			? await sb.User.get(context.params.user)
+			: null;
+
+		if (context.params.user && !targetUserAlias) {
+			return {
+				success: false,
+				reply: `Provided user does not exist!`
+			};
+		}
+
 		const randomAlias = await sb.Query.getRecordset(rs => rs
 			.select("ID", "User_Alias", "Name")
 			.from("data", "Custom_Command_Alias")
+			.where({ condition: context.params.command }, "Command = %s", context.params.command)
+			.where({ condition: context.params.createdAfter }, "Created > %d", context.params.createdAfter)
+			.where({ condition: context.params.createdBefore }, "Created < %d", context.params.createdBefore)
+			.where({ condition: context.params.invocation }, "Invocation %*like*", context.params.invocation)
+			.where({ condition: context.params.name }, "Name = %s", context.params.name)
 			.where("Command IS NOT NULL")
 			.where("Parent IS NULL OR Edited IS NOT NULL") // either an original alias or an edited copy
 			.orderBy("RAND()")
