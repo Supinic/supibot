f79bd91d2a0df1dc80e179afd17a5a6a9a3e92cc
Supinic <supinic@protonmail.com>
2021-10-27 21:21:53 +0200

    added a parameter to force japanese-only transliteration

--- commands/transliterate/index.js
+++ commands/transliterate/index.js
@@ -4,8 +4,10 @@ module.exports = {
 	Author: "supinic",
 	Cooldown: 15000,
 	Description: "Transliterates non-latin text into Latin. Should support most of the languages not using Latin (like Japanese, Chinese, Russian, ...)",
-	Flags: ["mention","non-nullable","pipe"],
-	Params: null,
+	Flags: ["mention","non-nullable","pipe","use-params"],
+	Params: [
+		{ name: "japaneseOnly", type: "boolean" }
+	],
 	Whitelist_Response: null,
 	Static_Data: null,
 	Code: (async function transliterate (context, ...args) {
@@ -15,26 +17,44 @@ module.exports = {
 				reply: "No input provided!"
 			};
 		}
-	
-		const html = await sb.Got({
-			url: "https://ichi.moe/cl/qr",
-			searchParams: new sb.URLParams()
-				.set("r", "htr")
-				.set("q", args.join(" "))
-				.toString()
-		}).text();
-	
-		const $ = sb.Utils.cheerio(html);
-		const words = Array.from($("#div-ichiran-result span.ds-text:not(.hidden) span.ds-word")).map(i => i.firstChild.data);
-		if (words.length > 0) {
-			return {
-				reply: words.join(" ")
-			};
+
+		if (context.params.japaneseOnly) {
+			const response = await sb.Got("GenericAPI", {
+				url: "https://ichi.moe/cl/qr",
+				searchParams: {
+					r: "htr",
+					q: args.join(" ")
+				}
+			});
+
+			const html = response.body;
+			const $ = sb.Utils.cheerio(html);
+			const words = Array.from($("#div-ichiran-result span.ds-text:not(.hidden) span.ds-word")).map(i => i.firstChild.data);
+
+			if (words.length > 0) {
+				return {
+					reply: words.join(" ")
+				};
+			}
+			else {
+				return {
+					success: false,
+					reply: `Could not tranlsliterate specifically from Japanese!`
+				};
+			}
 		}
-	
+
 		return {
 			reply: sb.Utils.transliterate(args.join(" "))
 		};
 	}),
-	Dynamic_Description: null
+	Dynamic_Description: (async (prefix) => [
+		`<code>${prefix}transliterate (text)</code>`,
+		"Transliterates using automatic character detection. Should work for most non-Latin scripts.",
+		"",
+
+		`<code>${prefix}transliterate (text) japaneseOnly:true</code>`,
+		"Transliterates using a website specifically made to transliterate Japanese text only.",
+		"Might not always work."
+	])
 };
