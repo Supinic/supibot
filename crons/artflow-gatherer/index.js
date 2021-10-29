6f65147c464b4fd03c9f8113a5ac40108e44e3ea
Supinic <supinic@protonmail.com>
2021-10-29 16:52:36 +0200

    decreased cron interval from every 5 min to every 30 min

--- crons/artflow-gatherer/index.js
+++ crons/artflow-gatherer/index.js
@@ -1,6 +1,6 @@
 module.exports = {
 	Name: "artflow-gatherer",
-	Expression: "0 */5 * * * *",
+	Expression: "0 */30 * * * *",
 	Description: "Collects image data from artflow.ai",
 	Defer: (() => ({
 		start: 0,
