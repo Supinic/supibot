d7e305115789322c6c912b5d134269f73f912516
Supinic <supinic@protonmail.com>
2021-10-31 18:58:30 +0100

    added loader files to SPM/commands
    - the idea is to use `require("spm/commands")` and receive command definitions

---
+++ commands/index.js
@@ -0,0 +1,45 @@
+const fs = require("fs/promises");
+const path = require("path");
+
+const config = require("./config.json");
+const blacklist = config?.blacklist ?? [];
+const whitelist = config?.whitelist ?? [];
+
+if (blacklist.length > 0 && whitelist.length > 0) {
+	throw new Error("Cannot combine both blacklist and whitelist options");
+}
+
+const nodeList = await fs.readdir(".", {
+	withFileTypes: true
+});
+
+const definitions = [];
+const failed = [];
+const skipped = [];
+
+const directoryList = nodeList.filter(i => i.isDirectory());
+for (const dir of directoryList) {
+	if (blacklist.length > 0 && blacklist.includes(dir.name)) {
+		skipped.push(dir.name);
+		continue;
+	}
+	else if (whitelist.length > 0 && !whitelist.includes(dir.name)) {
+		skipped.push(dir.name);
+		continue;
+	}
+
+	const indexPath = path.join(__dirname, dir.name, "index.js");
+	try {
+		const definition = require(indexPath);
+		definitions.push(definition);
+	}
+	catch {
+		failed.push(dir.name);
+	}
+}
+
+return {
+	definitions,
+	failed,
+	skipped
+};
