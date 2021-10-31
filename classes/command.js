1c3e4628729c18cfef0d41cba519934e1884e351
Supinic <supinic@protonmail.com>
2021-10-31 19:47:07 +0100

    Revert "load staticData by binding the command into the function"
    This reverts commit 3a1ac480b6d70b9fe3c2e2ee97bd25357c01b70b.

--- classes/command.js
+++ classes/command.js
@@ -353,7 +353,7 @@ class Command extends require("./template.js") {
 				});
 			}
 
-			const staticData = staticDataFn.call(this);
+			const staticData = staticDataFn(this);
 
 			if (!staticData || staticData?.constructor?.name !== "Object") {
 				console.warn(`Command ${this.ID} has invalid static data type!`);
