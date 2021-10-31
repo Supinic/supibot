1de058bcfea99820ab4b4c459d8647aea067b33b
Supinic <supinic@protonmail.com>
2021-10-31 19:08:41 +0100

    wrapped SPM/commands into async function
    - added await to top level SPM

--- commands/index.js
+++ commands/index.js
@@ -1,45 +1,47 @@
-const fs = require("fs/promises");
-const path = require("path");
+(async () => {
+	const fs = require("fs/promises");
+	const path = require("path");
 
-const config = require("./config.json");
-const blacklist = config?.blacklist ?? [];
-const whitelist = config?.whitelist ?? [];
+	const config = require("./config.json");
+	const blacklist = config?.blacklist ?? [];
+	const whitelist = config?.whitelist ?? [];
 
-if (blacklist.length > 0 && whitelist.length > 0) {
-	throw new Error("Cannot combine both blacklist and whitelist options");
-}
-
-const nodeList = await fs.readdir(".", {
-	withFileTypes: true
-});
-
-const definitions = [];
-const failed = [];
-const skipped = [];
-
-const directoryList = nodeList.filter(i => i.isDirectory());
-for (const dir of directoryList) {
-	if (blacklist.length > 0 && blacklist.includes(dir.name)) {
-		skipped.push(dir.name);
-		continue;
-	}
-	else if (whitelist.length > 0 && !whitelist.includes(dir.name)) {
-		skipped.push(dir.name);
-		continue;
+	if (blacklist.length > 0 && whitelist.length > 0) {
+		throw new Error("Cannot combine both blacklist and whitelist options");
 	}
 
-	const indexPath = path.join(__dirname, dir.name, "index.js");
-	try {
-		const definition = require(indexPath);
-		definitions.push(definition);
-	}
-	catch {
-		failed.push(dir.name);
-	}
-}
+	const nodeList = await fs.readdir(".", {
+		withFileTypes: true
+	});
 
-module.exports = {
-	definitions,
-	failed,
-	skipped
-};
+	const definitions = [];
+	const failed = [];
+	const skipped = [];
+
+	const directoryList = nodeList.filter(i => i.isDirectory());
+	for (const dir of directoryList) {
+		if (blacklist.length > 0 && blacklist.includes(dir.name)) {
+			skipped.push(dir.name);
+			continue;
+		}
+		else if (whitelist.length > 0 && !whitelist.includes(dir.name)) {
+			skipped.push(dir.name);
+			continue;
+		}
+
+		const indexPath = path.join(__dirname, dir.name, "index.js");
+		try {
+			const definition = require(indexPath);
+			definitions.push(definition);
+		}
+		catch {
+			failed.push(dir.name);
+		}
+	}
+
+	module.exports = {
+		definitions,
+		failed,
+		skipped
+	};
+})();
