fb413e05f9654d1fb7cd14d91215f810b94bc70d
Supinic <supinic@protonmail.com>
2022-01-25 17:51:34 +0100

    refactored command to use Helix (from Kraken)

--- commands/shoutout/index.js
+++ commands/shoutout/index.js
@@ -15,7 +15,7 @@ module.exports = {
 				reply: "No user provided!"
 			};
 		}
-	
+
 		const { controller } = sb.Platform.get("twitch");
 		const targetUserID = await controller.getUserID(target);
 		if (!targetUserID) {
@@ -30,29 +30,61 @@ module.exports = {
 				reply: "This isn't going to make you any more famous, you got to put the work in yourself!"
 			};
 		}
-	
-		const { statusCode, body: data } = await sb.Got("Kraken", `channels/${targetUserID}`);
-		if (statusCode === 422) {
+
+		/*
+			{
+				broadcaster_id,
+				broadcaster_language,
+				broadcaster_login,
+				broadcaster_name,
+				delay,
+				game_id,
+				game_name,
+				title
+			}
+			game_id and game_name and title is "" when never streamed before
+			if delay is `null` the streamer is banned (?)
+		 */
+		const response = await sb.Got("Helix", {
+			url: "channels",
+			searchParams: {
+				broadcaster_id: targetUserID
+			}
+		});
+
+		if (response.statusCode !== 200) {
 			return {
 				success: false,
-				reply: "That user is currently banned!"
+				reply: `That user doesn't exist or has deactivated their account!`
 			};
 		}
-		else if (statusCode !== 200) {
-			console.warn({ statusCode, data });
-			return {
-				success: false,
-				reply: "Cannot find that user's last stream data!"
-			};
-		}
-	
-		const { game, name, url } = data;
-		const gameString = (game !== null)
-			? `They last streamed in game/category ${game}.`
+
+		const {
+			broadcaster_login: login,
+			broadcaster_name: name,
+			game_name: game,
+			title,
+			delay
+		} = response.body.data[0];
+
+		const url = `https://twitch.tv/${login}`;
+		const nameString = (login.toLowerCase() === name.toLowerCase())
+			? name
+			: `${name} (${login})`;
+
+		const bannedString = (delay === null) ? "They're likely banned! 🚨" : "";
+		const titleString = (title) ? ` with title "${title}".` : ".";
+		const gameString = (game)
+			? `They last streamed in game/category ${game}${titleString}`
 			: "";
-	
+
 		return {
-			reply: sb.Utils.tag.trim `Shout out to ${name}! ${gameString} Check them out here: ${url}`
+			reply: sb.Utils.tag.trim `
+				Shout out to ${nameString}!
+				${gameString} 
+				${bannedString}
+				Check them out here: ${url}
+			`
 		};
 	}),
 	Dynamic_Description: null
