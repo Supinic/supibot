91106f9f4ecc5915cf44b4b64a732c0cdcdfc234
Supinic <supinic@protonmail.com>
2022-02-25 18:47:40 +0100

    bumped user agent string to Chrome 98 @ Windows 10

--- got/fake-agent/index.mjs
+++ got/fake-agent/index.mjs
@@ -3,7 +3,7 @@ export default {
 	optionsType: "object",
 	options: {
 		headers: {
-			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4586.70 Safari/537.36"
+			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36"
 		}
 	},
 	parent: "Global",
