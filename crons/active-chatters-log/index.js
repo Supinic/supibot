01b4cd565fe4d080520ae5786ddab16d1709a4b0
Supinic <supinic@protonmail.com>
2022-03-15 19:51:45 +0100

    early exit if used modules aren't loaded yet

--- crons/active-chatters-log/index.js
+++ crons/active-chatters-log/index.js
@@ -5,12 +5,16 @@ module.exports = {
 	Defer: null,
 	Type: "Bot",
 	Code: (async function activeChattersLog () {
+		if (!sb.Query || !sb.User) {
+			return;
+		}
+
 		const row = await sb.Query.getRow("data", "Active_Chatter_Log");
 		row.setValues({
 			Amount: sb.User.data.size,
 			Timestamp: new sb.Date().discardTimeUnits("s", "ms")
 		});
-	
+
 		await row.save();
 	})
 };
