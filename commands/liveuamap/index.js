9190885545da6144a2383c704618bce0b3a805cf
Supinic <supinic@protonmail.com>
2022-03-18 16:57:22 +0100

    added support for selecting a language from the website

--- commands/liveuamap/index.js
+++ commands/liveuamap/index.js
@@ -4,21 +4,42 @@ module.exports = {
 	Author: "supinic",
 	Cooldown: 10000,
 	Description: "Gets a recent event from the 2022 Russia/Ukraine war",
-	Flags: ["mention","non-nullable","pipe"],
-	Params: null,
+	Flags: ["mention","non-nullable","pipe","use-params"],
+	Params: [
+		{ name: "lang", type: "string" }
+	],
 	Whitelist_Response: null,
 	Static_Data: (() => ({
-		cacheKey: "liveuamap-data",
-		threshold: 10
+		baseCacheKey: "liveuamap-data",
+		threshold: 10,
+		supportedLanguageCodes: ["en", "ru", "uk", "pl"]
 	})),
-	Code: (async function liveUaMap () {
-		const { cacheKey } = this.staticData;
+	Code: (async function liveUaMap (context) {
+		const { baseCacheKey, supportedLanguageCodes } = this.staticData;
+
+		const inputLanguage = context.params.lang ?? "en";
+		const languageCode = sb.Utils.modules.languagesISO.getCode(inputLanguage);
+		if (!languageCode) {
+			return {
+				success: false,
+				reply: `Could not parse your provided language!`
+			};
+		}
+		else if (!supportedLanguageCodes.includes(languageCode)) {
+			const supportedLanguageNames = supportedLanguageCodes.map(i => sb.Utils.modules.languagesISO.getName(i));
+			return {
+				success: false,
+				reply: `Your provided language is not supported! Use one of: ${supportedLanguageNames.join(", ")}`
+			};
+		}
+
+		const cacheKey = `${baseCacheKey}-${languageCode}`;
 		let data = await this.getCacheData(cacheKey);
 		if (!data) {
 			let response;
 			try {
 				response = await sb.Got("FakeAgent", {
-					url: "https://liveuamap.com/",
+					url: `https://liveuamap.com/${languageCode}`,
 					responseType: "text",
 					timeout: 60_000
 				});
@@ -67,7 +88,7 @@ module.exports = {
 
 			data = result;
 			await this.setCacheData(cacheKey, data, {
-				expiry: 300_000 // 5 minutes
+				expiry: 600_000 // 5 minutes
 			});
 		}
 
