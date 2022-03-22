362bd90994a2654c95bcc3473d68f0fc796e74e9
Supinic <supinic@protonmail.com>
2022-03-22 17:14:18 +0100

    added parameter `delimiter` to let users specify their own

--- commands/pick/index.js
+++ commands/pick/index.js
@@ -4,8 +4,10 @@ module.exports = {
 	Author: "supinic",
 	Cooldown: 10000,
 	Description: "Picks a single word from the provided list of words in a message.",
-	Flags: ["mention","pipe"],
-	Params: null,
+	Flags: ["mention","pipe","use-params"],
+	Params: [
+		{ name: "delimiter", type: "string" }
+	],
 	Whitelist_Response: null,
 	Static_Data: null,
 	Code: (async function pick (context, ...words) {
@@ -15,8 +17,12 @@ module.exports = {
 			};
 		}
 
-		// normalize input - there might be arguments with other or multiple whitespace inside of them
-		const prepared = words.join(" ").split(/\s+/).filter(Boolean);
+		const splitRegex = (context.params.delimiter)
+			? new RegExp(context.params.delimiter)
+			: /\s+/;
+
+		// normalize input - there might be arguments with other or multiple whitespace inside them
+		const prepared = words.join(" ").split(splitRegex).filter(Boolean);
 		return {
 			reply: sb.Utils.randArray(prepared),
 			cooldown: (context.append.pipe)
