7bd51b49f7a0244dbd980048cea4f8469f8ba26c
Supinic <supinic@protonmail.com>
2022-04-26 19:16:25 +0200

    added the `fixed` parameter
    - dynamic description updated too

--- commands/math/index.js
+++ commands/math/index.js
@@ -6,6 +6,7 @@ module.exports = {
 	Description: "Does math. For more info, check the documentation for math.js",
 	Flags: ["external-input","mention","non-nullable","pipe","use-params"],
 	Params: [
+		{ name: "fixed", type: "boolean" },
 		{ name: "precision", type: "number" }
 	],
 	Whitelist_Response: null,
@@ -33,6 +34,10 @@ module.exports = {
 			parameters.precision = context.params.precision;
 		}
 
+		if (context.params.fixed !== false) {
+			parameters.expr = `format((${parameters.expr}), { notation: "fixed" })`;
+		}
+
 		const response = await sb.Got("GenericAPI", {
 			method: "POST",
 			responseType: "json",
@@ -48,8 +53,13 @@ module.exports = {
 			};
 		}
 		else {
+			let reply = response.body.result;
+			if (context.params.fixed !== false) {
+				reply = reply.replace(/(^")|("$)/g, "");
+			}
+
 			return {
-				reply: response.body.result
+				reply
 			};
 		}
 	}),
@@ -58,16 +68,27 @@ module.exports = {
 		`Look here for more info: <a href="https://mathjs.org/">mathjs documentation</a>`,
 		"",
 
+		"Simple math:",
 		`<code>${prefix}math 1+1</code>`,
 		"2",
 		"",
 
-		`<code>${prefix}math e^(i*pi)</code>`,
-		"-1 + 1.2246467991473532e-16i",
-		"",
-
+		"Conversions between units:",
 		`<code>${prefix}math 100 inches to cm</code>`,
 		"254 cm",
-		""
+		"",
+
+		"Precision:",
+		"When <code>precision</code> parameter is provided, the result will be trimmed to keep that many precision digits in.",
+		`<code>${prefix}math (expression) <u>precision:(number)</u></code>`,
+		`<code>${prefix}math 1/3 precision:2</code> => 0.33`,
+		`<code>${prefix}math 2/3 precision:6</code> => 0.666666`,
+		"",
+
+		"Fixed notation:",
+		"When <code>fixed:false</code> is provided, the result use the default exponential notation.",
+		`<code>${prefix}math (expression) <u>fixed:false</u></code>`,
+		`<code>${prefix}math 1000 * 1000 fixed:true</code> => 1000000`,
+		`<code>${prefix}math 1000 * 1000 fixed:false</code> => 1e+6`
 	])
 };
