595047c43240a63a1b305f2f1ed1c501d77448a9
Heryin <54620595+Heryin@users.noreply.github.com>
2022-05-20 18:21:07 +0200

    Update index.js (#74)
    Updated the description based on the fact that the bot doesn't have to be moderated for the command to work anymore.

--- commands/russianroulette/index.js
+++ commands/russianroulette/index.js
@@ -3,7 +3,7 @@ module.exports = {
 	Aliases: ["rr"],
 	Author: "supinic",
 	Cooldown: 15000,
-	Description: "Play the roulette. If you win, nothing happens; if you lose, you get timed out. You can add a number 1-600 (default: 1) which says how long you will be timed out, should you lose. Only works in channels where Supibot is moderator.",
+	Description: "Play the roulette. If you win, nothing happens; if you lose, you get timed out. You can add a number 1-600 (default: 1) which says how long you will be timed out, should you lose. You can use the command anywhere, but you can only get timed out in a channel where Supibot is a moderator.",
 	Flags: ["mention","pipe"],
 	Params: null,
 	Whitelist_Response: null,
