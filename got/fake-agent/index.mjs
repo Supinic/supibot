7fe4b9f44cb1cb7afa70735761a57642fbf016de
Supinic <supinic@protonmail.com>
2022-06-28 18:01:55 +0200

    updated fake-agent user-agent header

--- got/fake-agent/index.mjs
+++ got/fake-agent/index.mjs
@@ -3,7 +3,7 @@ export default {
 	optionsType: "object",
 	options: {
 		headers: {
-			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36"
+			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.53 Safari/537.36"
 		}
 	},
 	parent: "Global",
