c64ee1a41f31862d686112df3f720956d38cbe64
Supinic <supinic@protonmail.com>
2022-07-07 19:41:49 +0200

    removed redundant non-capturing group

--- commands/shuffle/index.js
+++ commands/shuffle/index.js
@@ -17,17 +17,17 @@ module.exports = {
 				reply: "No input provided!"
 			};
 		}
-	
+
 		let reply;
 		if (context.params.fancy) {
 			const result = [];
-			const message = args.join(" ").split(/\b|(?:(\s))/).filter(Boolean);
+			const message = args.join(" ").split(/\b|\s/).filter(Boolean);
 			while (message.length > 0) {
 				const randomIndex = sb.Utils.random(0, message.length - 1);
 				result.push(message[randomIndex]);
 				message.splice(randomIndex, 1);
 			}
-	
+
 			reply = result.join(" ").replace(/\s+/g, " ");
 		}
 		else {
@@ -38,10 +38,10 @@ module.exports = {
 				result.push(message[randomIndex]);
 				message.splice(randomIndex, 1);
 			}
-	
+
 			reply = result.join(" ");
 		}
-	
+
 		return {
 			reply,
 			cooldown: {
@@ -52,11 +52,11 @@ module.exports = {
 	Dynamic_Description: (async (prefix) => [
 		"For a given message, shuffles the words around.",
 		"",
-	
+
 		`<code>${prefix}shuffle this is a random message</code>`,
 		`a random is message this`,
 		"",
-	
+
 		`<code>${prefix}shuffle fancy:true (this) isn't a random! message</code>`,
 		`) isn a ' ! random this ( message`
 	])
