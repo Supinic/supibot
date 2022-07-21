53a69412205653d7b353686e1ff3b899de474f74
Supinic <supinic@protonmail.com>
2022-07-21 22:27:22 +0200

    updated the module's string matching

--- chat-modules/wanna-become-famous/index.js
+++ chat-modules/wanna-become-famous/index.js
@@ -12,7 +12,7 @@ module.exports = {
 
 		let type = "";
 		const msg = sb.Utils.removeAccents(context.message).toLowerCase();
-		if (msg.includes("become famous?") && msg.includes("bigfollows")) {
+		if (msg.includes("become famous?")) {
 			type = "becoming famous";
 		}
 		else if (msg.includes("get raided")) {
