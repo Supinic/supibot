4964d973f2bf727ca573221ebf7d78cab35b1768
Supinic <supinic@protonmail.com>
2022-07-29 17:37:26 +0200

    experimental support for inline discord embeds

--- commands/dalle/index.js
+++ commands/dalle/index.js
@@ -1,3 +1,4 @@
+const { createEmbeds } = require("./discord-embed");
 module.exports = {
 	Name: "dalle",
 	Aliases: [],
@@ -12,11 +13,40 @@ module.exports = {
 	],
 	Whitelist_Response: null,
 	Static_Data: null,
+	Dynamic_Description: (async (prefix) => [
+		"Creates a DALL-E AI generated image, based on your prompt.",
+		"Alternatively, searches for an existing prompt someone else has created.",
+		"",
+
+		`<code>${prefix}dalle (your prompt here)</code>`,
+		`<code>${prefix}dalle Billy Herrington as president of the United States</code>`,
+		"Creates a set of nine pictures with your prompt, and posts a link to it in the chat.",
+		"Warning: This creation can take up to 2-5 minutes, so be patient. When the image is being generated, you cannot use any other commands until it finishes.",
+		"Even still, the generation service can be overloaded at times (usually in the evening EU time), in which case you'll have to try again later.",
+		"<u>Special cooldown: <code>60 seconds</code></u>",
+		"",
+
+		`<code>${prefix}dalle search:(your search here)</code>`,
+		`<code>${prefix}dalle search:forsen</code>`,
+		`<code>${prefix}dalle search:"my nightmare tonight"</code>`,
+		"Searches for an existing image set, based on your prompt - that someone has created before.",
+		"",
+
+		`<code>${prefix}dalle id:(post ID)</code>`,
+		`<code>${prefix}dalle id:e5832f798a41d335</code>`,
+		"Looks up a specific post by its ID",
+		"",
+
+		`<code>${prefix}dalle random:true</code>`,
+		"Posts a random image set that someone has created before."
+	]),
 	Code: (async function dallE (context, ...args) {
+		const { createEmbeds } = require("./discord-embed.js");
+
 		if (context.params.search || context.params.random || context.params.id) {
 			const { id, random, search } = context.params;
 			const image = await sb.Query.getRecordset(rs => rs
-				.select("ID", "Prompt")
+				.select("ID", "Prompt", "Created", "Creation_Time")
 				.from("data", "DALL-E")
 				.orderBy("RAND()")
 				.where(
@@ -42,8 +72,18 @@ module.exports = {
 				};
 			}
 
+			const discordData = {};
+			if (context.platform.Name === "discord") {
+				discordData.embeds = createEmbeds(image.ID, {
+					prompt: image.Prompt,
+					created: image.Created,
+					creationTime: image.Creation_Time
+				});
+			}
+
 			return {
-				reply: `https://supinic.com/data/dall-e/detail/${image.ID} DALL-E image set for "${image.Prompt}"`
+				reply: `https://supinic.com/data/dall-e/detail/${image.ID} DALL-E image set for "${image.Prompt}"`,
+				discord: discordData
 			};
 		}
 
@@ -135,19 +175,31 @@ module.exports = {
 
 		const jsonImageData = images.map(i => i.replace(/\\n/g, ""));
 		const row = await sb.Query.getRow("data", "DALL-E");
+		const ID = hash.digest().toString("hex").slice(0, 16);
+		const created = new sb.Date();
+		const creationTime = (Number(nanoExecutionTime) / 1e9);
+
 		row.setValues({
-			ID: hash.digest().toString("hex").slice(0, 16),
+			ID,
 			User_Alias: context.user.ID,
 			Channel: context.channel?.ID ?? null,
 			Prompt: query,
-			Created: new sb.Date(),
-			Creation_Time: (Number(nanoExecutionTime) / 1e9),
+			Created: created,
+			Creation_Time: creationTime,
 			Data: JSON.stringify(jsonImageData)
 		});
 
 		await row.save({ skipLoad: false });
 
-		const { ID } = row.values;
+		const discordData = {};
+		if (context.platform.Name === "discord") {
+			discordData.embeds = createEmbeds(ID, {
+				prompt: query,
+				created,
+				creationTime
+			});
+		}
+
 		return {
 			cooldown: {
 				user: context.user.ID,
@@ -155,34 +207,9 @@ module.exports = {
 				channel: null,
 				length: 60_000
 			},
-			reply: `https://supinic.com/data/dall-e/detail/${ID} DALL-E image set for prompt "${query}"`
+			discord: discordData,
+			reply: `https://supinic.com/data/dall-e/detail/${ID} DALL-E image set for prompt "${query}"`,
+			removeEmbeds: true
 		};
-	}),
-	Dynamic_Description: (async (prefix) => [
-		"Creates a DALL-E AI generated image, based on your prompt.",
-		"Alternatively, searches for an existing prompt someone else has created.",
-		"",
-
-		`<code>${prefix}dalle (your prompt here)</code>`,
-		`<code>${prefix}dalle Billy Herrington as president of the United States</code>`,
-		"Creates a set of nine pictures with your prompt, and posts a link to it in the chat.",
-		"Warning: This creation can take up to 2-5 minutes, so be patient. When the image is being generated, you cannot use any other commands until it finishes.",
-		"Even still, the generation service can be overloaded at times (usually in the evening EU time), in which case you'll have to try again later.",
-		"<u>Special cooldown: <code>60 seconds</code></u>",
-		"",
-
-		`<code>${prefix}dalle search:(your search here)</code>`,
-		`<code>${prefix}dalle search:forsen</code>`,
-		`<code>${prefix}dalle search:"my nightmare tonight"</code>`,
-		"Searches for an existing image set, based on your prompt - that someone has created before.",
-		"",
-
-		`<code>${prefix}dalle id:(post ID)</code>`,
-		`<code>${prefix}dalle id:e5832f798a41d335</code>`,
-		"Looks up a specific post by its ID",
-		"",
-
-		`<code>${prefix}dalle random:true</code>`,
-		"Posts a random image set that someone has created before."
-	])
+	})
 };
