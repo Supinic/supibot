476420d5bff94dc15b29a28fff8da96c182fe661
EuleHaken <73948555+EuleHaken@users.noreply.github.com>
2022-08-11 19:42:36 +0200

    Specify a channel as context for command "firstline"  (#87)
    * allow for specifying a channel
    * replaced function parameter `channel` with command parameter `channel`
    * added a channel article based on if the specified channel is the current one
    * added a user article based on if the specified user is the current one
    * added dynamic description

--- commands/firstline/index.js
+++ commands/firstline/index.js
@@ -3,20 +3,37 @@ module.exports = {
 	Aliases: ["fl"],
 	Author: "supinic",
 	Cooldown: 5000,
-	Description: "Posts the target user's first chat line in the context of the current channel, and the date they sent it.",
+	Description: "Posts the target user's first chat line in the context of the current or a specified channel, and the date they sent it.",
 	Flags: ["external-input","mention","opt-out","pipe"],
 	Params: [
+		{ name: "channel", type: "string" },
 		{ name: "textOnly", type: "boolean" }
 	],
 	Whitelist_Response: null,
 	Static_Data: null,
 	Code: (async function firstLine (context, user) {
-		if (!context.channel) {
+		const { channel } = context.params;
+		if (!context.channel && !channel) {
 			return {
 				success: false,
-				reply: "This command is not available in private messages!"
+				reply: "In order to use this command in private messages, you have to provide a channel parameter! Check the command's extended help for more info."
 			};
 		}
+
+		const targetChannel = (channel)
+			? sb.Channel.get(channel)
+			: context.channel;
+
+		if (!targetChannel) {
+			return {
+				success: false,
+				reply: "Provided channel not found in the database!"
+			}
+		}
+
+		const channelArticle = (targetChannel === context.channel)
+			? "this"
+			: "that";
 
 		const targetUser = (user)
 			? await sb.User.get(user)
@@ -29,11 +46,15 @@ module.exports = {
 			};
 		}
 
+		const userArticle = (targetUser === context.user)
+			? "You have"
+			: "That user has";
+
 		let metaData = await sb.Query.getRecordset(rs => rs
 			.select("First_Message_Posted", "First_Message_Text")
 			.from("chat_data", "Message_Meta_User_Alias")
 			.where("User_Alias = %n", targetUser.ID)
-			.where("Channel = %n", context.channel.ID)
+			.where("Channel = %n", targetChannel.ID)
 			.limit(1)
 			.single()
 		);
@@ -41,15 +62,15 @@ module.exports = {
 		if (!metaData) {
 			return {
 				success: false,
-				reply: "That user has not said anything in this channel!"
+				reply: `${userArticle} not said anything in ${channelArticle} channel!`
 			};
 		}
 		else if (!metaData.First_Message_Posted) {
-			const dbChannelName = context.channel.getDatabaseName();
+			const dbChannelName = targetChannel.getDatabaseName();
 			if (!await sb.Query.isTablePresent("chat_line", dbChannelName)) {
 				return {
 					success: false,
-					reply: `No first line data is available for that user in this channel!`
+					reply: `${userArticle} no first line data available in ${channelArticle} channel!`
 				};
 			}
 
@@ -65,7 +86,7 @@ module.exports = {
 			const row = await sb.Query.getRow("chat_data", "Message_Meta_User_Alias");
 			await row.load({
 				User_Alias: targetUser.ID,
-				Channel: context.channel.ID
+				Channel: targetChannel.ID
 			});
 
 			row.setValues({
@@ -92,7 +113,7 @@ module.exports = {
 			partialReplies: [
 				{
 					bancheck: false,
-					message: `${prefix} first message in this channel was (${sb.Utils.timeDelta(metaData.First_Message_Posted)}):`
+					message: `${prefix} first message in ${channelArticle} channel was (${sb.Utils.timeDelta(metaData.First_Message_Posted)}):`
 				},
 				{
 					bancheck: true,
@@ -101,5 +122,32 @@ module.exports = {
 			]
 		};
 	}),
-	Dynamic_Description: null
+	Dynamic_Description: (async (prefix) => [
+		"Posts the target user's first chat line.",
+		"",
+
+		"You can specify a channel as context with the channel parameter (see examples below).",
+		"If you don't specify a channel, the current channel will be used as context.",
+		"The command can only be used in whispers, if you specify a valid channel.",
+		"",
+
+		`<code>${prefix}firstline</code>`,
+		"Your first message in this channel was...",
+		"",
+
+		`<code>${prefix}fl (user)</code>`,
+		"That user's first message in this channel was...",
+		"",
+
+		`<code>${prefix}fl channel:(channel)</code>`,
+		"Your first message in the specified channel was...",
+		"",
+
+		`<code>${prefix}fl (user) channel:(channel)</code>`,
+		"That user's first message in the specified channel was...",
+		"",
+
+		`<code>${prefix}fl textOnly:true</code>`,
+		"(first message text here)"
+	])
 };
