486e7ce8d71fe8f232595eeed5cfdf173094f076
Supinic <supinic@protonmail.com>
2022-08-11 13:05:01 +0200

    also added the status code into the response

--- commands/randomline/justlog.js
+++ commands/randomline/justlog.js
@@ -47,7 +47,7 @@ const getRandomChannelLine = async function (channelID) {
 	else if (response.statusCode !== 200) {
 		return {
 			success: false,
-			reason: "The channel logs are not available at the moment! Try again later."
+			reason: `The channel logs are not available at the moment (status code ${response.statusCode})! Try again later.`
 		};
 	}
 
