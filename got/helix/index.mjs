784433c36527ba5541bb6660457b88c985d1e76b
Supinic <supinic@protonmail.com>
2022-08-12 15:10:03 +0200

    adjusted Helix token usage

--- got/helix/index.mjs
+++ got/helix/index.mjs
@@ -6,7 +6,7 @@ export default {
 			throw new Error("Helix sb.Got instance cannot initialize - missing client-id");
 		}
 
-		const token = sb.Config.get("TWITCH_APP_ACCESS_TOKEN", false) ?? sb.Config.get("TWITCH_OAUTH", false);
+		const token = sb.Config.get("TWITCH_OAUTH", false);
 		if (!token) {
 			throw new Error("Helix sb.Got instance cannot initialize - missing token");
 		}
@@ -15,7 +15,7 @@ export default {
 			prefixUrl: "https://api.twitch.tv/helix",
 			headers: {
 				"Client-ID": sb.Config.get("TWITCH_CLIENT_ID"),
-				Authorization: `Bearer ${token}`
+				Authorization: `Bearer ${token.replace("oauth:", "")}`
 			}
 		};
 	}),
