bd3d760960cdf245d13d4c2474d36a02c68c4000
Supinic <supinic@protonmail.com>
2022-08-12 16:32:49 +0200

    moved `$stf` as a new sub-type of `$set/unset`
    - finally gave this command a proper functionality and home

--- commands/set/index.js
+++ commands/set/index.js
@@ -126,6 +126,44 @@ module.exports = {
 			};
 		};
 
+		const fetchTrackIDs = async (tracks) => {
+			const stringIDs = tracks.map(i => {
+				const type = sb.Utils.modules.linkParser.autoRecognize(i);
+				if (!type) {
+					return null;
+				}
+
+				return sb.Utils.modules.linkParser.parseLink(i);
+			}).filter(Boolean);
+
+			if (stringIDs.length === 0) {
+				return [];
+			}
+
+			return await sb.Query.getRecordset(rs => rs
+				.select("ID")
+				.from("music", "Track")
+				.where("Link IN %s+", stringIDs)
+				.flat("ID")
+			);
+		};
+
+		const updateTrackFavouriteStatus = async (IDs, status) => {
+			for (const ID of IDs) {
+				const row = await sb.Query.getRow("music", "Track");
+				await row.load({ User_Alias: context.user.ID, Track: ID }, true);
+				if (!row.loaded) {
+					row.setValues({
+						User_Alias: context.user.ID,
+						Track: ID
+					});
+				}
+
+				row.values.Active = status;
+				await row.save({ skipLoad: true });
+			}
+		};
+
 		return {
 			availableFlags,
 			variables: [
@@ -818,6 +856,28 @@ module.exports = {
 
 						return {
 							reply: `Successfully removed your timer "${name}".`
+						};
+					}
+				},
+				{
+					name: "trackfavourite",
+					aliases: ["tf", "track-fav", "trackfavorite", "track-favourite", "track-favorite"],
+					parameter: "arguments",
+					description: `Lets you favourite a track in Supinic's track list from chat. Not toggleable, only sets the favourite. You can unset or check the favourite on the website. https://supinic.com/track/gachi/list`,
+					set: async (context, ...args) => {
+						const IDs = await fetchTrackIDs(args);
+						await updateTrackFavouriteStatus(IDs, true);
+
+						return {
+							reply: `Successfully set ${IDs.length} track(s) as your favourite.`
+						};
+					},
+					unset: async (context, ...args) => {
+						const IDs = await fetchTrackIDs(args);
+						await updateTrackFavouriteStatus(IDs, false);
+
+						return {
+							reply: `Successfully unset ${IDs.length} track(s) as your favourite.`
 						};
 					}
 				}
