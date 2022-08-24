6be07cb4627fda65bc265b2c39917be4804a142c
Supinic <supinic@protonmail.com>
2022-08-24 18:24:54 +0200

    added new subscription type

--- commands/subscribe/index.js
+++ commands/subscribe/index.js
@@ -28,6 +28,15 @@ module.exports = {
 				}
 			},
 			{
+				name: "Runelite",
+				aliases: ["Runelite updates"],
+				notes: "Every hour, supibot checks for new releases of Runelite. If a change is detected, you will be notified in #supinic.",
+				response: {
+					added: "You will now be pinged whenever a new release of Runelite is detected.",
+					removed: "You will no longer receive pings when Runelite releases an uupdate."
+				}
+			},
+			{
 				name: "GGG tracker",
 				aliases: ["ggg", "poe"],
 				notes: "Every minute, Supibot checks for new posts by GGG staff on their forums and Reddit. If you are subscribed, a new post like this will ping you in #supinic.",
@@ -52,7 +61,7 @@ module.exports = {
 								Data: "{}",
 								Active: true
 							});
-	
+
 							await subscription.save();
 						}
 						else if (invocation === "unsubscribe") {
@@ -62,22 +71,22 @@ module.exports = {
 							};
 						}
 					}
-	
+
 					const data = JSON.parse(subscription.values.Data ?? "{}");
 					data.channels = data.channels ?? [];
-	
+
 					const twitch = sb.Platform.get("twitch");
 					const channels = args
 						.map(i => sb.Channel.get(i.toLowerCase(), twitch))
 						.filter(i => i !== null && i.Type !== "Inactive")
 						.map(i => i.ID);
-	
+
 					if (channels.length === 0) {
 						if (invocation === "unsubscribe") {
 							subscription.values.Active = false;
 							subscription.values.Data = null;
 							await subscription.save();
-	
+
 							return {
 								reply: "Successfully unsubscribed from all channels going live."
 							};
@@ -96,29 +105,29 @@ module.exports = {
 							};
 						}
 					}
-	
+
 					let response;
 					const lengthBefore = data.channels.length;
 					if (invocation === "subscribe") {
 						data.channels.push(...channels);
 						data.channels = data.channels.filter((i, ind, arr) => arr.indexOf(i) === ind);
-	
+
 						response = (data.channels.length > lengthBefore)
 							? `Successfully subscribed to ${data.channels.length - lengthBefore} channels going live.`
 							: "You did not subscribe to any new channels.";
 					}
 					else if (invocation === "unsubscribe") {
 						data.channels = data.channels.filter(i => !channels.includes(i));
-	
+
 						response = (data.channels.length < lengthBefore)
 							? `Successfully unsubscribed from ${lengthBefore - data.channels.length} channels going live.`
 							: "You did not unsubscribe from any channels.";
 					}
-	
+
 					subscription.values.Active = (data.channels.length !== 0);
 					subscription.values.Data = JSON.stringify(data, null, 4);
 					await subscription.save();
-	
+
 					return {
 						reply: response
 					};
@@ -142,7 +151,7 @@ module.exports = {
 				reply: "No event provided! Check the command's extended help for a list."
 			};
 		}
-	
+
 		type = type.toLowerCase();
 		const event = this.staticData.types.find(i => i.name === type || i.aliases.includes(type));
 		if (!event) {
@@ -151,7 +160,7 @@ module.exports = {
 				reply: "Incorrect event provided! Check the extended command's help for a list."
 			};
 		}
-	
+
 		const { invocation } = context;
 		const subData = await sb.Query.getRecordset(rs => rs
 			.select("ID", "Active")
@@ -161,16 +170,16 @@ module.exports = {
 			.limit(1)
 			.single()
 		);
-	
+
 		if (typeof event.handler === "function") {
 			const subscription = await sb.Query.getRow("data", "Event_Subscription");
 			if (subData?.ID) {
 				await subscription.load(subData.ID);
 			}
-	
+
 			return event.handler(context, subscription, ...args);
 		}
-	
+
 		const response = (invocation === "subscribe") ? event.response.added : event.response.removed;
 		if (subData) {
 			if (
@@ -183,13 +192,13 @@ module.exports = {
 					reply: `You are already ${invocation}d ${preposition} this event!`
 				};
 			}
-	
+
 			await sb.Query.getRecordUpdater(rs => rs
 				.update("data", "Event_Subscription")
 				.set("Active", !subData.Active)
 				.where("ID = %n", subData.ID)
 			);
-	
+
 			return {
 				reply: `Sucessfully ${invocation}d. ${response}`
 			};
@@ -201,7 +210,7 @@ module.exports = {
 					reply: `You are not subscribed to this event, so you cannot unsubscribe!`
 				};
 			}
-	
+
 			const row = await sb.Query.getRow("data", "Event_Subscription");
 			row.setValues({
 				User_Alias: context.user.ID,
@@ -209,7 +218,7 @@ module.exports = {
 				Type: event.name,
 				Active: true
 			});
-	
+
 			await row.save();
 			return {
 				reply: `Sucessfully subscribed. ${response}`
@@ -225,20 +234,20 @@ module.exports = {
 				<br>${i.notes}			
 			</li>
 		`).join("");
-	
+
 		return [
 			"Subscribes or unsubscribes your account from an event in Supibot's database.",
 			"Depending on the event, you will be notified in different ways.",
 			"",
-	
+
 			`<code>${prefix}subscribe (type)</code>`,
 			"You will be subscribed to a given event.",
 			"",
-	
+
 			`<code>${prefix}unsubscribe (type)</code>`,
 			"You will be unsubscribed from a given event.",
 			"",
-	
+
 			"List of available events:",
 			`<ul>${typesList}</ul>`
 		];
