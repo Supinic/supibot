a9e5550071b088b3a1609dd071955a307cccccc2
Supinic <supinic@protonmail.com>
2022-08-31 17:58:40 +0200

    minor refactor - new parameter, cooldown reduction
    - cooldown: 60s -> 30s
    - added parameter `imageSummary` - uses the `v1/simple` endpoint and Imgur to upload a picture summary for the query instead

--- commands/query/index.js
+++ commands/query/index.js
@@ -2,10 +2,12 @@ module.exports = {
 	Name: "query",
 	Aliases: ["wolframalpha", "wa"],
 	Author: "supinic",
-	Cooldown: 60000,
+	Cooldown: 30000,
 	Description: "Wolfram Alpha query",
 	Flags: ["external-input","mention","non-nullable","pipe"],
-	Params: null,
+	Params: [
+		{ name: "imageSummary", type: "boolean" }
+	],
 	Whitelist_Response: null,
 	Static_Data: null,
 	Code: (async function query (context, ...args) {
@@ -16,21 +18,71 @@ module.exports = {
 			};
 		}
 
-		const rawData = await sb.Got({
-			throwHttpErrors: false,
-			url: "http://api.wolframalpha.com/v1/result",
-			searchParams: {
-				appid: sb.Config.get("API_WOLFRAM_ALPHA_APPID"),
-				i: args.join(" ")
-			}
-		}).text();
+		if (context.params.imageSummary) {
+			const response = await sb.Got({
+				url: "http://api.wolframalpha.com/v1/simple",
+				throwHttpErrors: false,
+				responseType: "buffer",
+				searchParams: {
+					appid: sb.Config.get("API_WOLFRAM_ALPHA_APPID"),
+					i: args.join(" "),
+					width: 500,
+					units: "metric",
+					background: "193555",
+					foreground: "white",
+					layout: "labelbar"
+				}
+			});
 
-		const data = sb.Config.get("WOLFRAM_QUERY_CENSOR_FN")(rawData);
-		return {
-			reply: (context.platform.Name === "discord")
-				? `\`${data}\``
-				: data
-		};
+			if (response.statusCode !== 200) {
+				return {
+					success: false,
+					reply: response.body.toString()
+				};
+			}
+
+			const uploadResult = await sb.Utils.uploadToImgur(response.data);
+			if (!uploadResult.link) {
+				return {
+					success: false,
+					reply: `Could not upload the image summary!`
+				};
+			}
+
+			return {
+				reply: `Image summary: ${uploadResult.link}`
+			};
+		}
+		else {
+			const response = await sb.Got({
+				url: "http://api.wolframalpha.com/v1/result",
+				throwHttpErrors: false,
+				responseType: "text",
+				searchParams: {
+					appid: sb.Config.get("API_WOLFRAM_ALPHA_APPID"),
+					i: args.join(" ")
+				}
+			});
+
+			const data = sb.Config.get("WOLFRAM_QUERY_CENSOR_FN")(response.body);
+			return {
+				reply: (context.platform.Name === "discord")
+					? `\`${data}\``
+					: data
+			};
+		}
 	}),
-	Dynamic_Description: null
+	Dynamic_Description: (async (prefix) => [
+		"Asks WolframAlpha for the response for a given question or query.",
+		"",
+
+		`<code>${prefix}query (your query here)</code>`,
+		`<code>${prefix}query population of Austria</code>`,
+		`Retrieves a simple response to your query.`,
+		"",
+
+		`<code>${prefix}query (your query here) <u>imageSummary:true</u></code>`,
+		`<code>${prefix}query past presidents of India <u>imageSummary:true</u></code>`,
+		`Retrieves and uploads an image with a summary of the response to your query.`
+	])
 };
