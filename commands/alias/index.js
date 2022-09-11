af4cf518a1b03f8a206b34c8673d55245367ff9a
Supinic <supinic@protonmail.com>
2022-09-11 19:13:16 +0200

    typo fix

--- commands/alias/index.js
+++ commands/alias/index.js
@@ -328,7 +328,7 @@ module.exports = {
 				}
 
 				const permissions = await context.getUserPermissions();
-				if (permissions.is(sb.User.permission.regular)) {
+				if (permissions.is(sb.User.permissions.regular)) {
 					return {
 						success: false,
 						reply: `Only the owner and ambassadors of this channel can use this subcommand!`
