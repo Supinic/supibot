1228f9fcb4fbb3bafb774cec0eaa38d0fa011939
Supinic <supinic@protonmail.com>
2022-09-28 18:44:58 +0200

    added an API to list command-user specific filters

--- api/command.js
+++ api/command.js
@@ -96,5 +96,72 @@ module.exports = {
 			statusCode: 200,
 			data: { info }
 		};
+	},
+
+	userFilters: async (req, res, url) => {
+		const rawUserID = url.searchParams.get("userID");
+		if (!rawUserID) {
+			return {
+				statusCode: 400,
+				error: { message: `Valid user ID must be provided` }
+			};
+		}
+
+		const userID = Number(rawUserID);
+		if (!sb.Utils.isValidInteger(userID)) {
+			return {
+				statusCode: 400,
+				error: { message: `Malformed user ID provided` }
+			};
+		}
+
+		const commandName = url.searchParams.get("command");
+		if (!commandName) {
+			return {
+				statusCode: 400,
+				error: { message: `Valid command name must be provided` }
+			};
+		}
+
+		const commandData = sb.Command.get(commandName);
+		if (!commandData) {
+			return {
+				statusCode: 404,
+				error: { message: `Provided command does not exist` }
+			};
+		}
+
+		const optout = await sb.Query.getRecordset(rs => rs
+			.select("ID")
+			.from("chat_data", "Filter")
+			.where("Command = %s", commandData.Name)
+			.where("User_Alias = %n", userID)
+			.where("Type = %s", "Opt-out")
+			.where("Active = %b", true)
+			.single()
+			.flat("ID")
+		);
+
+		const blocks = await sb.Query.getRecordset(rs => rs
+			.select("Filter.ID AS ID", "Blocked_User.Name AS blockedUsername")
+			.from("chat_data", "Filter")
+			.join({
+				alias: "Blocked_User",
+				toTable: "User_Alias",
+				on: "Filter.Blocked_User = Blocked_User.ID"
+			})
+			.where("Command = %s", commandData.Name)
+			.where("User_Alias = %n", userID)
+			.where("Type = %s", "Block")
+			.where("Active = %b", true)
+		);
+
+		return {
+			statusCode: 200,
+			data: {
+				optout,
+				blocks
+			}
+		};
 	}
 };
