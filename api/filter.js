29b3bbf77630574f9b5d16cfe2c6907e000f73d6
Supinic <supinic@protonmail.com>
2022-10-19 18:50:08 +0200

    moved filter API to filter file

--- api/filter.js
+++ api/filter.js
@@ -7,6 +7,7 @@ module.exports = {
 			data: { message: "OK" }
 		};
 	},
+
 	reloadSpecific: async (req, res, url) => {
 		const IDs = url.searchParams.getAll("ID").map(Number).filter(Boolean);
 		const result = await sb.Filter.reloadSpecific(...IDs);
@@ -19,6 +20,125 @@ module.exports = {
 				active,
 				inactive,
 				result
+			}
+		};
+	},
+
+	command: async (req, res, url) => {
+		const commandName = url.searchParams.get("command");
+		if (!commandName) {
+			return {
+				statusCode: 400,
+				error: { message: `Valid command name must be provided` }
+			};
+		}
+
+		const commandData = sb.Command.get(commandName);
+		if (!commandData) {
+			return {
+				statusCode: 404,
+				error: { message: `Provided command does not exist` }
+			};
+		}
+
+		const data = await sb.Query.getRecordset(rs => rs
+			.select("Filter.ID", "Type", "User_Alias", "Channel", "Invocation", "Response", "Reason", "Filter.Data")
+			.select("Channel.Name AS Channel_Name", "Channel.Description AS Channel_Description")
+			.select("User_Alias.Name AS Username")
+			.select("Platform.Name AS Platform_Name")
+			.select("Blocked.Name AS Blocked_Username")
+			.from("chat_data", "Filter")
+			.leftJoin("chat_data", "Channel")
+			.leftJoin("chat_data", "User_Alias")
+			.leftJoin({
+				alias: "Blocked",
+				toTable: "User_Alias",
+				on: "Filter.Blocked_User = Blocked.ID"
+			})
+			.leftJoin({
+				toTable: "Platform",
+				on: "Channel.Platform = Platform.ID"
+			})
+			.where("Channel IS NULL OR Channel.Mode <> %s", "Inactive")
+			.where("Command = %s", commandData.Name)
+			.where("Active = %b", true)
+		);
+
+		for (const item of data) {
+			if (item.Data) {
+				item.Data = JSON.parse(item.Data);
+			}
+		}
+
+		return {
+			statusCode: 200,
+			data
+		};
+	},
+
+	userCommand: async (req, res, url) => {
+		const rawUserID = url.searchParams.get("userID");
+		if (!rawUserID) {
+			return {
+				statusCode: 400,
+				error: { message: `Valid user ID must be provided` }
+			};
+		}
+
+		const userID = Number(rawUserID);
+		if (!sb.Utils.isValidInteger(userID)) {
+			return {
+				statusCode: 400,
+				error: { message: `Malformed user ID provided` }
+			};
+		}
+
+		const commandName = url.searchParams.get("command");
+		if (!commandName) {
+			return {
+				statusCode: 400,
+				error: { message: `Valid command name must be provided` }
+			};
+		}
+
+		const commandData = sb.Command.get(commandName);
+		if (!commandData) {
+			return {
+				statusCode: 404,
+				error: { message: `Provided command does not exist` }
+			};
+		}
+
+		const optout = await sb.Query.getRecordset(rs => rs
+			.select("ID")
+			.from("chat_data", "Filter")
+			.where("Command = %s", commandData.Name)
+			.where("User_Alias = %n", userID)
+			.where("Type = %s", "Opt-out")
+			.where("Active = %b", true)
+			.single()
+			.flat("ID")
+		);
+
+		const blocks = await sb.Query.getRecordset(rs => rs
+			.select("Filter.ID AS ID", "Blocked_User.Name AS blockedUsername")
+			.from("chat_data", "Filter")
+			.join({
+				alias: "Blocked_User",
+				toTable: "User_Alias",
+				on: "Filter.Blocked_User = Blocked_User.ID"
+			})
+			.where("Command = %s", commandData.Name)
+			.where("User_Alias = %n", userID)
+			.where("Type = %s", "Block")
+			.where("Active = %b", true)
+		);
+
+		return {
+			statusCode: 200,
+			data: {
+				optout,
+				blocks
 			}
 		};
 	}
