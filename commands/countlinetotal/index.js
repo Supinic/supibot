2db02ae39d84d7c9c2474f0320385ed25a06fe75
Supinic <supinic@protonmail.com>
2022-10-25 17:59:23 +0200

    removed reference to `Command_Execution`
    - originally, the command checked for its own previous usage, and calculated the lines used by text parsing

--- commands/countlinetotal/index.js
+++ commands/countlinetotal/index.js
@@ -9,7 +9,7 @@ module.exports = {
 	Whitelist_Response: null,
 	Static_Data: null,
 	Code: (async function countLineTotal (context) {
-		const [response, chatLineAmount, history] = await Promise.all([
+		const [response, chatLineAmount] = await Promise.all([
 			sb.Got("RaspberryPi4", { url: "ssd/size" }),
 			sb.Query.getRecordset(rs => rs
 				.select("SUM(AUTO_INCREMENT) AS Chat_Lines")
@@ -17,30 +17,12 @@ module.exports = {
 				.where("TABLE_SCHEMA = %s", "chat_line")
 				.flat("Chat_Lines")
 				.single()
-			),
-			sb.Query.getRecordset(rs => rs
-				.select("Executed", "Result")
-				.from("chat_data", "Command_Execution")
-				.where("Command = %s", this.Name)
-				.where("Result <> %s", "")
-				.orderBy("Executed ASC")
-				.limit(1)
-				.single()
 			)
 		]);
 
-		let historyText = "";
 		const currentSize = sb.Utils.round(response.body.data.size / (10 ** 9), 3);
 		const maximumSize = sb.Config.get("DATA_DRIVE_MAXIMUM_SIZE_GB", false) ?? 220; // default size is hardcoded ~220 GB
 		const percentUsage = sb.Utils.round((currentSize / maximumSize) * 100, 2);
-
-		if (history) {
-			const days = (sb.Date.now() - history.Executed) / 864.0e5;
-			const originalLines = Number(history.Result.match(/logging([\d ])+lines/)[1]);
-			const linesPerHour = sb.Utils.round((chatLineAmount - originalLines) / (days * 24), 0);
-
-			historyText = `Lines are added at a rate of ${sb.Utils.groupDigits(linesPerHour)} lines/hr.`;
-		}
 
 		const cooldown = {};
 		if (context.channel) {
@@ -59,7 +41,6 @@ module.exports = {
 			reply: sb.Utils.tag.trim `
 				Currently logging ${sb.Utils.groupDigits(chatLineAmount)} lines in total across all channels,
 				taking up ~${currentSize} GB of space (${percentUsage}%).
-				${historyText}
 			`
 		};
 	}),
