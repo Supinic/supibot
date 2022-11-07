27a72fa6c1d1f75287b72b9820c45e9e8a64d95f
Supinic <supinic@protonmail.com>
2022-11-07 16:48:42 +0100

    updated relaxed regex

--- chat-modules/ping-supi/index.js
+++ chat-modules/ping-supi/index.js
@@ -7,7 +7,7 @@ module.exports = {
 
 		this.data.timeout ??= 0;
 
-		const relaxedRegex = /supi\b|supinic(?!\.com)|bupi|супи/i;
+		const relaxedRegex = /(supi|supinic(?!\.com)|bupi|супи)\b/i;
 		const strictRegex = /\bsupinic(?!\.com)\b/i;
 
 		const skippedUsers = [1, 1127, 8697460, 12182780];
