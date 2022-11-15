a3c299a8913a4984b2485e8f5d839cd3a9c1b679
Supinic <supinic@protonmail.com>
2022-11-15 18:11:47 +0100

    initial implementation

---
+++ commands/howlongtobeat/index.js
@@ -0,0 +1,66 @@
+module.exports = {
+	Name: "howlongtobeat",
+	Aliases: ["hltb"],
+	Author: "supinic",
+	Cooldown: 5000,
+	Description: "For a provided game, shows how long it takes to beat based on the howlongobeat.com website's data.",
+	Flags: ["mention","non-nullable","pipe"],
+	Params: null,
+	Whitelist_Response: null,
+	Static_Data: null,
+	Code: (async function howLongToBeat (context, ...args) {
+		if (args.length === 0) {
+			return {
+				success: false,
+				reply: `No game name provided!`
+			};
+		}
+
+		const response = await sb.Got("GenericAPI", {
+			url: "https://howlongtobeat.com/api/search",
+			method: "POST",
+			headers: {
+				Referer: "https://howlongtobeat.com"
+			},
+			json: {
+				searchType: "games",
+				searchTerms: [...args],
+				size: 1
+			}
+		});
+
+		// 	main: gameData.comp_main_count,
+		// 	plus: gameData.comp_plus_count,
+		// 	full: gameData.comp_100_count,
+		// 	all: gameData.comp_all_count
+		const { data } = response.body;
+		if (data.length === 0) {
+			return {
+				success: false,
+				reply: `No game has been found for your query!`
+			};
+		}
+
+		const [gameData] = data;
+		const hours = {
+			main: sb.Utils.round(gameData.comp_main / 3600, 1),
+			plus: sb.Utils.round(gameData.comp_plus / 3600, 1),
+			full: sb.Utils.round(gameData.comp_100 / 3600, 1),
+			all: sb.Utils.round(gameData.comp_all / 3600, 1)
+		};
+
+
+		const url = `https://howlongtobeat.com/game/${gameData.game_id}`;
+		return {
+			reply: sb.Utils.tag.trim `
+				Average time to beat ${gameData.game_name} (${gameData.release_world}):
+				Main story - ${hours.main} hours;
+				Side content - ${hours.plus} hours;
+				100% - ${hours.full} hours;
+				All styles - ${hours.all} hours.
+				${url}
+			`
+		};
+	}),
+	Dynamic_Description: null
+};
