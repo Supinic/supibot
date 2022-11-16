95704a0703d37a9796fc12178443f96d94dad007
Supinic <supinic@protonmail.com>
2022-11-16 17:49:17 +0100

    added support for specific user's specific alias statistics

--- commands/statistics/types/aliases.js
+++ commands/statistics/types/aliases.js
@@ -1,10 +1,10 @@
 module.exports = {
 	name: "aliases",
 	aliases: ["alias"],
-	description: "Checks the global (or-use data for user-created supibot command aliases.",
-	execute: async (context, type, user) => {
-		if (user) {
-			const userData = await sb.User.get(user);
+	description: "Checks the statistics of either: all aliases globally; or aliases of a provided user; or a specific alias by a provided user.",
+	execute: async (context, type, userName, aliasName) => {
+		if (userName) {
+			const userData = await sb.User.get(userName);
 			if (!userData) {
 				return {
 					success: false,
@@ -12,67 +12,110 @@ module.exports = {
 				};
 			}
 
-			const [aliases, copyData] = await Promise.all([
+			const pronoun = (userData === context.user) ? "You" : "They";
+			const posPronoun = (userData === context.user) ? "Your" : "Their";
+			if (aliasName) {
+				const aliasData = await sb.Query.getRecordset(rs => rs
+					.select("ID", "Parent")
+					.from("data", "Custom_Command_Alias")
+					.where("User_Alias = %n", userData.ID)
+					.where("Name = %s", aliasName)
+					.where("Parent IS NULL")
+					.single()
+				);
+
+				if (!aliasData) {
+					return {
+						success: false,
+						reply: `${pronoun} don't have an alias with that name!`
+					};
+				}
+				else if (aliasData.Parent) {
+					return {
+						success: false,
+						reply: `This alias is a copy or a link to an existing alias and hence has no statistics available!`
+					};
+				}
+
+				/** @type { { Invocation: string|null }[] } */
+				const data = await sb.Query.getRecordset(rs => rs
+					.select("Invocation")
+					.from("data", "Custom_Command_Alias")
+					.where("User_Alias = %n", userData.ID)
+					.where("Parent = %n", aliasData.ID)
+				);
+
+				const [copies, links] = sb.Utils.splitByCondition(data, i => i.Invocation);
+				return {
+					reply: `${posPronoun} alias "${aliasName} has been copied ${copies.length} times, and linked ${links.length} times.`
+				};
+			}
+			else {
+				/** @type {[number[], number[]]} */
+				const data = await Promise.all([
+					sb.Query.getRecordset(rs => rs
+						.select("COUNT(*) AS Count")
+						.from("data", "Custom_Command_Alias")
+						.where("User_Alias = %n", userData.ID)
+						.single()
+						.flat("Count")
+					),
+					sb.Query.getRecordset(rs => rs
+						.select("Copy.User_Alias AS Copier")
+						.from("data", "Custom_Command_Alias")
+						.where("Custom_Command_Alias.User_Alias = %n", userData.ID)
+						.join({
+							alias: "Copy",
+							toTable: "Custom_Command_Alias",
+							on: "Copy.Parent = Custom_Command_Alias.ID"
+						})
+						.flat("Copier")
+					)
+				]);
+
+				const [aliases, copyData] = data;
+				const copies = copyData.length;
+				const users = new Set(copyData).size;
+				const [who, whose] = (context.user === userData) ? ["You", "your"] : ["They", "their"];
+
+				return {
+					reply: sb.Utils.tag.trim `
+						${who} currently have ${aliases} command aliases,
+						and ${users} distinct users have created ${copies} copies of ${whose} aliases.
+					`
+				};
+			}
+		}
+		else {
+			const [aliases, copies, users] = await Promise.all([
+				sb.Query.getRecordset(rs => rs
+					.select("MAX(ID) AS Max")
+					.from("data", "Custom_Command_Alias")
+					.single()
+					.flat("Max")
+				),
 				sb.Query.getRecordset(rs => rs
 					.select("COUNT(*) AS Count")
 					.from("data", "Custom_Command_Alias")
-					.where("User_Alias = %n", userData.ID)
+					.where("Parent IS NOT NULL")
 					.single()
 					.flat("Count")
 				),
 				sb.Query.getRecordset(rs => rs
-					.select("Copy.User_Alias AS Copier")
+					.select("COUNT(DISTINCT User_Alias) AS Count")
 					.from("data", "Custom_Command_Alias")
-					.where("Custom_Command_Alias.User_Alias = %n", userData.ID)
-					.join({
-						alias: "Copy",
-						toTable: "Custom_Command_Alias",
-						on: "Copy.Parent = Custom_Command_Alias.ID"
-					})
-					.flat("Copier")
+					.single()
+					.flat("Count")
 				)
 			]);
 
-			const copies = copyData.length;
-			const users = new Set(copyData).size;
-			const [who, whose] = (context.user === userData) ? ["You", "your"] : ["They", "their"];
-
 			return {
 				reply: sb.Utils.tag.trim `
-					${who} currently have ${aliases} command aliases,
-					and ${users} distinct users have created ${copies} copies of ${whose} aliases.
+					${aliases} command aliases have been created so far
+					(out of which, ${copies} are direct copies of others),
+					used by ${users} users in total.
 				`
 			};
 		}
-
-		const [aliases, copies, users] = await Promise.all([
-			sb.Query.getRecordset(rs => rs
-				.select("MAX(ID) AS Max")
-				.from("data", "Custom_Command_Alias")
-				.single()
-				.flat("Max")
-			),
-			sb.Query.getRecordset(rs => rs
-				.select("COUNT(*) AS Count")
-				.from("data", "Custom_Command_Alias")
-				.where("Parent IS NOT NULL")
-				.single()
-				.flat("Count")
-			),
-			sb.Query.getRecordset(rs => rs
-				.select("COUNT(DISTINCT User_Alias) AS Count")
-				.from("data", "Custom_Command_Alias")
-				.single()
-				.flat("Count")
-			)
-		]);
-
-		return {
-			reply: sb.Utils.tag.trim `
-				${aliases} command aliases have been created so far
-				(out of which, ${copies} are direct copies of others),
-				used by ${users} users in total.
-			`
-		};
 	}
 };
