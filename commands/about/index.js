0fdfb8c9e2d9a201321deaabad73dbfe0cae4628
Supinic <supinic@protonmail.com>
2022-12-03 19:31:06 +0100

    removed usage of `chat_line` from `$about`

--- commands/about/index.js
+++ commands/about/index.js
@@ -11,31 +11,18 @@ module.exports = {
 	Code: (async function about (context) {
 		let presentSince = "";
 		if (context.channel) {
-			let date;
-			const tableName = context.channel.getDatabaseName();
+			const botData = await sb.User.get(context.platform.Self_Name);
 
-			if (!await sb.Query.isTablePresent("chat_line", tableName)) {
-				const botData = await sb.User.get(context.platform.Self_Name);
-				date = await sb.Query.getRecordset(rs => rs
-					.select("First_Message_Posted")
-					.from("chat_data", "Message_Meta_User_Alias")
-					.where("Channel = %n", context.channel.ID)
-					.where("User_Alias = %n", botData.ID)
-					.limit(1)
-					.flat("First_Message_Posted")
-					.single()
-				);
-			}
-			else {
-				date = await sb.Query.getRecordset(rs => rs
-					.select("Posted")
-					.from("chat_line", tableName)
-					.orderBy("ID ASC")
-					.limit(1)
-					.flat("Posted")
-					.single()
-				);
-			}
+			/** @type {CustomDate|null} */
+			const date = await sb.Query.getRecordset(rs => rs
+				.select("First_Message_Posted")
+				.from("chat_data", "Message_Meta_User_Alias")
+				.where("Channel = %n", context.channel.ID)
+				.where("User_Alias = %n", botData.ID)
+				.limit(1)
+				.flat("First_Message_Posted")
+				.single()
+			);
 
 			if (date) {
 				presentSince = `I am present in this channel since ${date.format("Y-m-d")} (${sb.Utils.timeDelta(date)})`;
