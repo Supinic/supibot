7a818e10cccdaf8899d0449a12a99b286e0593e2
Supinic <supinic@protonmail.com>
2022-12-07 18:42:43 +0100

    added new `platform.js` controller to list

--- api/index.js
+++ api/index.js
@@ -14,6 +14,7 @@ module.exports = (function () {
 		["command", "command.js"],
 		["filter", "filter.js"],
 		["health", "health.js"],
+		["platform", "platform.js"],
 		["reminder", "reminder.js"],
 		["user", "user.js"]
 	];
