cc473a37b65cf8303e453bc613cbec140d9c2a34
Supinic <supinic@protonmail.com>
2022-12-13 21:32:20 +0100

    Revert "removed redundant comma from SQL expression"
    This reverts commit ed934aee41188ae05da544963781652da1fe95ea.

--- classes/channel.js
+++ classes/channel.js
@@ -62,7 +62,7 @@ module.exports = class Channel extends require("./template.js") {
 				`\`Historic\` TINYINT(1) UNSIGNED NOT NULL DEFAULT 0,`,
 				`\`Text\` TEXT NOT NULL,`,
 				"`Posted` DATETIME(3) NULL DEFAULT NULL,",
-				"PRIMARY KEY (`ID`)",
+				"PRIMARY KEY (`ID`),",
 				")",
 				"COLLATE=`utf8mb4_general_ci` ENGINE=InnoDB AUTO_INCREMENT=1 PAGE_COMPRESSED=1;"
 			].join(" "));
