79785b58f73467a55660564044d38e3315d255d2
Supinic <supinic@protonmail.com>
2023-01-17 17:25:46 +0100

    use the right method property

--- commands/osrs/index.js
+++ commands/osrs/index.js
@@ -33,7 +33,7 @@ module.exports = {
 			subcommand = subcommands.find(i => i.default === true);
 		}
 
-		return await subcommand.call(this, context, ...args);
+		return await subcommand.execute.apply(context, ...args);
 	}),
 	Dynamic_Description: (async function (prefix) {
 		const subcommands = require("./subcommands");
