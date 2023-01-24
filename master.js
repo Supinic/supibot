58e1f159bf97821fc0200b95ae8f4dd0eb35d320
Supinic <supinic@protonmail.com>
2023-01-24 18:30:32 +0100

    require SPM/commands rather than the main entrypoint (mjs)

--- master.js
+++ master.js
@@ -14,7 +14,7 @@
 	const initializeSbObject = require("supi-core");
 	globalThis.sb = await initializeSbObject();
 
-	const { commands } = await require("supibot-package-manager");
+	const commands = await require("supibot-package-manager/commands");
 	await sb.Command.importData(commands.definitions);
 
 	const controllers = {};
