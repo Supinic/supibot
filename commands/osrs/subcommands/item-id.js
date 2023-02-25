f212d39587d55dd9fabfa56787643bfa8b268767
Supinic <supinic@protonmail.com>
2023-02-25 19:07:38 +0100

    expanded description for `$osrs itemid`

--- commands/osrs/subcommands/item-id.js
+++ commands/osrs/subcommands/item-id.js
@@ -2,7 +2,10 @@ module.exports = {
 	name: "itemid",
 	title: "Item IDs",
 	aliases: ["item-id"],
-	description: [`Posts the item's ingame ID. Shows up to 5 best matching results.`],
+	description: [
+		`<code>$osrs itemid (item name)</code>`,
+		`Posts the item's in-game ID. Shows up to 5 best matching results.`
+	],
 	execute: async function (context, ...args) {
 		const data = await sb.Query.getRecordset(rs => {
 			rs.select("Game_ID", "Name")
