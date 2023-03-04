c0b17368691205178003239e1bea61ee9e7e0ebe
Supinic <supinic@protonmail.com>
2023-03-04 19:38:28 +0100

    refactored $gpt to use ChatGPT 3.5 (#37)
    - old functionality is removed, Ada, Babbage, Curie and Davinci models are not available
    - only conversational model available at the moment is "Turbo"
    - history is now kept by default
    - moved history manipulation from `$unset` and `$check` to `$gpt history:clear` and `$gpt history:check` respectively
    - increased hourly limits 2x (100 → 200 and 1000 → 2000 for regulars and subs, respectively)
    - refactored moderation into its own module

---
+++ commands/gpt/moderation.js
@@ -0,0 +1,53 @@
+const check = async (context, text) => {
+	text = text.trim();
+
+	const moderationCheck = await sb.Got("GenericAPI", {
+		method: "POST",
+		throwHttpErrors: false,
+		url: `https://api.openai.com/v1/moderations`,
+		headers: {
+			Authorization: `Bearer ${sb.Config.get("API_OPENAI_KEY")}`
+		},
+		json: {
+			input: text
+		}
+	});
+
+	if (!moderationCheck.ok || !Array.isArray(moderationCheck.body.results)) {
+		const logId = await sb.Logger.log(
+			"Command.Warning",
+			`GPT moderation failed! ${JSON.stringify({ body: moderationCheck.body })}`,
+			context.channel,
+			context.user
+		);
+
+		return {
+			success: false,
+			reply: `Could not check your response for moderation! Please try again later. Reference ID: ${logId}`
+		};
+	}
+
+	const [moderationResult] = moderationCheck.body.results;
+	const { categories, category_scores: scores } = moderationResult;
+	if (categories.hate || categories["violence/graphic"] || categories["sexual/minors"]) {
+		const logId = await sb.Logger.log(
+			"Command.Warning",
+			`Unsafe GPT content generated! ${JSON.stringify({ text, scores })}`,
+			context.channel,
+			context.user
+		);
+
+		return {
+			success: false,
+			reply: `Unsafe content generated! Reference ID: ${logId}`
+		};
+	}
+
+	return {
+		success: true
+	};
+};
+
+module.exports = {
+	check
+};
