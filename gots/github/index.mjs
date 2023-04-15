a9b9740d06e9859a8b6b5fb05f0eb70741d8e9d1
Supinic <supinic@protonmail.com>
2023-04-15 13:38:56 +0200

    use authenticated GitHub request

--- gots/github/index.mjs
+++ gots/github/index.mjs
@@ -1,9 +1,26 @@
 export const definition = {
 	name: "GitHub",
 	optionsType: "object",
-	options: {
-		prefixUrl: "https://api.github.com"
+	headers: {
+		Authorization: sb.Config.get("API_GITHUB_KEY")
 	},
+	options: (() => {
+		if (!sb.Config.has("API_GITHUB_KEY")) {
+			throw new Error("GitHub sb.Got instance cannot initialize - missing client-id");
+		}
+
+		const token = sb.Config.get("API_GITHUB_KEY", false);
+		if (!token) {
+			throw new Error("GitHub sb.Got instance cannot initialize - missing token");
+		}
+
+		return {
+			prefixUrl: "https://api.github.com",
+			headers: {
+				Authorization: `Bearer ${token}`
+			}
+		};
+	}),
 	parent: "Global",
 	description: null
 };
