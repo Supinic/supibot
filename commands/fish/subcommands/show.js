d40d65f89f99d68090d3254d4e0df22ad4842c0e
Supinic <supinic@protonmail.com>
2023-05-23 17:40:34 +0200

    added support for checking other users' collections

--- commands/fish/subcommands/show.js
+++ commands/fish/subcommands/show.js
@@ -5,14 +5,34 @@ module.exports = {
 	aliases: ["count", "display", "collection"],
 	description: [
 		`<code>$fish show</code>`,
-		"Show off your fishing trophy collection."
+		"Show off your fishing trophy collection.",
+		"",
+
+		`<code>$fish show (user)</code>`,
+		`<code>$fish show @Supinic</code>`,
+		"Check out another user's fishing collection."
 	],
-	execute: async (context) => {
+	execute: async (context, user) => {
+		const targetUserData = (user)
+			? await sb.User.get(user)
+			: context.user;
+
+		if (!targetUserData) {
+			return {
+				success: false,
+				reply: `No such user exists!`
+			};
+		}
+
 		/** @type {UserFishData} */
-		const fishData = await context.user.getDataProperty("fishData") ?? getInitialStats();
+		const fishData = await targetUserData.getDataProperty("fishData") ?? getInitialStats();
+		const [subject, possessive] = (targetUserData === context.user)
+			? ["You", "your"]
+			: ["They", "their"];
+
 		if (fishData.catch.total === 0) {
 			return {
-				reply: `You have no fish in your collection.`
+				reply: `${subject} have no fish in ${possessive} collection.`
 			};
 		}
 
@@ -27,7 +47,7 @@ module.exports = {
 		}
 
 		return {
-			reply: `You have ${fishData.catch.total} fish in your collection. Here they are: ${result.join("")}`
+			reply: `${subject} have ${fishData.catch.total} fish in ${possessive} collection. Here they are: ${result.join("")}`
 		};
 	}
 };
