583e527a6ad15aa2cdad978b0c3f616c6520b9b1
Supinic <supinic@protonmail.com>
2023-05-30 18:17:58 +0200

    redirect users to use `$help fish` instead of `$fish help`

--- commands/fish/subcommands/help.js
+++ commands/fish/subcommands/help.js
@@ -1,18 +1,8 @@
 module.exports = {
 	name: "help",
 	aliases: [],
-	description: [
-		`<code>$fish help</code>`,
-		"Shows a brief description of the subcommands."
-	],
+	description: [],
 	execute: async () => ({
-		reply: sb.Utils.tag.trim `
-			Use one of these:
-			$fish → go fishing;
-			$fish (bait) → buy bait and go fishing;
-			$fish sell (fish) → sell one of your fish;
-			$fish show → show off your collection;
-			$fish help → you're reading this.
-		`
+		reply: `Use "$help fish" instead, please.`
 	})
 };
