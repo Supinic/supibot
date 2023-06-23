1f29bc325f5f5364a350aa3511fa203237d215c8
Supinic <supinic@protonmail.com>
2023-06-23 16:37:18 +0200

    removed `$fish help` as it isn't needed anymore

--- commands/fish/subcommands/help.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	name: "help",
-	aliases: [],
-	description: [],
-	execute: async () => ({
-		reply: `Use "$help fish" instead, please.`
-	})
-};
--- commands/fish/subcommands/index.js
+++ commands/fish/subcommands/index.js
@@ -1,7 +1,6 @@
 const subcommands = [
 	require(`./buy.js`),
 	require(`./fish.js`),
-	require(`./help.js`),
 	require(`./leaderboard.js`),
 	require(`./sell.js`),
 	require(`./show.js`),
