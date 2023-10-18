73aaa0929d82481468b15748cbce1dd18f030598
Supinic <supinic@protonmail.com>
2023-10-18 18:31:50 +0200

    initial implementation

---
+++ commands/metrics/definitions.js
@@ -0,0 +1,38 @@
+module.exports = [
+	{
+		name: "commands-per-minute",
+		aliases: ["cpm"],
+		description: [
+			"<code>$metrics commands-per-minute</code>",
+			"<code>$metrics cpm</code>",
+			"Shows how many commands Supibot is currently executing globally, per minute."
+		],
+		type: "basic",
+		query: "sum(rate(supibot_command_executions_total[1h]))*60",
+		format: (result) => `Supibot is currently executing ${sb.Utils.round(result, 2)} commands per minute.`
+	},
+	{
+		name: "read-messages-per-minute",
+		aliases: ["rmpm"],
+		description: [
+			"<code>$metrics read-messages-per-minute</code>",
+			"<code>$metrics rmpm</code>",
+			"Shows how many messages Supibot reads per minute, in all the channels it is in, combined."
+		],
+		type: "basic",
+		query: "sum(rate(supibot_messages_read_total[1h]))*60",
+		format: (result) => `Supibot is currently reading ${sb.Utils.round(result, 2)} messages per minute.`
+	},
+	{
+		name: "top-messages-channel",
+		aliases: ["tmc"],
+		description: [
+			"<code>$metrics top-messages-channel</code>",
+			"<code>$metrics tmc</code>",
+			"Shows the top channel sorted by how many messages per minute are being sent in it, plus the value as well."
+		],
+		type: "basic",
+		query: "topk(1, rate(supibot_messages_read_total[5m])) * 60",
+		format: (result, metric) => `The currently fastest channel Supibot is in is ${metric.channel}, with ${sb.Utils.round(result, 2)} messages per minute.`
+	}
+];
---
+++ commands/metrics/index.js
@@ -0,0 +1,90 @@
+const definitions = require("./definitions.js");
+const { baseUrl } = require("./prometheus-config.json");
+
+module.exports = {
+	Name: "metrics",
+	Aliases: [],
+	Author: "supinic",
+	Cooldown: 5000,
+	Description: "Shows off various metrics related to Supibot - e.g. current commands per minute.",
+	Flags: [],
+	Params: null,
+	Whitelist_Response: null,
+	Static_Data: null,
+	Code: async function metrics (context, type) {
+		if (!type) {
+			return {
+				success: false,
+				reply: `You did not provide any metric name! Use "commands-per-minute", for example, or check this command's help for a full list.`
+			};
+		}
+
+		type = type.toLowerCase();
+
+		const definition = definitions.find(i => i.name === type || i.aliases.includes(type));
+		if (!definition) {
+			return {
+				success: false,
+				reply: `You did not provide any valid metric name!`
+			};
+		}
+
+		let reply;
+		if (definition.type === "basic") {
+			const response = await sb.Got("GenericAPI", {
+				url: `${baseUrl}/query`,
+				searchParams: {
+					query: definition.query
+				}
+			});
+
+			if (!response.ok || response.body.status !== "success") {
+				return {
+					success: false,
+					reply: `The Prometheus metrics API is currently out of order! Try again later.`
+				};
+			}
+
+			/** @type {PrometheusApiResponse} */
+			const data = response.body;
+			const { metric, value } = data.data.result[0];
+
+			reply = definition.format(Number(value[1]), metric);
+		}
+
+		if (!reply) {
+			return {
+				success: false,
+				reply: `No result has been generated from the Prometheus metrics API! Try again later.`
+			};
+		}
+		else {
+			return {
+				reply
+			};
+		}
+	},
+	Dynamic_Description: async () => {
+		const list = definitions.flatMap(i => [...i.description, "", ""]);
+		return [
+			`Queries the Prometheus metrics API to give out various fun statistics about Supibot!`,
+			"",
+
+			...list
+		];
+	}
+};
+
+/**
+ * @typedef {Object} PrometheusApiResponse
+ * @property {Object} data
+ * @property {PrometheusApiResult[]} data.result
+ * @property {string} data.resultType
+ * @property {"success"} status
+ */
+
+/**
+ * @typedef {Object} PrometheusApiResult
+ * @property {Object} metric
+ * @property {[number, string]} value
+ */
