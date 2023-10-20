66dccac32f59a2d569a76e305ee95453ea76523e
Supinic <supinic@protonmail.com>
2023-10-20 17:05:58 +0200

    update `user agent` in `FakeAgent`

--- gots/fake-agent/index.mjs
+++ gots/fake-agent/index.mjs
@@ -3,7 +3,7 @@ export const definition = {
 	optionsType: "object",
 	options: {
 		headers: {
-			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.53 Safari/537.36"
+			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
 		}
 	},
 	parent: "Global",
