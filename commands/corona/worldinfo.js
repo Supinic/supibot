5d9bbb694191401af712ebbd9bd7738529fc0fa0
Supinic <supinic@protonmail.com>
2024-01-24 19:39:08 +0100

    `$corona` - vastly reduced complexion, only provides global stats now
    - also likely to retire the `supi-corona` service running outside of Supibot

---
+++ commands/corona/worldinfo.js
@@ -0,0 +1,30 @@
+const url = "https://www.worldometers.info/coronavirus";
+
+module.exports = async () => {
+	let response = null;
+	try {
+		response = await sb.Got("FakeAgent", {
+			url,
+			responseType: "text"
+		});
+	}
+	catch (e) {
+		return {
+			success: false
+		};
+	}
+
+	const $ = sb.Utils.cheerio(response.body);
+	const nodes = $(".maincounter-number span");
+	const [total, deaths, recoveries] = [...nodes].map(i => {
+		const textNode = i.children[0].data;
+		return Number(textNode.trim().replaceAll(",", ""));
+	});
+
+	return {
+		success: true,
+		total,
+		deaths,
+		recoveries
+	};
+};
