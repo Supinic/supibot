150d9a8b2c537392b6848686c3ba1ef3127773b7
Brian <18603393+brian6932@users.noreply.github.com>
2024-01-24 13:13:47 -0500

    Fix: `firstfollowedchannel` Made `firstchannelfollower` match

--- commands/firstfollowedchannel/index.js
+++ commands/firstfollowedchannel/index.js
@@ -26,26 +26,13 @@ module.exports = {
 			headers: {
 				Accept: "*/*",
 				"Accept-Language": "en-US",
-				Authorization: `OAuth ${sb.Config.get("TWITCH_GQL_OAUTH")}`,
 				"Client-ID": sb.Config.get("TWITCH_GQL_CLIENT_ID"),
 				"Client-Version": sb.Config.get("TWITCH_GQL_CLIENT_VERSION"),
 				"Content-Type": "text/plain;charset=UTF-8",
 				Referer: `https://dashboard.twitch.tv/`,
 				"X-Device-ID": sb.Config.get("TWITCH_GQL_DEVICE_ID")
 			},
-			query: ` 
-				query {
-					user(login: "${name}") {
-						follows(order: ASC, first: 10) {
-							edges {
-								followedAt
-								node {
-									login
-								}
-							}
-						}
-					}
-				}`
+			query: `query ChannelFollows{user(login:"${name}"){follows(first:1){edges{node{login}followedAt}}}}`
 		});
 
 		if (Array.isArray(response.body.errors) && response.body.errors[0].message === "service error") {
