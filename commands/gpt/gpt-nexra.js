678f1080d19cd8e6f31caec3f6eb09937b155176
Supinic <supinic@protonmail.com>
2024-02-02 17:41:52 +0100

    `$gpt` - Add Nexra API to GPT models (#61)
    * add experimental support for the `Nexra` API for GPT models
    * add handling of Nexra API's "JSON" format
    * their format is sometimes prefixed with non-json characters, strip those out

---
+++ commands/gpt/gpt-nexra.js
@@ -0,0 +1,72 @@
+const GptMessages = require("./gpt-messages.js");
+const GptHistory = require("./history-control.js");
+
+module.exports = class GptNexra extends GptMessages {
+	static async getHistory (context) {
+		const { historyMode } = await GptMessages.getHistoryMode(context);
+		return (historyMode === "enabled")
+			? (await GptHistory.get(context.user) ?? [])
+			: [];
+	}
+
+	static async execute (context, query, modelData) {
+		// !!! TEMPORARY MEASURE !!!
+		const RESTRICTED_CHANNELS = [
+			sb.Channel.get("supinic", "twitch"),
+			sb.Channel.get("pajlada", "twitch"),
+			sb.Channel.get("supibot", "twitch")
+		].map(i => i?.ID).filter(Boolean);
+
+		if (!RESTRICTED_CHANNELS.includes(context.channel?.ID)) {
+			return {
+				success: false,
+				reply: `Usage of this model is restricted to specific channels!`
+			};
+		}
+
+		const messages = await this.getHistory(context);
+		const response = await sb.Got("GenericAPI", {
+			method: "POST",
+			throwHttpErrors: false,
+			responseType: "text",
+			url: "https://nexra.aryahcr.cc/api/chat/gpt",
+			json: {
+				messages,
+				model: modelData.url,
+				prompt: query,
+				markdown: false,
+				top_p: 1,
+				frequency_penalty: 0,
+				presence_penalty: 0
+			}
+		});
+
+		let i = 0;
+		const text = response.body;
+		while (text[i] !== "{" || i > text.length) {
+			i++;
+		}
+
+		if (text[i] !== "{") {
+			return {
+				success: false,
+				reply: `Nexra API returned an invalid response!`
+			};
+		}
+
+		response.body = JSON.parse(text.slice(i));
+
+		return { response };
+	}
+
+	static extractMessage (response) {
+		return response.body.gpt;
+	}
+
+	static getUsageRecord () { return 0; }
+	static getCompletionTokens () { return 0; }
+	static getPromptTokens () { return 0; }
+	static getProcessingTime () { return null; }
+
+	static setHistory () {}
+};
