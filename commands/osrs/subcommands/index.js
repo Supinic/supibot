abf830cf88e76fe0bd1db9eeff6bde41831c126d
Supinic <supinic@protonmail.com>
2024-05-01 13:09:19 +0200

    implement stars data fetching

--- commands/osrs/subcommands/index.js
+++ commands/osrs/subcommands/index.js
@@ -2,6 +2,7 @@ module.exports = [
 	require("./item-id.js"),
 	require("./killcount.js"),
 	require("./price.js"),
+	require("./stars.js"),
 	require("./stats.js"),
 	require("./tears-of-guthix.js"),
 	require("./wiki.js")
