53eec9db437610da1ea4be46528be3fd54bd45fd
Supinic <supinic@protonmail.com>
2024-05-14 17:42:34 +0200

    `$check location` - initial implementation

--- commands/check/subcommands.js
+++ commands/check/subcommands.js
@@ -357,6 +357,60 @@ module.exports = (command) => [
 		execute: (context, identifier) => handleErrorInspection(command, context, "error", identifier)
 	},
 	{
+		name: "location",
+		aliases: [],
+		description: "Checks your or someone else's location, as set up within Supibot. Respects private locations.",
+		execute: async (context, identifier) => {
+			let targetUser = context.user;
+			if (identifier) {
+				targetUser = await sb.User.get(identifier, true);
+			}
+
+			if (!targetUser) {
+				return {
+					success: false,
+					reply: "Provided user does not exist!"
+				};
+			}
+			else if (targetUser.Name === context.platform.Self_Name) {
+				return {
+					reply: `My public location: Sitting on top of Supinic's LACK table`
+				};
+			}
+
+			const locationData = await targetUser.getDataProperty("location");
+			if (!locationData) {
+				const pronoun = (context.user.ID === targetUser.ID) ? "You" : "They";
+				return {
+					reply: `${pronoun} have not set up a location.`
+				};
+			}
+
+			const { formatted, hidden = false } = locationData;
+			if (hidden) {
+				if (context.user === targetUser) {
+					await context.sendIntermediateMessage("As your location is private, I private messaged you with it.");
+					return {
+						reply: `Your private location: ${formatted}`,
+						replyWithPrivateMessage: true
+					};
+				}
+				else {
+					return {
+						success: false,
+						reply: `That user's location is private!`
+					};
+				}
+			}
+			else {
+				const pronoun = (context.user.ID === targetUser.ID) ? "Your" : "Their";
+				return {
+					reply: `${pronoun} public location: ${formatted}`
+				};
+			}
+		}
+	},
+	{
 		name: "logs",
 		aliases: [],
 		description: "Checks the Supibot chat line logging status of the current channel.",
