d84b4a54c5bded3802d99c054b39c02d48570efa
Supinic <supinic@protonmail.com>
2024-05-17 17:09:08 +0200

    initial commit

--- commands/faq/index.js
+++ commands/faq/index.js
@@ -1,3 +1,11 @@
+const BASE_GPT_QUERY = sb.Utils.tag.trim `
+	This is the FAQ documentation for a chat bot Supibot. 
+	Its commands are prefixed with "$".
+	You will now assist me by answering my query with the appropriate FAQ item, formatted as needed.
+	If there are no FAQ items applicable, reply with "This is not applicable to Supibot FAQ!".
+	FAQ: 
+`;
+
 module.exports = {
 	Name: "faq",
 	Aliases: [],
@@ -9,32 +17,63 @@ module.exports = {
 	Whitelist_Response: null,
 	Static_Data: null,
 	Code: (async function faq (context, ...args) {
-		if (args.length > 0) {
-			const query = args.join(" ");
-			for (const column of ["Question", "Answer"]) {
-				const data = await sb.Query.getRecordset(rs => rs
-					.select("Question", "Answer")
-					.from("data", "FAQ")
-					.where(`${column} %*like*`, query)
-				);
+		if (args.length === 0) {
+			return {
+				reply: sb.Utils.tag.trim `
+					Check the FAQ here: https://supinic.com/data/faq/list
+				    If you didn't find an answer, make a suggestion with the $suggest command.
+				`
+			};
+		}
 
-				if (data.length === 1) {
-					const row = data[0];
-					const answer = sb.Utils.removeHTML(row.Answer);
-					const question = sb.Utils.removeHTML(row.Question);
+		const faqData = await sb.Query.getRecordset(rs => rs
+			.select("Question", "Answer")
+			.from("data", "FAQ")
+		);
 
-					return {
-						reply: `Q: ${question} A: ${answer}`
-					};
-				}
+		const result = [];
+		for (const item of faqData) {
+			result.push(`Q: ${sb.Utils.removeHTML(item.Question)} -- A: ${sb.Utils.removeHTML(item.Answer)}`);
+		}
+
+		const response = await sb.Got("GenericAPI", {
+			method: "POST",
+			throwHttpErrors: false,
+			responseType: "text",
+			url: "https://nexra.aryahcr.cc/api/chat/gpt",
+			json: {
+				messages: [
+					{ role: "user", content: `${BASE_GPT_QUERY}${result.join("\n")}` }
+				],
+				model: "gpt-4-32k",
+				prompt: args.join(" "),
+				markdown: false,
+				top_p: 1,
+				frequency_penalty: 0,
+				presence_penalty: 0
 			}
+		});
+
+		const index = response.body.indexOf("{");
+		if (index === -1) {
+			return {
+				success: false,
+				reply: `Nexra API returned an invalid response! Try again later.`
+			};
+		}
+
+		try {
+			response.body = JSON.parse(response.body.slice(index));
+		}
+		catch (e) {
+			return {
+				success: false,
+				reply: `Nexra API returned an invalid response! Try again later.`
+			};
 		}
 
 		return {
-			reply: sb.Utils.tag.trim `
-				Check the FAQ here: https://supinic.com/data/faq/list
-			 	If you didn't find an answer, make a suggestion with the $suggest command.
-			`
+			reply: `ℹ ${response.body.gpt}`
 		};
 	}),
 	Dynamic_Description: null
