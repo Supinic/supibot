b1dc8f0c1f278804e0ef5f50e2f3d7d542ca945d
Supinic <supinic@protonmail.com>
2024-07-02 19:45:53 +0200

    refactor `$statistics reminder` to use both live and historic data

--- commands/statistics/types/reminders.js
+++ commands/statistics/types/reminders.js
@@ -3,19 +3,32 @@ module.exports = {
 	aliases: ["reminders"],
 	description: "Shows how many times you or someone else have used reminders.",
 	execute: async (context) => {
-		const data = await sb.Query.getRecordset(rs => rs
-			.select("SUM(Schedule IS NULL) AS Unscheduled")
-			.select("SUM(Schedule IS NOT NULL) AS Scheduled")
-			.from("chat_data", "Reminder")
-			.where("User_From = %n", context.user.ID)
-			.single()
-		);
+		const [liveData, historyData] = await Promise.all([
+			sb.Query.getRecordset(rs => rs
+				.select("SUM(Schedule IS NULL) AS Unscheduled")
+				.select("SUM(Schedule IS NOT NULL) AS Scheduled")
+				.from("chat_data", "Reminder")
+				.where("User_From = %n", context.user.ID)
+				.single()
+			),
+			sb.Query.getRecordset(rs => rs
+				.select("SUM(Schedule IS NULL) AS Unscheduled")
+				.select("SUM(Schedule IS NOT NULL) AS Scheduled")
+				.from("chat_data", "Reminder_History")
+				.where("User_From = %n", context.user.ID)
+				.single()
+			)
+		]);
+
+		const unscheduled = liveData.Unscheduled + historyData.Unscheduled;
+		const scheduled = liveData.Scheduled + historyData.Scheduled;
+		const total = unscheduled + scheduled;
 
 		return {
 			reply: sb.Utils.tag.trim `
-				So far, you have created ${data.Unscheduled} direct reminders
-				and ${data.Scheduled} timed reminders,
-				for a total of ${data.Unscheduled + data.Scheduled}.
+				So far, you have created ${unscheduled} direct reminders
+				and ${scheduled} timed reminders,
+				for a total of ${total}.
 			`
 		};
 	}
