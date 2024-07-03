3e1ad917871fabac363bda98cb67c710bef1b91c
Supinic <supinic@protonmail.com>
2024-07-03 17:08:02 +0200

    add better responses for `$unset reminder`

--- commands/set/subcommands/reminder.js
+++ commands/set/subcommands/reminder.js
@@ -25,21 +25,25 @@ module.exports = {
 		await row.load(ID, true);
 
 		if (!row.loaded) {
-			return {
-				success: false,
-				reply: "That reminder does not exist!"
-			};
+			const historyRow = await sb.Query.getRow("chat_data", "Reminder_History");
+			await historyRow.load(ID, true);
+			if (historyRow.loaded) {
+				return {
+					success: false,
+					reply: "That reminder is already deactivated!"
+				};
+			}
+			else {
+				return {
+					success: false,
+					reply: "There is no reminder with such ID!"
+				};
+			}
 		}
 		else if (row.values.User_From !== context.user.ID && row.values.User_To !== context.user.ID) {
 			return {
 				success: false,
 				reply: "That reminder was not created by you or set for you!"
-			};
-		}
-		else if (!row.values.Active) {
-			return {
-				success: false,
-				reply: "That reminder is already deactivated!"
 			};
 		}
 		else if (context.channel?.ID && !row.values.Schedule && row.values.User_To === context.user.ID) {
