2df0ea08e10c9fb6e5412d825f2e05386839956e
Supinic <supinic@protonmail.com>
2024-07-09 18:18:10 +0200

    added different types of queries - `type` parameter

--- commands/_summary/index.js
+++ commands/_summary/index.js
@@ -1,7 +1,19 @@
 const GptNexra = require("../gpt/gpt-nexra.js");
 
 const RAW_TEXT_REGEX = /^\[(?<date>[\d-\s:]+)]\s+#\w+\s+(?<username>\w+):\s+(?<message>.+?)$/;
-const BASE_MESSAGE = "Concisely summarize the following messages from an online chatroom: \n";
+
+const BASE_QUERY = sb.Utils.tag.trim `
+	Concisely summarize the following messages from an online chatroom
+	(attempt to ignore chat bots replying to users' commands, and assume unfamiliar words to be emotes)
+`;
+
+const QUERIES = {
+	base: `${BASE_QUERY}`,
+	topical: `${BASE_QUERY} into a numbered list of discussion topics (maximum of 5)`,
+	single: `${BASE_QUERY} into a description of a single, most important topic being discussed - do not include any additional topics`,
+	user: `${BASE_QUERY} into a bullet point list of username-specific topics `
+};
+
 const RUSTLOG_RESPONSES = {
 	403: "That channel has opted out from being logged!",
 	404: "That channel is not being logged at the moment!",
@@ -16,7 +28,7 @@ module.exports = {
 	Description: "TODO",
 	Flags: ["mention","pipe"],
 	Params: [
-		{ name: "channel", type: "string" }
+		{ name: "type", type: "string" }
 	],
 	Whitelist_Response: null,
 	Code: (async function TODO (context, channelInput) {
@@ -62,13 +74,22 @@ module.exports = {
 
 		const gptData = logsResponse.body
 			.split(/\r?\n/)
-			.map(i => i.match(RAW_TEXT_REGEX))
+			.map(i => i.match(RAW_TEXT_REGEX)?.groups)
 			.filter(Boolean)
 			.sort((a, b) => new sb.Date(a.date) - new sb.Date(b.date))
 			.map(i => `${i.username}: ${i.message}`)
 			.join("\n");
 
-		const { response } = await GptNexra.execute(context, `${BASE_MESSAGE} ${gptData}`, {
+		const queryType = context.params.type ?? "base";
+		const query = QUERIES[queryType];
+		if (!query) {
+			return {
+				success: false,
+				reply: `Invalid query type provided! Use one of: ${Object.keys(QUERIES).join(", ")}`
+			};
+		}
+
+		const { response } = await GptNexra.execute(context, `${query}\n\n${gptData}`, {
 			url: "gpt-4-32k"
 		});
 
