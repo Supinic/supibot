a7c52423658a9320eda841198fe43c24c25da384
Supinic <supinic@protonmail.com>
2024-07-09 18:03:27 +0200

    switch to text API instead of JSON
    - parse with regex
    - also move channel resolution to command
    - move channel from param to first user argument

--- commands/_summary/index.js
+++ commands/_summary/index.js
@@ -1,6 +1,12 @@
 const GptNexra = require("../gpt/gpt-nexra.js");
 
+const RAW_TEXT_REGEX = /^\[(?<date>[\d-\s:]+)]\s+#\w+\s+(?<username>\w+):\s+(?<message>.+?)$/;
 const BASE_MESSAGE = "Concisely summarize the following messages from an online chatroom: \n";
+const RUSTLOG_RESPONSES = {
+	403: "That channel has opted out from being logged!",
+	404: "That channel is not being logged at the moment!",
+	default: "Unspecified error occured! Try again later."
+};
 
 module.exports = {
 	Name: "_summary",
@@ -13,23 +19,35 @@ module.exports = {
 		{ name: "channel", type: "string" }
 	],
 	Whitelist_Response: null,
-	Code: (async function TODO (context) {
-		if (context.platform.name !== "twitch") {
+	Code: (async function TODO (context, channelInput) {
+		let channel = channelInput;
+		if (!channel) {
+			if (context.platform.name !== "twitch") {
+				return {
+					success: false,
+					reply: `Pepega TODO`
+				};
+			}
+
+			channel = context.channel.Name;
+		}
+
+		const twitch = sb.Platform.get("twitch");
+		const channelId = await twitch.getUserID(channel);
+		if (!channelId) {
 			return {
 				success: false,
-				reply: `Pepega TODO`
+				reply: `No user TODO`
 			};
 		}
 
-		const channel = context.params.channel ?? context.channel.Name;
 		const { year, month, day } = new sb.Date(sb.Date.getTodayUTC());
-		const url = `https://logs.ivr.fi/channel/${channel}/${year}/${month}/${day}`;
-
+		const url = `https://logs.ivr.fi/channelid/${channelId}/${year}/${month}/${day}`;
 		const logsResponse = await sb.Got("GenericAPI", {
 			url,
 			throwHttpErrors: false,
+			responseType: "text",
 			searchParams: {
-				json: "1",
 				reverse: "1",
 				limit: "20"
 			}
@@ -38,13 +56,16 @@ module.exports = {
 		if (!logsResponse.ok) {
 			return {
 				success: false,
-				reply: `Logs: oops`
+				reply: RUSTLOG_RESPONSES[logsResponse.statusCode] ?? RUSTLOG_RESPONSES.default
 			};
 		}
 
-		const gptData = logsResponse.body.messages
-			.sort((a, b) => new sb.Date(a.timestamp) - new sb.Date(b.timestamp))
-			.map(i => `${i.displayName}: ${i.text}`)
+		const gptData = logsResponse.body
+			.split(/\r?\n/)
+			.map(i => i.match(RAW_TEXT_REGEX))
+			.filter(Boolean)
+			.sort((a, b) => new sb.Date(a.date) - new sb.Date(b.date))
+			.map(i => `${i.username}: ${i.message}`)
 			.join("\n");
 
 		const { response } = await GptNexra.execute(context, `${BASE_MESSAGE} ${gptData}`, {
