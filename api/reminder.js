538783d34457683303ea7ca7f9df08e7a23100f9
Supinic <supinic@protonmail.com>
2024-07-30 16:39:17 +0200

    Internal Supibot API: Unset reminders (#93)

--- api/reminder.js
+++ api/reminder.js
@@ -21,5 +21,54 @@ module.exports = {
 				result
 			}
 		};
+	},
+	unset: async (req, res, url) => {
+		const user = url.searchParams.get("user");
+		if (!user) {
+			return {
+				statusCode: 400,
+				error: { message: "No user name provided" }
+			};
+		}
+
+		const rawId = url.searchParams.get("id");
+		const id = Number(rawId);
+		if (!rawId || !sb.Utils.isValidInteger(id)) {
+			return {
+				statusCode: 400,
+				error: { message: "No or malformed reminder ID provided" }
+			};
+		}
+
+		/** @type {Reminder | null} */
+		const reminder = sb.Reminder.get(id);
+		if (!reminder) {
+			return {
+				statusCode: 400,
+				error: { message: "Reminder does not exist or has already been deactivated" }
+			};
+		}
+
+		const userData = await sb.User.get(user, true);
+		if (!userData) {
+			return {
+				statusCode: 400,
+				error: { message: "User does not exist" }
+			};
+		}
+
+		if (reminder.User_From !== userData.ID && reminder.User_To !== userData.ID) {
+			return {
+				statusCode: 400,
+				error: { message: "Provided user is not the author nor the target of provided reminder" }
+			};
+		}
+
+		await reminder.deactivate(true, true);
+
+		return {
+			statusCode: 200,
+			data: { message: "OK" }
+		};
 	}
 };
