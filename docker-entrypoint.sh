885a9a8d96964937119e41cf8ae62115e55fdc8c
supinic <supinic@protonmail.com>
2024-08-07 21:08:27 +0200

    fix entrypoint typo

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@ export DEFAULT_PACKAGEMANAGER=yarn
 yarn run init-database
 yarn run auto-setup
 
-if [[ -z "${DEBUG_MODE}" ]]; then
+if [ -z "${DEBUG_MODE}" ]; then
   yarn debug
 else
   yarn start
