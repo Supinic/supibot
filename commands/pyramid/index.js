6f35a50643f1967bf40bcfdc46d582c9f5e9d2c9
supinic <supinic@protonmail.com>
2024-09-07 15:03:08 +0200

    Revert "`pyramid` improve speed"
    This reverts commit 977eb4d8fd3ede1fd58e2951baf979e090a119a7.

--- commands/pyramid/index.js
+++ commands/pyramid/index.js
@@ -49,12 +49,12 @@ module.exports = {
 
 		for (let i = 1; i <= size; i++) {
 			context.channel.send(emote.repeat(i));
-			await setTimeout(100);
+			await setTimeout(250);
 		}
 
 		for (let i = (size - 1); i > 0; i--) {
 			context.channel.send(emote.repeat(i));
-			await setTimeout(100);
+			await setTimeout(250);
 		}
 
 		return null;
