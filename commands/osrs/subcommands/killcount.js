24189d40c129916119f9e7c9b049372f3b6e712f
supinic <supinic@protonmail.com>
2024-10-01 18:10:32 +0200

    `osrs` add `force` parameter option to `osrs kc`

--- commands/osrs/subcommands/killcount.js
+++ commands/osrs/subcommands/killcount.js
@@ -29,7 +29,8 @@ module.exports = {
 		}
 
 		const data = await OsrsUtils.fetch(user, {
-			seasonal: Boolean(context.params.seasonal)
+			seasonal: Boolean(context.params.seasonal),
+			force: Boolean(context.params.force)
 		});
 
 		if (data.success === false) {
