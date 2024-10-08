f0da003170fa906fbde127f026afdbfd6454af9d
supinic <supinic@protonmail.com>
2024-10-08 19:23:46 +0200

    `link` hide the command from public listing

--- commands/link/index.js
+++ commands/link/index.js
@@ -4,7 +4,7 @@ module.exports = {
 	Author: "supinic",
 	Cooldown: 5000,
 	Description: "Verifies your account linking challenge across platforms. You should only ever use this command if you are prompted to.",
-	Flags: ["mention","system"],
+	Flags: ["developer","mention","system"],
 	Params: null,
 	Whitelist_Response: null,
 	Code: (async function link (context, challengeString) {
@@ -20,7 +20,7 @@ module.exports = {
 				reply: "If you have a challenge string, use it. If you don't have one, don't use this command."
 			};
 		}
-	
+
 		const challengeID = await sb.Query.getRecordset(rs => rs
 			.select("ID")
 			.from("chat_data", "User_Verification_Challenge")
@@ -32,26 +32,26 @@ module.exports = {
 			.single()
 			.flat("ID")
 		);
-	
+
 		if (typeof challengeID !== "number") {
 			return {
 				success: false,
 				reply: `No active verification found!`
 			};
 		}
-	
+
 		const row = await sb.Query.getRow("chat_data", "User_Verification_Challenge");
 		await row.load(challengeID);
-	
+
 		const sourcePlatform = sb.Platform.get(row.values.Platform_From);
 		const targetPlatform = sb.Platform.get(row.values.Platform_To);
-		
+
 		const idColumnName = `${sourcePlatform.capital}_ID`;
 		await context.user.saveProperty(idColumnName, row.values.Specific_ID);
-		
+
 		row.values.Status = "Completed";
 		await row.save();
-	
+
 		return {
 			reply: `Verification completed! You may now use the bot on ${sourcePlatform.capital} as well as ${targetPlatform.capital}.`
 		};
