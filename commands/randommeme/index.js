3e1b7a998085e3c8ba80bff1c468a646e7757d58
supinic <supinic@protonmail.com>
2024-12-05 13:40:00 +0100

    `randommeme` use credentials if available

--- commands/randommeme/index.js
+++ commands/randommeme/index.js
@@ -1,13 +1,22 @@
 let redditGotInstance;
 const redditGot = (...args) => {
-	redditGotInstance ??= sb.Got.get("GenericAPI").extend({
+	const options = {
 		prefixUrl: "https://www.reddit.com/r/",
 		throwHttpErrors: false,
 		headers: {
 			Cookie: "_options={%22pref_quarantine_optin%22:true,%22pref_gated_sr_optin%22:true};"
 		}
-	});
+	};
 
+	if (process.env.API_REDDIT_USERNAME && process.env.API_REDDIT_SECRET) {
+		options.username = process.env.API_REDDIT_USERNAME;
+		options.password = process.env.API_REDDIT_SECRET;
+	}
+	else {
+		console.log("$rm command will not use authorized requests - no credentials found");
+	}
+
+	redditGotInstance ??= sb.Got.get("GenericAPI").extend(options);
 	return redditGotInstance(...args);
 };
 
