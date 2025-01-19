2cd3b9dcc83f174166df5e4f4e38cbceabad5411
supinic <supinic@protonmail.com>
2025-01-19 13:46:31 +0100

    refactored `fish` subcommands to `import`

--- commands/fish/subcommands/index.js
+++ commands/fish/subcommands/index.js
@@ -1,12 +1,21 @@
+import BuySubcommand from "./buy.js";
+import ConfigSubcommand from "./config.js";
+import FishSubcommand from "./fish.js";
+import LeaderboardSubcommand from "./leaderboard.js";
+import SellSubcommand from "./sell.js";
+import ShowSubcommand from "./show.js";
+import StatsSubcommand from "./stats.js";
+import TrapSubcommand from "./trap.js";
+
 const subcommands = [
-	require(`./buy.js`),
-	require(`./config.js`),
-	require(`./fish.js`),
-	require(`./leaderboard.js`),
-	require(`./sell.js`),
-	require(`./show.js`),
-	require(`./stats.js`),
-	require(`./trap.js`)
+	BuySubcommand,
+	ConfigSubcommand,
+	FishSubcommand,
+	LeaderboardSubcommand,
+	SellSubcommand,
+	ShowSubcommand,
+	StatsSubcommand,
+	TrapSubcommand
 ];
 
 export default {
