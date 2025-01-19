d8b3a2e33b7d601a76434e62d3a2791029bce78d
supinic <supinic@protonmail.com>
2025-01-19 13:44:27 +0100

    refactored `bot` subcommands to `import`

--- commands/bot/subcommands/index.js
+++ commands/bot/subcommands/index.js
@@ -1,13 +1,23 @@
+import BanphraseApiSubcommand from "./banphrase-api.js";
+import CheckLiveSubcommand from "./check-live.js";
+import EnableRustlogSubcommand from "./enable-rustlog.js";
+import GlobalEmotesSubcommand from "./global-emotes.js";
+import LinksSubcommand from "./links.js";
+import OfflineOnlySubcommand from "./offline-only.js";
+import RejoinSubcommand from "./rejoin.js";
+import RenameSubcommand from "./rename.js";
+import ToggleSubcommand from "./toggle.js";
+
 const subcommands = [
-	require("./banphrase-api.js"),
-	require("./check-live.js"),
-	require("./enable-rustlog.js"),
-	require("./global-emotes.js"),
-	require("./links.js"),
-	require("./offline-only.js"),
-	require("./rejoin.js"),
-	require("./rename.js"),
-	require("./toggle.js")
+	BanphraseApiSubcommand,
+	CheckLiveSubcommand,
+	EnableRustlogSubcommand,
+	GlobalEmotesSubcommand,
+	LinksSubcommand,
+	OfflineOnlySubcommand,
+	RejoinSubcommand,
+	RenameSubcommand,
+	ToggleSubcommand
 ];
 
 export default {
