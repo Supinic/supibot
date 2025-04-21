1f64cd676788bf151e60925c5356bf86f771eeb0
supinic <supinic@protonmail.com>
2025-04-21 19:33:41 +0200

    `$afk` rewritten to TS

--- commands/afk/index.ts
+++ commands/afk/index.ts
@@ -1,13 +1,25 @@
+import type { CommandDefinition } from "../../classes/command.js";
+import type { Status as AfkStatus } from "../../classes/afk.js";
 import afkDefinitions from "../../classes/afk-definitions.json" with { type: "json" };
+import { SupiError } from "supi-core";
 
-const { invocations, specialSuffixes } = afkDefinitions;
+type Invocation = {
+	name: AfkStatus;
+	specialSuffix?: "foodEmojis";
+	aliases?: string[];
+	status: string;
+	textSuffix: string;
+	noTextString: string;
+};
+
+const invocations = afkDefinitions.invocations as Invocation[];
+const { specialSuffixes } = afkDefinitions;
 const STATUS_LENGTH_CHARACTER_LIMIT = 2000;
 
 export default {
 	Name: "afk",
-	Aliases: ["gn","brb","shower","food","lurk","poop","💩","work","study","nap"],
-	Author: "supinic",
-	Cooldown: 10000,
+	Aliases: ["gn", "brb", "shower", "food", "lurk", "poop", "💩", "work", "study", "nap"],
+	Cooldown: 10_000,
 	Description: "Flags you as AFK. Supports a custom AFK message.",
 	Flags: ["pipe"],
 	Params: null,
@@ -20,31 +32,39 @@ export default {
 			};
 		}
 
-		const { invocation } = context;
+		const invocation = context.invocation as AfkStatus; // @todo remove type cast when CommandDefinition is fully generic
 		const target = invocations.find(i => i.name === invocation || i.aliases?.includes(invocation));
+		if (!target) {
+			throw new SupiError({
+			    message: "Assert error: AFK invocation does not match",
+				args: { invocation }
+			});
+		}
 
 		let text = args.join(" ").trim();
 		if (text.length === 0 && target.noTextString) {
-			text = target.noTextString ?? "(no message)";
+			text = target.noTextString;
 		}
 
 		if (target.textSuffix) {
 			text = `${text} ${target.textSuffix}`;
 		}
 		else if (target.specialSuffix) {
-			const suffixes = specialSuffixes[target.specialSuffix] ?? [];
-			const suffix = core.Utils.randArray(suffixes) ?? "";
+			const suffixes = specialSuffixes[target.specialSuffix];
+			const suffix = core.Utils.randArray(suffixes);
+
 			text = `${text} ${suffix}`;
 		}
 
 		await sb.AwayFromKeyboard.set(context.user, {
 			Text: core.Utils.wrapString(text, STATUS_LENGTH_CHARACTER_LIMIT, { keepWhitespace: false }),
-			Status: target.name ?? invocation,
+			Status: target.name,
 			Silent: false,
 			Interrupted_ID: null
 		});
 
 		return {
+			success: true,
 			partialReplies: [
 				{
 					bancheck: true,
@@ -61,7 +81,7 @@ export default {
 			]
 		};
 	}),
-	Dynamic_Description: (async (prefix) => [
+	Dynamic_Description: (prefix) => [
 		"Flags you as AFK (away from keyboard).",
 		"While you are AFK, others can check if you are AFK.",
 		"On your first message while AFK, the status ends and the bot will announce you coming back.",
@@ -81,5 +101,5 @@ export default {
 		``,
 
 		`and more - check the aliases`
-	])
-};
+	]
+} satisfies CommandDefinition;
