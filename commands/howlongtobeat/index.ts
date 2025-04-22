c7962daec4f26681ab8b2f9a3f3dea0ac1e90533
supinic <supinic@protonmail.com>
2025-04-22 18:31:10 +0200

    `$howlongtobeat` rewritten to TS

--- commands/howlongtobeat/index.ts
+++ commands/howlongtobeat/index.ts
@@ -1,3 +1,5 @@
+import type { CommandDefinition } from "../../classes/command.js";
+
 // Need to fetch a randomized hash to attach to the ~~api/search~~ ~~api/find~~ ~~api/lookup~~ ~~api/s~~ ~~api/ouch~~ api/seek endpoint
 // Reference: https://github.com/ScrappyCocco/HowLongToBeat-PythonAPI/issues/25
 // Reference from `search` to `find`: https://github.com/ScrappyCocco/HowLongToBeat-PythonAPI/pull/35
@@ -9,8 +11,8 @@ const FILE_PREFIX = "_next/static/chunks/pages";
 const FILE_HASH_REGEX = /static\/chunks\/pages\/(_app-\w+?\.js)/;
 const ENDPOINT_HASH_REGEX = /\/api\/seek\/".concat\("(\w+)"\)\s*(.concat\("(\w+)"\))?/;
 
-const fetchFileHash = async (force = false) => {
-	const existing = await core.Cache.getByPrefix(HLTB_JS_FILE_HASH_KEY);
+const fetchFileHash = async (force: boolean = false) => {
+	const existing = await core.Cache.getByPrefix(HLTB_JS_FILE_HASH_KEY) as string | undefined;
 	if (!force && existing) {
 		return existing;
 	}
@@ -36,8 +38,8 @@ const fetchFileHash = async (force = false) => {
 	return match[1];
 };
 
-const fetchEndpointHash = async (fileHash, force = false) => {
-	const existing = await core.Cache.getByPrefix(HLTB_ENDPOINT_HASH_KEY);
+const fetchEndpointHash = async (fileHash: string, force: boolean = false) => {
+	const existing = await core.Cache.getByPrefix(HLTB_ENDPOINT_HASH_KEY) as string | undefined;
 	if (!force && existing) {
 		return existing;
 	}
@@ -65,16 +67,62 @@ const fetchEndpointHash = async (fileHash, force = false) => {
 	return hash;
 };
 
+type HltbGame = {
+	comp_100: number;
+	comp_100_count: number;
+	comp_all: number;
+	comp_all_count: number;
+	comp_lvl_co: number;
+	comp_lvl_combine: number;
+	comp_lvl_mp: number;
+	comp_lvl_sp: number;
+	comp_main: number;
+	comp_main_count: number;
+	comp_plus: number;
+	comp_plus_count: number;
+	count_backlog: number;
+	count_comp: number;
+	count_playing: number;
+	count_retired: number;
+	count_review: number;
+	count_speedrun: number;
+	game_alias: string;
+	game_id: number;
+	game_image: string;
+	game_name: string;
+	game_name_date: number;
+	game_type: string;
+	invested_co: number;
+	invested_co_count: number;
+	invested_mp: number;
+	invested_mp_count: number;
+	profile_platform: string;
+	profile_popular: number;
+	release_world: number;
+	review_score: number;
+};
+type HltbData = {
+	category: string;
+	color: string;
+	count: number;
+	data: HltbGame[];
+	displayModifier: unknown;
+	pageCurrent: number;
+	pageSize: number;
+	pageTotal: number;
+	title: string;
+	userData: unknown[];
+};
+
 export default {
 	Name: "howlongtobeat",
 	Aliases: ["hltb"],
-	Author: "supinic",
 	Cooldown: 5000,
 	Description: "For a provided game, shows how long it takes to beat based on the https://howlongtobeat.com website's data.",
 	Flags: ["mention","non-nullable","pipe"],
 	Params: null,
 	Whitelist_Response: null,
-	Code: (async function howLongToBeat (context, ...args) {
+	Code: async function howLongToBeat (context, ...args) {
 		if (args.length === 0) {
 			return {
 				success: false,
@@ -98,7 +146,7 @@ export default {
 			};
 		}
 
-		const response = await core.Got.get("FakeAgent")({
+		const response = await core.Got.get("FakeAgent")<HltbData>({
 			url: `https://howlongtobeat.com/api/seek/${endpointHash}`,
 			method: "POST",
 			throwHttpErrors: false,
@@ -143,10 +191,6 @@ export default {
 			};
 		}
 
-		// 	main: gameData.comp_main_count,
-		// 	plus: gameData.comp_plus_count,
-		// 	full: gameData.comp_100_count,
-		// 	all: gameData.comp_all_count
 		const { data } = response.body;
 		if (data.length === 0) {
 			return {
@@ -174,6 +218,6 @@ export default {
 				${url}
 			`
 		};
-	}),
+	},
 	Dynamic_Description: null
-};
+} satisfies CommandDefinition;
