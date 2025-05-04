fcf15d8ce61c734e97b2dfae0f421bcc26954914
supinic <supinic@protonmail.com>
2025-05-04 13:13:13 +0200

    `$restart` rewritten to TS

--- commands/restart/index.ts
+++ commands/restart/index.ts
@@ -1,11 +1,13 @@
 import { promisify } from "node:util";
 import { exec } from "node:child_process";
+
 import config from "../../config.json" with { type: "json" };
+import type { CommandDefinition } from "../../classes/command.js";
 
 const shell = promisify(exec);
 const { basePath } = config;
 
-const methods = {
+const restartMethods = {
 	pull: {
 		message: "git pull",
 		commands: [
@@ -21,12 +23,14 @@ const methods = {
 		message: "yarn build",
 		commands: ["yarn build"]
 	}
-};
+} as const;
+const isRestartMethod = (input: string): input is keyof typeof restartMethods => (
+	Object.keys(restartMethods).includes(input)
+);
 
 export default {
 	Name: "restart",
 	Aliases: null,
-	Author: "supinic",
 	Cooldown: 10_000,
 	Description: "Restarts the bot. Optionally, also pulls git changes and/or upgrades packages via yarn.",
 	Flags: ["system", "whitelist"],
@@ -34,14 +38,14 @@ export default {
 	Whitelist_Response: "Only available to administrators or helpers!",
 	Code: async function restart (context, ...commands) {
 		for (const name of commands) {
-			const reloadCommand = methods[name];
-			if (!reloadCommand) {
+			if (!isRestartMethod(name)) {
 				return {
 					success: false,
-					reply: `Incorrect reload command provided! Use one of: ${Object.keys(methods).join(", ")}`
+					reply: `Incorrect reload command provided! Use one of: ${Object.keys(restartMethods).join(", ")}`
 				};
 			}
 
+			const reloadCommand = restartMethods[name];
 			await context.sendIntermediateMessage(`VisLaud 👉 ${reloadCommand.message}`);
 
 			for (const command of reloadCommand.commands) {
@@ -50,12 +54,16 @@ export default {
 		}
 
 		await context.sendIntermediateMessage("Restarting...");
+
+		// It is fully intended to exit the process here.
 		// eslint-disable-next-line unicorn/no-process-exit
 		setTimeout(() => process.exit(0), 1000);
 
-		return null;
+		return {
+			reply: null
+		};
 	},
-	Dynamic_Description: async () => [
+	Dynamic_Description: () => [
 		"Restarts the process of Supibot or the supinic.com website.",
 		"Only usable by administrators and whitelisted users.",
 		"The subcommands, except for \"all\" can be combined between each other",
@@ -80,4 +88,4 @@ export default {
 		"<code>$restart bot all</code>",
 		"Combination of all commands in this order: pull, prod-update, build, exit."
 	]
-};
+} satisfies CommandDefinition;
