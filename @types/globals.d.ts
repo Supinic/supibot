e1c14dfe48bb002e0f473f51af13ee5db5f73061
supinic <supinic@protonmail.com>
2025-10-03 17:04:52 +0200

    create IVR API `user` zod schema

--- @types/globals.d.ts
+++ @types/globals.d.ts
@@ -54,68 +54,7 @@ export type TypeExtract<T, U> = {
     [P in OnlyKeysOfType<T, U>]: U;
 };
 
-type BaseIvrUserData = {
-    login: string;
-    id: string;
-    bio: string | null;
-    follows: null;
-    followers: number;
-    profileViewCount: null;
-    chatColor: string;
-    logo: string;
-    banner: string;
-    verifiedBot: null;
-    createdAt: string;
-    updatedAt: string;
-    emotePrefix: string | null;
-    roles: {
-        isAffliate: boolean;
-        isPartner: boolean;
-        isStaff: boolean;
-    };
-    badges: {
-        setId: string;
-        title: string;
-        description: string;
-        version: string;
-    }[];
-    chatterCount: number | null;
-    chatSettings: {
-        chatDelayMs: number;
-        followersOnlyDurationMinutes: number | null;
-        slowModeDurationSeconds: number | null;
-        blockLinks: boolean;
-        isSubscribersOnlyModeEnabled: boolean;
-        isEmoteOnlyModeEnabled: boolean;
-        isFastSubsModeEnabled: boolean;
-        isUniqueChatModeEnabled: boolean;
-        requireVerifiedAccount: boolean;
-        rules: string[];
-    };
-    stream: {
-        title: string;
-        id: string;
-        createdAt: string;
-        type: "live" | "rerun";
-        viewersCount: number;
-        game: {
-            displayName: string;
-        } | null;
-    } | null;
-    lastBroadcast: {
-        startedAt: string;
-        title: string;
-    } | null;
-    panels: { id: string; }[];
-}
-type RegularIvrUserData = BaseIvrUserData & {
-    banned: false;
-};
-type BannedIvrUserData = BaseIvrUserData & {
-    banned: true;
-    banReason: "TOS_INDEFINITE" | "TOS_TEMPORARY" | "DMCA" | "DEACTIVATED";
-};
-export type IvrUserData = RegularIvrUserData | BannedIvrUserData;
+export type { IvrUserData } from "../utils/schemas.js"
 
 export type IvrClipData = {
     clipKey: string | null;
---
+++ utils/schemas.ts
@@ -0,0 +1,85 @@
+// @todo figure out where to place this file properly within the project
+import * as z from "zod";
+
+const BaseIvrUserDataSchema = z.object({
+	login: z.string(),
+	id: z.string(),
+	bio: z.string().nullable(),
+	follows: z.null(),
+	followers: z.number(),
+	profileViewCount: z.null(),
+	chatColor: z.string(),
+	logo: z.string(),
+	banner: z.string(),
+	verifiedBot: z.null(),
+	createdAt: z.string(),
+	updatedAt: z.string(),
+	emotePrefix: z.string().nullable(),
+	roles: z.object({
+		isAffiliate: z.boolean(),
+		isPartner: z.boolean(),
+		isStaff: z.boolean()
+	}),
+	badges: z.array(
+		z.object({
+			setId: z.string(),
+			title: z.string(),
+			description: z.string(),
+			version: z.string()
+		})
+	),
+	chatterCount: z.number().nullable(),
+	chatSettings: z.object({
+		chatDelayMs: z.number(),
+		followersOnlyDurationMinutes: z.number().nullable(),
+		slowModeDurationSeconds: z.number().nullable(),
+		blockLinks: z.boolean(),
+		isSubscribersOnlyModeEnabled: z.boolean(),
+		isEmoteOnlyModeEnabled: z.boolean(),
+		isFastSubsModeEnabled: z.boolean(),
+		isUniqueChatModeEnabled: z.boolean(),
+		requireVerifiedAccount: z.boolean(),
+		rules: z.array(z.string())
+	}),
+	stream: z
+		.object({
+			title: z.string(),
+			id: z.string(),
+			createdAt: z.string(),
+			type: z.union([z.literal("live"), z.literal("rerun")]),
+			viewersCount: z.number(),
+			game: z
+				.object({
+					displayName: z.string()
+				})
+				.nullable()
+		})
+		.nullable(),
+	lastBroadcast: z
+		.object({
+			startedAt: z.string(),
+			title: z.string()
+		})
+		.nullable(),
+	panels: z.array(z.object({ id: z.string() }))
+});
+
+const RegularIvrUserDataSchema = BaseIvrUserDataSchema.extend({
+	banned: z.literal(false)
+});
+
+const BannedIvrUserDataSchema = BaseIvrUserDataSchema.extend({
+	banned: z.literal(true),
+	banReason: z.union([
+		z.literal("TOS_INDEFINITE"),
+		z.literal("TOS_TEMPORARY"),
+		z.literal("DMCA"),
+		z.literal("DEACTIVATED")
+	])
+});
+
+export const ivrUserDataSchema = z.array(
+	z.union([RegularIvrUserDataSchema, 	BannedIvrUserDataSchema])
+);
+
+export type IvrUserData = z.infer<typeof ivrUserDataSchema>;
