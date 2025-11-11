28c4d8e6be46302287cc59012c85a78a0cd6bcbe
supinic <supinic@protonmail.com>
2025-11-11 18:45:38 +0100

    `$gpt` improve system message by adding bot/platform/channel info

--- commands/gpt/gpt-openai.ts
+++ commands/gpt/gpt-openai.ts
@@ -66,9 +66,21 @@ const getHistory = async (context: GptContext, query: string, options: { noSyste
 		];
 	}
 	else {
+		const { platform, channel } = context;
+		const channelString = (channel)
+			? `, in channel ${channel.Description ?? channel.Name}`
+			: "";
+
+		const contextSystemMessage = core.Utils.tag.trim `
+			You are being queried on behalf of ${platform.selfName},
+			a chat bot running on ${platform.name}${channelString}.
+			Don't reference the chat bot directly, but rather assume its position,
+			and reply in first person when queried via name.
+		`;
+
 		return [
 			...promptHistory,
-			{ role: "user", content: `${DEFAULT_SYSTEM_MESSAGE} ${query}` }
+			{ role: "user", content: `${DEFAULT_SYSTEM_MESSAGE} ${contextSystemMessage} ${query}` }
 		];
 	}
 };
