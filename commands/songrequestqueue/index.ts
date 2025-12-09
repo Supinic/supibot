7da93b800c364fac89e9f5539930f7746a876051
supinic <supinic@protonmail.com>
2025-12-09 20:12:18 +0100

    convert `$srq` to TS + mpv client

--- commands/songrequestqueue/index.ts
+++ commands/songrequestqueue/index.ts
@@ -1,7 +1,9 @@
+import { SupiDate } from "supi-core";
+import { declare } from "../../classes/command.js";
 import cacheKeys from "../../utils/shared-cache-keys.json" with { type: "json" };
 const { SONG_REQUESTS_STATE, SONG_REQUESTS_MPV_PAUSED } = cacheKeys;
 
-export default {
+export default declare({
 	Name: "songrequestqueue",
 	Aliases: ["srq","queue"],
 	Author: "supinic",
@@ -10,54 +12,48 @@ export default {
 	Flags: ["mention","pipe","whitelist"],
 	Params: [],
 	Whitelist_Response: "Only available in supinic's channel.",
-	Code: (async function songRequestQueue (context) {
+	Code: (async function songRequestQueue () {
+		if (!sb.MpvClient) {
+			return {
+				success: false,
+				reply: "mpv client is not available! Check configuration if this is required."
+			};
+		}
+
 		const state = await core.Cache.getByPrefix(SONG_REQUESTS_STATE);
 		if (!state || state === "off") {
 			return {
+				success: true,
 				reply: "Song requests are currently turned off. Check out the history up to 14 days back: https://supinic.com/stream/song-request/history"
 			};
 		}
-		else if (state === "cytube") {
-			const command = sb.Command.get("cytube");
-			if (command) {
-				const result = await command.execute(context);
-				return {
-					reply: `Song requests are currently using Cytube. Join here: ${result.reply} :)`
-				};
-			}
-			else {
-				return {
-					reply: `Song requests are currently using Cytube.`
-				};
-			}
-		}
 
-		const data = await sb.MpvClient.getNormalizedPlaylist();
-		if (data.length === 0) {
+		const playlist = await sb.MpvClient.getPlaylist();
+		if (playlist.length === 0) {
 			return {
+				success: true,
 				reply: "No songs are currently queued. Check history here: https://supinic.com/stream/song-request/history"
 			};
 		}
 
-		const status = sb.MpvClient.getCurrentStatus();
-		const total = data.reduce((acc, cur) => acc + cur.Duration, 0);
-		const current = data.find(i => i.Status === "Current");
+		const { position } = await sb.MpvClient.getUpdatedStatus();
+		const total = playlist.reduce((acc, cur) => acc + (cur.duration ?? 0), 0);
 
-		const length = total - (current?.End_Time ?? status.position);
-		const delta = core.Utils.timeDelta(Math.round(sb.Date.now() + length * 1000), true);
-
+		const length = total - (position ?? 0);
+		const delta = core.Utils.timeDelta(Math.round(SupiDate.now() + length * 1000), true);
 		const pauseState = await core.Cache.getByPrefix(SONG_REQUESTS_MPV_PAUSED);
 		const pauseString = (pauseState === true)
 			? "Song requests are paused at the moment."
 			: "";
 
 		return {
+			success: true,
 			reply: core.Utils.tag.trim `
-				There are ${data.length} videos in the queue, with a total length of ${delta}.
+				There are ${playlist.length} videos in the queue, with a total length of ${delta}.
 				${pauseString} 
 				Check the queue here: https://supinic.com/stream/song-request/queue
 			`
 		};
 	}),
 	Dynamic_Description: null
-};
+});
