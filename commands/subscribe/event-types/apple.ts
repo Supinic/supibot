b9c819ffebc51d5a794d7d7f225c5791237ab2ab
badoge <18620902+badoge@users.noreply.github.com>
2026-01-02 08:34:38 -0800

    `$subscribe` add Apple Newsroom RSS feed (#153)

---
+++ commands/subscribe/event-types/apple.ts
@@ -0,0 +1,18 @@
+import { RssEventDefinition } from "../generic-event.js";
+
+export default {
+	name: "Apple Newsroom",
+	aliases: ["apple"],
+	notes: "Posts new Apple Newsroom articles",
+	channelSpecificMention: true,
+	response: {
+		added: "You will now be pinged whenever a new Apple Newsroom article is posted.",
+		removed: "You will no longer receive pings for Apple Newsroom articles."
+	},
+	generic: true,
+	cronExpression: "0 */5 * * * *",
+	subName: "Apple Newsroom article",
+	type: "rss",
+	cacheKey: "apple-newsroom-last-publish-date",
+	url: "https://www.apple.com/newsroom/rss-feed.rss"
+} satisfies RssEventDefinition;
