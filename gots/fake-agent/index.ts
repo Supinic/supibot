20ad0bd1971baf343a31086e02de98524c161662
supinic <supinic@protonmail.com>
2026-01-06 19:42:09 +0100

    update `FakeAgent` user agent

--- gots/fake-agent/index.ts
+++ gots/fake-agent/index.ts
@@ -5,7 +5,7 @@ export default {
 	optionsType: "object",
 	options: {
 		headers: {
-			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36"
+			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.7444.175 Safari/537.36"
 		}
 	},
 	parent: "Global",
