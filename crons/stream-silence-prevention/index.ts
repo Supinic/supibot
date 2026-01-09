464696a96e3778a10f8fe865afbc5ce79b43e690
supinic <supinic@protonmail.com>
2026-01-09 17:08:36 +0100

    `stream-silence-prevention` adjust for new local listener API
    - the bot-side should be executing the mpv playlist addition

--- crons/stream-silence-prevention/index.ts
+++ crons/stream-silence-prevention/index.ts
@@ -1,8 +1,12 @@
+import * as z from "zod";
+
 import sharedKeys from "../../utils/shared-cache-keys.json" with { type: "json" };
 import { getConfig } from "../../config.js";
 
 const { listenerAddress, listenerPort } = getConfig().local ?? {};
 const { SONG_REQUESTS_STATE } = sharedKeys;
+
+const autoplaySchema = z.object({ link: z.string() });
 
 export default {
 	name: "stream-silence-prevention",
@@ -37,8 +41,11 @@ export default {
 			return;
 		}
 
-		await core.Got.get("GenericAPI")({
-			url: `${listenerAddress}:${listenerPort}/?autoplay=queue`
+		const response = await core.Got.get("GenericAPI")({
+			url: `${listenerAddress}:${listenerPort}/?autoplay=random`
 		});
+
+		const { link } = autoplaySchema.parse(response.body);
+		await sb.MpvClient.add(link, { user: null, duration: null });
 	})
 };
