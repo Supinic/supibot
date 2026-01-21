68c5b278d9c33c7746beee7efe6dc7a1e39bee1d
supinic <supinic@protonmail.com>
2026-01-21 21:21:30 +0100

    `$totalcountline` rewritten to TS

--- commands/totalcountline/index.ts
+++ commands/totalcountline/index.ts
@@ -1,43 +1,42 @@
-export default {
+import { declare } from "../../classes/command.js";
+
+export default declare({
 	Name: "totalcountline",
-	Aliases: ["acl","tcl"],
-	Author: "supinic",
+	Aliases: ["acl", "tcl"],
 	Cooldown: 10000,
 	Description: "Fetches the total amount of a user's (or yours, if nobody was specified) chat lines in all tracked channels summed together.",
-	Flags: ["mention","pipe","skip-banphrase"],
+	Flags: ["mention", "pipe", "skip-banphrase"],
 	Params: [],
 	Whitelist_Response: null,
 	Code: (async function totalCountLines (context, target) {
-		if (!target) {
-			target = context.user;
-		}
-
-		const userData = await sb.User.get(target);
+		const userData = (target) ? await sb.User.get(target) : context.user;
 		if (!userData) {
 			return {
 				success: false,
-				reply: `That user was not found in the database!`,
-				cooldown: 2500
+				reply: "Provided user does not exist!",
+				cooldown: 5000
 			};
 		}
 
-		const data = (await core.Query.getRecordset(rs => rs
+		const lines = await core.Query.getRecordset<number | undefined>(rs => rs
 			.select("SUM(Message_Count) AS Total")
 			.from("chat_data", "Message_Meta_User_Alias")
 			.where("User_Alias = %n", userData.ID)
 			.single()
-		));
+			.flat("Total")
+		);
 
-		if (data.Total === null) {
+		if (!lines) {
 			return {
-				reply: `That user is being tracked, but they have not said any lines in the channels I watch.`
+				success: true,
+				reply: "That user have not said any lines in the channels I'm in."
 			};
 		}
 
 		const who = (context.user.ID === userData.ID) ? "You have" : "That user has";
 		return {
-			reply: `${who} sent ${core.Utils.groupDigits(data.Total)} chat lines across all tracked channels so far.`
+			reply: `${who} sent ${core.Utils.groupDigits(lines)} chat lines across all tracked channels so far.`
 		};
 	}),
 	Dynamic_Description: null
-};
+});
