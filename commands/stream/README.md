c524a9ce3d0ebb3e5d98777bc5176232c6c5de0e
supinic <supinic@protonmail.com>
2026-01-24 15:34:50 +0100

    `$stream` rewritten to TS, simplified
    - removed "ttsmulti" and "ttslimit" as they are no longer used

--- commands/stream/README.md
+++
@@ -1,6 +0,0 @@
-This command uses the following config variables:
-
-- `TWITCH_OAUTH_EDITOR` - **NOT** setup by the setup script.
-  - Should be an oauth token with editor on your stream. Its unclear from the code if it should have the `oauth:` prefix.
-
-This command documentation can be expanded upon request.
--- commands/stream/index.ts
+++ commands/stream/index.ts
@@ -1,18 +1,12 @@
+import { declare } from "../../classes/command.js";
 import cacheKeys from "../../utils/shared-cache-keys.json" with { type: "json" };
-const {
-	PLAYSOUNDS_ENABLED,
-	TTS_ENABLED,
-	TTS_MULTIPLE_ENABLED,
-	TTS_TIME_LIMIT,
-	SONG_REQUESTS_STATE
-} = cacheKeys;
 
+const { PLAYSOUNDS_ENABLED, TTS_ENABLED, SONG_REQUESTS_STATE } = cacheKeys;
 const AVAILABLE_SONG_REQUEST_STATES = new Set(["mpv", "off"]);
 
-export default {
+export default declare({
 	Name: "stream",
 	Aliases: null,
-	Author: "supinic",
 	Cooldown: 0,
 	Description: "Multiple configurations regarding the stream. Mostly used for #supinic, and nobody else.",
 	Flags: ["developer","mention","pipe","system","whitelist"],
@@ -27,45 +21,25 @@ export default {
 		}
 
 		type = type.toLowerCase();
+
 		switch (type) {
 			case "tts": {
 				const value = (rest.shift() === "true");
 				await core.Cache.setByPrefix(TTS_ENABLED, value);
 
 				return {
+					success: true,
 					reply: `Text to speech is now set to ${value}.`
 				};
 			}
 
-			case "ttslimit": {
-				const limit = Number(rest.shift());
-				if (!Number.isFinite(limit) || limit < 0 || limit > 300_000) {
-					return {
-						reply: "Invalid value provided! Must be in the range <0, 300000>."
-					};
-				}
-
-				await core.Cache.setByPrefix(TTS_TIME_LIMIT, limit);
-				return {
-					reply: `Text to speech time limit is now set to ${limit} milliseconds.`
-				};
-			}
-
-			case "ttsmulti":
-			case "multitts": {
-				const value = (rest.shift() === "true");
-				await core.Cache.setByPrefix(TTS_MULTIPLE_ENABLED, value);
-				return {
-					reply: `Concurrent text to speech is now set to ${value}`
-				};
-			}
-
 			case "ps":
-			case "playsounds":
-			case "playsound": {
+			case "playsound":
+			case "playsounds": {
 				const value = (rest.shift() === "true");
 				await core.Cache.setByPrefix(PLAYSOUNDS_ENABLED, value);
 				return {
+					success: true,
 					reply: `Play sounds are now set to ${value}`
 				};
 			}
@@ -74,6 +48,7 @@ export default {
 				const value = (rest.shift() || "").toLowerCase();
 				if (!AVAILABLE_SONG_REQUEST_STATES.has(value)) {
 					return {
+						success: false,
 						reply: "Invalid song request state!"
 					};
 				}
@@ -84,12 +59,16 @@ export default {
 				}
 
 				return {
+					success: true,
 					reply: `Song requests are now set to ${value}`
 				};
 			}
-
-			default: return { reply: "Unrecognized command." };
 		}
+
+		return {
+			success: false,
+			reply: "Unrecognized command!"
+		};
 	}),
 	Dynamic_Description: null
-};
+});
