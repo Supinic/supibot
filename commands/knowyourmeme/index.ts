97444c8a1386a604d972814ce6b26fadd1e17f02
supinic <supinic@protonmail.com>
2026-02-05 20:55:05 +0100

    `$knowyourmeme` rewritten to TS + fixed
    - changed up the cheerio lookup to have a better success rate

--- commands/knowyourmeme/index.ts
+++ commands/knowyourmeme/index.ts
@@ -1,42 +1,45 @@
-export default {
+import { declare } from "../../classes/command.js";
+
+export default declare({
 	Name: "knowyourmeme",
 	Aliases: ["kym"],
-	Author: "supinic",
 	Cooldown: 30000,
-	Description: "Gets a smol description of a meme from Know Your Meme, it's just the summary.",
-	Flags: ["mention","non-nullable","pipe"],
+	Description: "Gets a brief description of a meme from Know Your Meme, just the summary.",
+	Flags: ["mention", "non-nullable", "pipe"],
 	Params: [],
 	Whitelist_Response: null,
 	Code: (async function knowYourMeme (context, ...args) {
 		if (args.length === 0) {
 			return {
+				success: false,
 				reply: "No search term provided!"
 			};
 		}
 
-		const searchHTML = await core.Got.get("FakeAgent")({
+		const serachResponse = await core.Got.get("FakeAgent")({
 			url: "https://knowyourmeme.com/search",
 			searchParams: {
 				q: args.join(" ")
 			},
 			responseType: "text"
-		}).text();
+		});
 
-		const $search = core.Utils.cheerio(searchHTML);
-		const firstLink = $search(".entry_list a").first().attr("href");
+		const $search = core.Utils.cheerio(serachResponse.body);
+		const firstLink = $search("a.item").attr("href");
 		if (!firstLink) {
 			return {
+				success: false,
 				reply: "No result found for given search term!"
 			};
 		}
 
-		const detailHTML = await core.Got.get("FakeAgent")({
+		const detailResponse = await core.Got.get("FakeAgent")({
 			prefixUrl: "https://knowyourmeme.com",
 			url: firstLink.replace(/^\//, ""),
 			responseType: "text"
-		}).text();
+		});
 
-		const $detail = core.Utils.cheerio(detailHTML);
+		const $detail = core.Utils.cheerio(detailResponse.body);
 		const summary = $detail("#entry_body h2#about")
 			.first()
 			.next()
@@ -44,14 +47,16 @@ export default {
 
 		if (!summary) {
 			return {
+				success: false,
 				reply: "No summary found for given meme!"
 			};
 		}
 
 		const link = `https://knowyourmeme.com${firstLink}`;
 		return {
+			success: true,
 			reply: `${link} ${summary}`
 		};
 	}),
 	Dynamic_Description: null
-};
+});
