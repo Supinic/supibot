eb9eb146d7c71f01ec41ef7cf3d218f96de72dbd
supinic <supinic@protonmail.com>
2026-02-05 20:42:27 +0100

    `$link` rewritten to TS

--- commands/link/index.ts
+++ commands/link/index.ts
@@ -1,27 +1,36 @@
-export default {
+import { declare } from "../../classes/command.js";
+import type { Platform } from "../../platforms/template.js";
+
+type Verification = {
+	Platform_From: Platform["ID"];
+	Platform_To: Platform["ID"];
+	Specific_ID: string;
+	Status: string;
+};
+
+export default declare({
 	Name: "link",
 	Aliases: null,
-	Author: "supinic",
 	Cooldown: 5000,
 	Description: "Verifies your account linking challenge across platforms. You should only ever use this command if you are prompted to.",
-	Flags: ["developer","mention","system"],
+	Flags: ["developer", "mention", "system"],
 	Params: [],
 	Whitelist_Response: null,
 	Code: (async function link (context, challengeString) {
-		if (!context.privateMessage) {
+		if (!challengeString || challengeString === "alias") {
 			return {
 				success: false,
-				reply: `You cannot use this command outside of private messages!`
+				reply: `You probably meant to use the "$alias link" command!`
 			};
 		}
-		else if (!challengeString) {
+		if (!context.channel) {
 			return {
 				success: false,
-				reply: "If you have a challenge string, use it. If you don't have one, don't use this command."
+				reply: "This command is only used for account linking, and must be used in PMs!"
 			};
 		}
 
-		const challengeID = await core.Query.getRecordset(rs => rs
+		const challengeID = await core.Query.getRecordset<number | undefined>(rs => rs
 			.select("ID")
 			.from("chat_data", "User_Verification_Challenge")
 			.where("User_Alias = %n", context.user.ID)
@@ -36,25 +45,26 @@ export default {
 		if (typeof challengeID !== "number") {
 			return {
 				success: false,
-				reply: `No active verification found!`
+				reply: `No active verification found! Double check your challenge string.`
 			};
 		}
 
-		const row = await core.Query.getRow("chat_data", "User_Verification_Challenge");
+		const row = await core.Query.getRow<Verification>("chat_data", "User_Verification_Challenge");
 		await row.load(challengeID);
 
-		const sourcePlatform = sb.Platform.get(row.values.Platform_From);
-		const targetPlatform = sb.Platform.get(row.values.Platform_To);
+		const sourcePlatform = sb.Platform.getAsserted(row.values.Platform_From);
+		const targetPlatform = sb.Platform.getAsserted(row.values.Platform_To);
+		const idColumnName = (sourcePlatform.name === "twitch") ? "Twitch_ID" : "Discord_ID";
 
-		const idColumnName = `${sourcePlatform.capital}_ID`;
 		await context.user.saveProperty(idColumnName, row.values.Specific_ID);
 
 		row.values.Status = "Completed";
 		await row.save();
 
 		return {
+			success: true,
 			reply: `Verification completed! You may now use the bot on ${sourcePlatform.capital} as well as ${targetPlatform.capital}.`
 		};
 	}),
 	Dynamic_Description: null
-};
+});
