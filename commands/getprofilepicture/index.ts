45275fc4f5d6990bfc54d7d06f07bbac15ddc229
supinic <supinic@protonmail.com>
2026-02-08 14:35:10 +0100

    `$getprofilepicture` rewritten to TS + zod (API)

--- commands/getprofilepicture/index.ts
+++ commands/getprofilepicture/index.ts
@@ -1,53 +1,32 @@
-import { SupiError } from "supi-core";
+import { declare } from "../../classes/command.js";
+import { ivrUserDataSchema } from "../../utils/schemas.js";
 
-export default {
+export default declare({
 	Name: "getprofilepicture",
 	Aliases: ["avatar", "pfp"],
-	Author: "supinic",
 	Cooldown: 10000,
 	Description: "For a given Twitch user, this command will fetch their profile picture.",
-	Flags: ["mention","non-nullable","pipe"],
+	Flags: ["mention", "non-nullable", "pipe"],
 	Params: [
 		{ name: "banner", type: "boolean" },
 		{ name: "linkOnly", type: "boolean" }
 	],
 	Whitelist_Response: null,
-	Code: (async function profilePicture (context, username) {
+	Code: async function profilePicture (context, username?: string) {
 		const login = sb.User.normalizeUsername(username ?? context.user.Name);
 		const response = await core.Got.get("IVR")({
 			url: "v2/twitch/user",
-			searchParams: {
-				login
-			}
+			searchParams: { login }
 		});
 
-		if (!response.ok) {
-			return {
-				success: false,
-				reply: `Could not fetch profile picture! ${response.body.message}`
-			};
-		}
-		else if (!Array.isArray(response.body)) {
-			// 2023-03-25: Super sanity check, IVR API maybe(?) sometimes returns a non-Array response for the `user` endpoint
-			const logId = await sb.Logger.log(
-				"Command.Fail",
-				`IVR API returned non-array: ${JSON.stringify(response.body)}`,
-				context.channel,
-				context.user
-			);
-
-			throw new SupiError({
-				message: `Invalid IVR API response - more data: Log #${logId}`
-			});
-		}
-		else if (response.body.length === 0) {
+		const user = ivrUserDataSchema.parse(response.body).at(0);
+		if (!user) {
 			return {
 				success: false,
 				reply: `No such user found!`
 			};
 		}
 
-		const [user] = response.body;
 		if (context.params.banner === true) {
 			if (user.banner === null) {
 				return {
@@ -70,8 +49,8 @@ export default {
 				? user.logo
 				: `Profile picture for ${user.displayName}: ${user.logo}`
 		};
-	}),
-	Dynamic_Description: (async (prefix) => [
+	},
+	Dynamic_Description: (prefix) => [
 		"Fetches a Twitch user's profile picture, or other pictures related to their account.",
 		"",
 
@@ -94,5 +73,5 @@ export default {
 		`<code>${prefix}pfp <u>banner:true</u> <u>linkOnly:true</u> (user)</code>`,
 		`<code>${prefix}pfp <u>banner:true</u> <u>linkOnly:true</u> @pajlada</code>`,
 		`Post only the link to the user's profile banner. If they don't have one, the message "N/A" will show up.`
-	])
-};
+	]
+});
