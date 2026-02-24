bb911e9c7485f6d33ce0eab54a45452085fd29cc
supinic <supinic@protonmail.com>
2026-02-24 20:25:49 +0100

    `$markov` rewrite to TS, vastly slim down code
    - use a wrapper helper method to grab AsyncMarkov data from module (should be refactored when available properly in TS)
    - remove multichannel mode, "dull" mode, "exact" mode
    - remove debug-parameter commands
    - remove "words" parameter + increase default amount of words

---
+++ commands/markov/index.ts
@@ -0,0 +1,119 @@
+import { SupiError } from "supi-core";
+import type AsyncMarkov from "async-markov";
+import { declare } from "../../classes/command.js";
+import type { Channel } from "../../classes/channel.js";
+
+const MODEL_SIZE_THRESHOLD = 25;
+const WORD_AMOUNT = 25;
+
+type ModuleRow = { channelId: Channel["ID"], name: Channel["Name"] };
+const getMarkovData = () => {
+	const module = sb.ChatModule.get("async-markov-experiment");
+	if (!module) {
+		throw new SupiError({
+			message: "Assert error: Markov module is not available"
+		});
+	}
+
+	const { data } = module;
+	if (!("markovs" in data)) {
+		throw new SupiError({
+			message: "Assert error: Markov module has no specific data available"
+		});
+	}
+
+	return {
+		// @todo use proper typing for markov's chat module when available
+		markovs: data.markovs as Map<Channel["ID"], AsyncMarkov>
+	};
+};
+
+export default declare({
+	Name: "markov",
+	Aliases: null,
+	Cooldown: 5000,
+	Description: "Creates a random sequence of words based on a Markov-chain module from Twitch chat.",
+	Flags: ["non-nullable","pipe"],
+	Params: [
+		{ name: "channel", type: "string" },
+		{ name: "stop", type: "boolean" }
+	],
+	Whitelist_Response: null,
+	Code: function markov (context, input) {
+		let targetChannel;
+		if (context.params.channel) {
+			const channelData = sb.Channel.get(context.params.channel);
+			if (!channelData) {
+				return {
+					success: false,
+					reply: "Invalid channel provided!"
+				};
+			}
+
+			targetChannel = channelData;
+		}
+		else if (context.channel) {
+			targetChannel = context.channel;
+		}
+		else {
+			targetChannel = sb.Channel.get("forsen"); // legacy fallback behaviour
+		}
+
+		if (!targetChannel) {
+			return {
+				success: false,
+				reply: `No such channel exists!`
+			};
+		}
+
+		const { markovs } = getMarkovData();
+		const markov = markovs.get(targetChannel.ID);
+		if (!markov) {
+			return {
+				success: false,
+				reply: "This channel does not have a markov-chain module configured!"
+			};
+		}
+		else if (markov.size < MODEL_SIZE_THRESHOLD) {
+			return {
+				success: false,
+				reply: `Markov-chain module does not have enough data available! (${markov.size}/${MODEL_SIZE_THRESHOLD} required)`
+			};
+		}
+
+		const string = markov.generateWords(WORD_AMOUNT, input);
+		return {
+			reply: `🔮 ${string}`
+		};
+	},
+	Dynamic_Description: async function (prefix) {
+		const channels = await core.Query.getRecordset<ModuleRow[]>(rs => rs
+			.select("Channel.ID AS channelId", "Channel.Name as name")
+			.from("chat_data", "Channel_Chat_Module")
+			.where("Chat_Module = %s", "async-markov-experiment")
+			.where("Channel.Platform = %n", 1)
+			.join({
+				toTable: "Channel",
+				on: "Channel_Chat_Module.Channel = Channel.ID"
+			})
+		);
+
+		const channelList = channels.map(i => (
+			`<li><a href="//twitch.tv/${i.name}">${i.name}</a> -- <a href="/data/other/markov/${i.channelId}/words">List of words</a>`
+		)).join("");
+
+		return [
+			`Uses a <a href="//en.wikipedia.org/wiki/Markov_model">Markov model</a> to generate "real-looking" sentences based on Twitch chat.`,
+			"Various channels are supported, and the command currently uses @Forsen's channel by default if no channel is provided.",
+			"",
+
+			`<code>${prefix}markov</code>`,
+			"Generates random words, with the first one being chosen randomly as well.",
+			"",
+
+			`<code>${prefix}markov channel:(channel)</code>`,
+			"Generates words in the specified channel's context.",
+			`List of currently supported channels: <ul>${channelList}</ul>`
+		];
+	}
+});
