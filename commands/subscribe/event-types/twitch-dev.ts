ec59ba37ebcc5c68eed67e17bd048111baf1c425
badoge <18620902+badoge@users.noreply.github.com>
2026-03-06 08:13:29 -0800

    `$subscribe` add Twitch dev changelog (#163)

---
+++ commands/subscribe/event-types/twitch-dev.ts
@@ -0,0 +1,17 @@
+import type { RssEventDefinition } from "../generic-event.js";
+
+export default {
+	name: "Twitch Developer Changelog",
+	aliases: ["twitchdev"],
+	channelSpecificMention: true,
+	response: {
+		added: "You will now be pinged whenever the Twitch developer documentation changes.",
+		removed: "You will no longer receive pings for Twitch developer documentation changes."
+	},
+	generic: true,
+	cronExpression: "0 */5 * * * *",
+	subName: "Twitch Developer Changelog",
+	type: "rss",
+	cacheKey: "twitch-dev-last-publish-date",
+	url: "https://chillymosh.com/rss/twitch-dev-changelog.xml"
+} satisfies RssEventDefinition;
