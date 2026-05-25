ac3f53ff9a471c4cfb8208f26038bf9fc0f0a37e
supinic <supinic@protonmail.com>
2026-05-25 13:22:05 +0200

    preliminary TS rework
    - introduce `unping` into `command-utils`

--- commands/top/index.ts
+++ commands/top/index.ts
@@ -1,7 +1,7 @@
-import connectedChannelGroups from "./connected-channels.json" with { type: "json" };
-import { TWITCH_ANTIPING_CHARACTER } from "../../utils/command-utils.js";
+import { unping } from "../../utils/command-utils.js";
+import { declare } from "../../classes/command.js";
 
-export default {
+export default declare({
 	Name: "top",
 	Aliases: null,
 	Author: "supinic",
@@ -13,7 +13,8 @@ export default {
 	],
 	Whitelist_Response: null,
 	Code: (async function top (context, rawLimit) {
-		if (!context.channel) {
+		const { channel } = context;
+		if (!channel) {
 			return {
 				success: false,
 				reply: `This command is not available here!`
@@ -40,13 +41,13 @@ export default {
 			};
 		}
 
-		const channelIDs = new Set([context.channel.ID]);
+		const channelIDs = new Set([channel.ID]);
 		if (context.platform.Name === "twitch" && !context.params.currentOnly) {
-			const previousIDs = await core.Query.getRecordset(rs => rs
+			const previousIDs = await core.Query.getRecordset<number[]>(rs => rs
 				.select("ID")
 				.from("chat_data", "Channel")
-				.where("ID <> %n", context.channel.ID)
-				.where("Specific_ID = %s", context.channel.Specific_ID)
+				.where("ID <> %n", channel.ID)
+				.where("Specific_ID = %s", channel.Specific_ID)
 				.flat("ID")
 			);
 
@@ -55,9 +56,9 @@ export default {
 			}
 		}
 
-		const top = await core.Query.getRecordset(rs => rs
-			.select("SUM(Message_Count) AS Total")
-			.select("User_Alias.Name AS Name")
+		const top = await core.Query.getRecordset<{ messages: number; username: string; }[]>(rs => rs
+			.select("SUM(Message_Count) AS messages")
+			.select("User_Alias.Name AS username")
 			.from("chat_data", "Message_Meta_User_Alias")
 			.join("chat_data", "User_Alias")
 			.where("Channel IN %n+", [...channelIDs])
@@ -66,9 +67,10 @@ export default {
 			.limit(limit)
 		);
 
-		const chatters = top.map((i, ind) => {
-			const name = `${i.Name[0]}${TWITCH_ANTIPING_CHARACTER}${i.Name.slice(1)}`;
-			return `#${ind + 1}: ${name} (${core.Utils.groupDigits(i.Total)})`;
+		const chatters = top.map(({ messages, username }, ind) => {
+			const name = unping(username);
+			const digits = core.Utils.groupDigits(messages);
+			return `#${ind + 1}: ${name} (${digits})`;
 		}).join(", ");
 
 		return {
@@ -78,7 +80,7 @@ export default {
 			reply: `Top ${limit} chatters: ${chatters}`
 		};
 	}),
-	Dynamic_Description: (async () => [
+	Dynamic_Description: () => [
 		"Shows you the top chatters in the current channel.",
 		"By default, shows the top 10 - but you can choose any number between 1 and 100.",
 		"In the case you select more users than 10-20, I recommend piping the output to $pbp or $hbp for ease of use.",
@@ -97,5 +99,5 @@ export default {
 		"Instead of using all previous channels' data (e.g. when it was renamed), this parameter will force it so only the current is used.",
 		"E.g.: If you're in channel @Pepega, but the channel renamed from @PepeHands in the past, this command will only show the top chatters in @Pepega.",
 		"By default, the command shows you data from all channels."
-	])
-};
+	]
+});
