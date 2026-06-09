cdcd14b8ec8d57b14068501d327e641729fec69b
badoge <18620902+badoge@users.noreply.github.com>
2026-06-09 20:46:24 +0400

    `$subscribe` add GitHub status (#173)

---
+++ commands/subscribe/event-types/github-status.ts
@@ -0,0 +1,17 @@
+import type { RssEventDefinition } from "../generic-event.js";
+
+export default {
+	name: "GitHub Status",
+	aliases: [],
+	channelSpecificMention: true,
+	response: {
+		added: "You will now be pinged whenever GitHub goes down.",
+		removed: "You will no longer receive pings when GitHub goes down.",
+	},
+	generic: true,
+	cronExpression: "0 */5 * * * *",
+	subName: "GitHub incident",
+	type: "rss",
+	cacheKey: "github-status-last-publish-date",
+	url: "https://www.githubstatus.com/history.rss",
+} satisfies RssEventDefinition;
