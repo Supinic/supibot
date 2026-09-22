e465a62d19467c9226062a1f9e0ea5fd58a1843e
supinic <supinic@protonmail.com>
2026-09-22 18:52:48 +0200

    wip 15 - global config for `raid-react-tts`

--- chat-modules/raid-react-tts/index.ts
+++ chat-modules/raid-react-tts/index.ts
@@ -1,4 +1,7 @@
+import { getConfig } from "../../config.js";
 import { defineChatModule } from "../../classes/chat-module.js";
+
+const { local = {} } = getConfig();
 
 export default defineChatModule({
 	name: "raid-react-tts",
@@ -7,16 +10,21 @@ export default defineChatModule({
 	scope: "channel",
 	handlers: {
 		async raid (context) {
-			const { channel, user, username, data } = context;
+			const { listenerAddress, listenerPort } = local;
+			if (!listenerAddress || !listenerPort) {
+				return;
+			}
+
+			const { channel, username, data } = context;
 			const amount = (data.viewers > 1000)
 				? (`${String(core.Utils.round(data.viewers / 1000, 1)).replace(".", " point ")} billion`)
 				: (`${data.viewers} million`);
 
-			const message = `FeelsGoodMan Clap ${user?.Name ?? username} just raided the stream with ${amount} viewers!`;
+			const message = `FeelsGoodMan Clap ${username} just raided the stream with ${amount} viewers!`;
 			await channel.send(message);
 
 			await core.Got.get("GenericAPI")({
-				url: "http://192.168.1.100:9999",
+				url: `${listenerAddress}:${listenerPort}`,
 				responseType: "text",
 				searchParams: new URLSearchParams({
 					tts: JSON.stringify([{
