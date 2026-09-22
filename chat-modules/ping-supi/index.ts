b431baf555919507b7856973d3434ee3838907e9
supinic <supinic@protonmail.com>
2026-09-22 18:41:10 +0200

    wip 12 - state for `ping-supi`

--- chat-modules/ping-supi/index.ts
+++ chat-modules/ping-supi/index.ts
@@ -1,7 +1,6 @@
 import { SupiDate } from "supi-core";
 import { defineChatModule } from "../../classes/chat-module.js";
 
-let timeoutTimestamp = 0;
 const relaxedRegex = /(supi|supinic(?!\.com)|bupi|супи|супик.?|супиник.?)\b/i;
 // const strictRegex = /\bsupinic(?!\.com)\b/i;
 const skippedUserIds = [1, 1127, 582108, 8697460, 12182780, 17865963];
@@ -11,12 +10,16 @@ export default defineChatModule({
 	description: "This module notifies Supinic whenever he is mentioned (in any channel, across platforms) via Twitch whispers.",
 	scope: "global",
 	platform: "all",
+	state: () => ({
+		// consider using a Map<Channel, number> to prevent multi-pings per-channel instead of globally
+		timestamp: 0
+	}),
 	handlers: {
-		async message (context) {
+		async message (context, { state }) {
 			const { message, channel, user } = context;
 			const now = SupiDate.now();
 
-			if (now < timeoutTimestamp) {
+			if (state.timestamp > now) {
 				return;
 			}
 			if (user?.ID && skippedUserIds.includes(user.ID)) {
@@ -37,9 +40,9 @@ export default defineChatModule({
 			// }
 
 			const twitchPlatform = sb.Platform.getAsserted("twitch");
-			const userName = user?.Name ?? `❓${context.raw.user}`;
+			const userName = user?.Name ?? `❓${context.raw?.user ?? "(unknown)"}`;
 
-			timeoutTimestamp = now + 1000;
+			state.timestamp = now + 1000;
 
 			const pingMessage = `[ ${channel.Description ?? channel.Name} ]: ${userName} : ${message}`;
 			await twitchPlatform.pm(pingMessage, await sb.User.getAsserted("supinic"));
