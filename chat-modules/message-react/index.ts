80fcfbd7727aacec753eb9ee5222a08bf362302b
supinic <supinic@protonmail.com>
2026-09-22 18:35:01 +0200

    wip 10 - zod schema for `message-react`

--- chat-modules/message-react/index.ts
+++ chat-modules/message-react/index.ts
@@ -1,3 +1,4 @@
+import * as z from "zod";
 import { defineChatModule } from "../../classes/chat-module.js";
 
 export default defineChatModule({
@@ -5,12 +6,27 @@ export default defineChatModule({
 	description: "According to arguments, reacts to a specific message(s) with a determined response.",
 	platform: "all",
 	scope: "channel",
+	config: z.array(z.object({
+		ignoreCase: z.boolean().optional(),
+		response: z.string(),
+		check: z.union([
+			z.object({
+				type: z.literal("string"),
+				string: z.string()
+			}),
+			z.object({
+				type: z.literal("includes"),
+				mode: z.enum(["any", "all"]),
+				values: z.array(z.string())
+			}),
+			z.object({
+				type: z.literal("regex"),
+				source: z.string()
+			})
+		])
+	})),
 	handlers: {
-		message (context, ...args: any[]) {
-			if (args.length === 0) {
-				return;
-			}
-
+		message (context, runtime) {
 			const { channel, platform, user } = context;
 			if (!user) {
 				return;
@@ -23,8 +39,8 @@ export default defineChatModule({
 			}
 
 			const { message } = context;
-			for (const item of args) {
-				let passed = false;
+			for (const item of runtime.config) {
+				let passed: boolean;
 				const { ignoreCase = false, check, response } = item;
 				const checkMessage = (ignoreCase) ? message.toLowerCase() : message;
 
@@ -49,7 +65,7 @@ export default defineChatModule({
 						);
 					}
 				}
-				else if (check.type === "regex") {
+				else {
 					const { source } = check;
 					const regex = core.Utils.parseRegExp(source);
 					if (!regex) {
