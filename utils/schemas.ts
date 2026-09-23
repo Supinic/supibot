d4131ee2a2820cf0511cff006e4bafc87ff7754e
supinic <supinic@protonmail.com>
2026-09-23 17:04:30 +0200

    update Helix vod schema (`stream_id`)

--- utils/schemas.ts
+++ utils/schemas.ts
@@ -100,7 +100,7 @@ export const twitchScheduleSchema = z.object({
 export const twitchVodSchema = z.object({
 	data: z.array(z.object({
 		id: z.string(),
-		stream_id: z.string(),
+		stream_id: z.string().nullish(),
 		created_at: z.string(), // RFC 3339
 		duration: z.string(), // ISO 8601
 		title: z.string(),
